a45186faf4cb0995d5951beb518c55196a84b815
Chris Larson <clarson@kixer.com>
2017-11-07 15:19:26 -0600

    chapter 2

---
+++ chris_larson/learning-spark/README.md
@@ -0,0 +1,7 @@
+Learning Spark by Karau, Konwinkski, Wendell, Zaharia from O'Reilly
+
+Chapter 2 demo:
+~~~~
+  cd readlog
+  .././spark/bin/spark-submit --class "ch2" --master local[4] target/scala-2.10/log-reader_2.10-0.0.1.jar
+~~~~
---
+++ chris_larson/learning-spark/readlog/src/main/scala/readlog.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object readlog {
+    def main(args: Array[String]) {
+      val conf = new SparkConf().setMaster("local").setAppName("Log Reader")
+      val sc = new SparkContext(conf)
+
+      val textFile = sc.textFile("../E1IN64R500NXTW.2017-10-19-12.9c2ba7f3.txt")
+      val linesWithMiss = textFile.filter(line => line.contains("Miss"))
+      linesWithMiss.take(10).foreach(println)
+    }
+}
