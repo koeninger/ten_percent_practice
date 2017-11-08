38725f18fc6924b3a5bdeb305ea293f7f5019324
Chris Larson <clarson@kixer.com>
2017-11-08 14:36:52 -0600

    ch3 RDDs

--- chris_larson/learning-spark/readlog/src/main/scala/readlog.scala
+++ chris_larson/learning-spark/readlog/src/main/scala/readlog.scala
@@ -8,7 +8,10 @@ object readlog {
       val sc = new SparkContext(conf)
 
       val textFile = sc.textFile("../E1IN64R500NXTW.2017-10-19-12.9c2ba7f3.txt")
-      val linesWithMiss = textFile.filter(line => line.contains("Miss"))
-      linesWithMiss.take(10).foreach(println)
+      val cacheMiss = textFile.filter(line => line.contains("Miss"))
+      val cacheHit = textFile.filter(line => line.contains("Hit"))
+      val errors = textFile.filter(line => line.contains("Error"))
+      val cacheMissAndErrors = cacheMiss.intersection(errors)
+      cacheMissAndErrors.take(10).foreach(println)
     }
 }
