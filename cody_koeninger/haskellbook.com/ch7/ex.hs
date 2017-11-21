0e78c88214b82844e520833d808266ac2f638a29
cody koeninger <cody@koeninger.org>
2017-11-20 20:01:39 -0600

    pg 236

---
+++ cody_koeninger/haskellbook.com/ch7/ex.hs
@@ -0,0 +1,12 @@
+addOneIfOdd n =
+    case odd n of
+      True -> f n
+      False -> n
+    where f = \n -> n + 1
+
+addFive = \x -> \y -> (if x > y then y else x) + 5
+
+mflip f x y = f y x          
+
+f :: (a, b, c) -> (d, e, f) -> ((a, d), (c, f))
+f (a, b, c) (d, e, f) = ((a, d), (c, f))
