beeddb51b71ae137a763131aaf838f0b3c8cb578
lynda <lwellhausen@kixer.com>
2017-12-05 18:16:52 -0600

    update fund object

--- lynda_wellhausen/api-experiment/server/packages/usfunds/USFundsReader.js
+++ lynda_wellhausen/api-experiment/server/packages/usfunds/USFundsReader.js
@@ -1,34 +1,29 @@
 
 Meteor.publish('usFundsSearch', function(symbol) {
     var self = this;
-    var fundData = {};
-    var fundDataArray = [];
+    var symbol = {};
     var hourlyData = [];
-    var i = 1;
+    var symbolData = {};
 
     HTTP.get('https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=AMAT&interval=60min&apikey=B7KCRRWX0P2ONSWL', {}, function (error, response) {
     if ( error ) {
         console.log( error );
     } else {
-        //console.log(response.data['Time Series (60min)']);
-        fundData = response.data['Time Series (60min)'];
-        //console.log('Fund Data: ', fundData);
-        fundDataArray = _.values(fundData);
-        //console.log('fundDataArray[0]', fundDataArray[1]);
-        console.log('fundDataArray', fundDataArray.length);
-        _.each(fundDataArray, function (data) {
-            hourlyData[i] = {
-                open: data['1. open'],
-                close: data['4. close'],
-                low: data['3. low'],
-                high: data['2. high']
-            }
-            //console.log('i: ', i);
-            //console.log('hourlyData[i]', hourlyData[i]);
-            i++
+        symbol = response.data['Time Series (60min)'];
+
+        _.forEach(symbol, function (data, timeStamp) {
+            var stockData = symbol[timeStamp];
+
+            symbolData[timeStamp] = {
+                open: stockData['1. open'],
+                high: stockData['2. high'],
+                low: stockData['3. low'],
+                close: stockData['4. close']
+            };
         });
-        //console.log(hourlyData);
-        //console.log('i: ',i);
+
+        //console.log('symbolData', symbolData);
+
         }
     });
 });
