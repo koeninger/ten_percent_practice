3d555aa4eaa6bcbbd022e61280c648e2e6c0c5e6
jvelis <jvelis@kixer.com>
2017-12-08 15:18:29 -0600

    fading circles exercise

--- juan_velis/creative_scala/src/main/scala/chapter7e.scala
+++ juan_velis/creative_scala/src/main/scala/chapter7e.scala
@@ -8,7 +8,6 @@ import doodle.backend.StandardInterpreter._
 object Example {
 
    // 7.6 boxes with paramaters
-
     def boxes(x:Int, size:Int):Image =
       x match {
         case 0 => Image.empty
@@ -27,10 +26,17 @@ object Example {
         case n => Image.circle(r).lineColor(Color.blue) on nestedcircles(n-1,r+5)
       }
 
+    def colorcircles(count:Int, r:Int, color:Color):Image =
+      count match {
+        case 0 => Image.empty
+        case n => Image.circle(r).lineColor(color) on colorcircles(n-1,r+7,color.spin(5.degrees))
+      }
+
     def main(args:Array[String]):Unit = {
       boxes(5, 5).draw
       colorboxes(5, Color.blue).draw
       nestedcircles(10,30).draw
+      colorcircles(10,30,Color.red).draw
     }
 
 }
