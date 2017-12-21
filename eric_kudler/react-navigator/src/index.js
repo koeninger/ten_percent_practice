20eccc16406be1657cd78865f33f72ce2632de11
Eric Kudler <joeento@gmail.com>
2017-12-21 10:39:51 -0500

    Started adding some of the navigation logic to the index

--- eric_kudler/react-navigator/src/index.js
+++ eric_kudler/react-navigator/src/index.js
@@ -127,10 +127,51 @@ class Game extends React.Component {
 	}
 }
 
+
+// ========================================
+class NavigationBar extends React.Component {
+	render() {
+		return (
+			<Navbar>
+				<Navbar.Header>
+					<Navbar.Brand>
+						<a href="#">React-Bootstrap</a>
+					</Navbar.Brand>
+				</Navbar.Header>
+				<Nav>
+					<NavItem eventKey={1} href="#">Link</NavItem>
+					<NavItem eventKey={2} href="#">Link</NavItem>
+					<NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+						<MenuItem eventKey={3.1}>Action</MenuItem>
+						<MenuItem eventKey={3.2}>Another action</MenuItem>
+						<MenuItem eventKey={3.3}>Something else here</MenuItem>
+						<MenuItem divider />
+						<MenuItem eventKey={3.4}>Separated link</MenuItem>
+					</NavDropdown>
+				</Nav>
+			</Navbar>
+		);
+	}
+}
+class Navigator extends React.Component {
+	constructor(props) {
+		super(props);
+  	}
+  	render() {
+  		return (
+  			<div>
+  				<NavigationBar />
+				<Panel>
+					jeior reogjruiegj fgre
+				</Panel>
+			</div>
+  		);
+  	}
+}
 // ========================================
 
 ReactDOM.render(
-	<Game />,
+	<Navigator />,
 	document.getElementById('root')
 );
 
