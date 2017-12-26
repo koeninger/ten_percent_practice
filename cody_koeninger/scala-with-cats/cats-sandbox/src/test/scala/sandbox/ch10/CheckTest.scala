48bd6f78942d383e50550aa3848cf839ad5a9ca6
cody koeninger <cody@koeninger.org>
2017-12-26 13:30:54 -0600

    cats pg 211

--- cody_koeninger/scala-with-cats/cats-sandbox/src/test/scala/sandbox/ch10/CheckTest.scala
+++ cody_koeninger/scala-with-cats/cats-sandbox/src/test/scala/sandbox/ch10/CheckTest.scala
@@ -1,28 +1,62 @@
 package sandbox.ch10
 
+import cats.Semigroupal
+import cats.data.NonEmptyList
+import cats.data.Validated
 import cats.data.Validated.{ Valid, Invalid }
-import cats.instances.list._
+
 import org.scalatest.FunSuite
 
 class CheckTest extends FunSuite {
-  type C = Check[List[String], Int]
-  val err = Invalid(List("boom"))
-  val ok: C = Check((x: Int) => Valid(x))
-  val bad: C = Check((x: Int) => err)
+  type Errors = NonEmptyList[String]
+  type AllErrors[A] = Validated[Errors, A]
 
-  test("and") {
-    assert(ok.and(bad).apply(1) == err)
-    assert(bad.and(ok).apply(1) == err)
-    assert(ok.and(ok).apply(1) == Valid(1))
-    assert(ok.or(bad).apply(1) == Valid(1))
-    assert(bad.or(ok).apply(1) == Valid(1))
-    assert(bad.or(bad).apply(1) == err)
+  def error(s: String): NonEmptyList[String] = NonEmptyList(s, Nil)
+
+  def longerThan(n: Int): Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must be longer than $n characters"),
+      s => s.size > n)
+
+  def alphanumeric: Predicate[Errors, String] =
+    Predicate.lift(
+      error("Must all be alphanumeric characters"),
+      s => s.forall(_.isLetterOrDigit))
+
+  def contains(c: Char): Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must contain the character $c"),
+      s => s.contains(c))
+
+  def containsOnce(c: Char): Predicate[Errors, String] =
+    Predicate.lift(
+      error(s"Must contain the character $c exactly once"),
+      s => s.filter(_ == c).length == 1)
+
+  def username: Predicate[Errors, String] =
+    longerThan(3).and(alphanumeric)
+
+  def email: Predicate[Errors, String] = Predicate.Pure { s =>
+    s.split('@') match {
+      case Array(left, right) =>
+        Semigroupal[AllErrors].product(
+          longerThan(0)(left),
+          longerThan(2).and(contains('.'))(right)).
+          map(_ => s)
+      case _ =>
+        Invalid(NonEmptyList("Must contain exactly one '@'", Nil))
+    }
   }
 
-  test("or") {
-    assert(ok.or(ok).apply(1) == Valid(1))
-    assert(ok.or(bad).apply(1) == Valid(1))
-    assert(bad.or(ok).apply(1) == Valid(1))
-    assert(bad.or(bad).apply(1) == err)
+  test("username") {
+    assert(username("bob").isInstanceOf[Invalid[_]])
+    assert(username("bobo").isInstanceOf[Valid[_]])
+  }
+
+  test("email") {
+    assert(email("f@f").isInstanceOf[Invalid[_]])
+    assert(email("f.com").isInstanceOf[Invalid[_]])
+    assert(email("@f.com").isInstanceOf[Invalid[_]])
+    assert(email("b@f.c").isInstanceOf[Valid[_]])
   }
 }
