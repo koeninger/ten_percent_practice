d456ffa82aa634f47c217a21796a7a0aafa2bb26
jvelis <jvelis@kixer.com>
2017-12-29 12:55:23 -0600

    chapter 9 - list lenght

--- juan_velis/creative_scala/src/main/scala/chapter9d.scala
+++ juan_velis/creative_scala/src/main/scala/chapter9d.scala
@@ -25,9 +25,17 @@ object Example {
         case hd::tl => hd+sum(tl)
       }
 
+    // take any type of list and return the lenght
+    def length[A](list: List[A]): Int =
+      list match {
+        case Nil => 0
+        case hd::tl => 1+length(tl)
+      }
+
     println(hi(10))
     println(increment(List(11,22,33,44,55)))
     println(sum(List(2,3,4,1)))
+    println(length(List(5,3,"juan",1,"was",3,"here",4,5,6)))
 
   }
 }
