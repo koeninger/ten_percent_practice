4f7a5ee72d13308d1693de2eddbff63a9f47c86c
lynda <lwellhausen@kixer.com>
2018-01-03 17:38:17 -0600

    bump into walls with d3

--- lynda_wellhausen/financially/client/main.js
+++ lynda_wellhausen/financially/client/main.js
@@ -35,22 +35,50 @@ angular.module('financially', [
             });
         },
 
-        $scope.toggleTodaysData = function () {
-            if (this.show == false) {
-                this.show = true;
-             } else
-                this.show = false;
+            $scope.toggleTodaysData = function () {
+                if (this.show == false) {
+                    this.show = true;
+                } else
+                    this.show = false;
             },
 
-        $scope.allData = function () {
-            data = Stocks.find({});
+            $scope.allData = function () {
+                data = Stocks.find({});
+            },
 
-        },
+            $scope.styleChart = function () {
+                d3.select(".chart")
+                    .selectAll("div")
+                    .data(data)
+                    .enter().append("div")
+                    .style("width", function (d) { return d * 10 + "px"; })
+                    .text(function (d) { return d; });
+            },
 
         this.helpers({
             todaysData() {
                 // console.log('testing', Stocks.find({ date: 15 }).fetch());
                 return Stocks.find({ date: 15 }).fetch();
+            },
+
+            arrayData() {
+                var i = 0;
+                var hoursArray = [];
+                var todaysData = Stocks.find({ date: 26 }).fetch();
+                $scope.td = {
+                    hoursArray: [],
+                    opensArray: [],
+                    closeArray: []
+                };
+
+                _.forEach(todaysData, function () {
+                    $scope.td.hoursArray.push(todaysData[i].hour);
+                    $scope.td.opensArray.push(todaysData[i].open);
+                    $scope.td.closeArray.push(todaysData[i].close);
+                    i++;
+                });
+
+                return $scope.td;
             }
         });
     }
