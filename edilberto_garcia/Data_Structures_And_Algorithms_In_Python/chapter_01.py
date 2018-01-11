c75f33c1f5c428ac64ad51c9bdec7fe816e29b07
Edilberto Garcia <egarcia@federatedmedia.net>
2018-01-11 18:19:19 -0500

    Chapter 1 continued

--- edilberto_garcia/Data_Structures_And_Algorithms_In_Python/chapter_01.py
+++ edilberto_garcia/Data_Structures_And_Algorithms_In_Python/chapter_01.py
@@ -33,4 +33,68 @@ def minmax(data):
 
 # *************************************************** #
 
+# R-1.4 Write a short Python function that takes a positive integer n and returns
+# the sum of the squares of all the positive integers smaller than n
+def sum_squares(n):
+	ret_val = 0;
+	for i in range(1, n):
+		ret_val += (i * i)
+	return ret_val
+# print sum_squares(12)
+
+# *************************************************** #
+
+# R-1.5 Give a single command that computes the sum from Exercise R-1.4, relying
+# on Python's comprehension syntax and the built-in sum function.
+
+sum([n * n for n in range(1, 12)])
+
+# *************************************************** #
+
+# R-1.6 Write a short Python function that takes a positive integer n and returns
+# the sum of the squares of all the odd positive integers smaller than n.
+
+def sum_squares_odd(n):
+	ret_val = 0;
+	for i in range(1, n):
+		if i % 2 == 1:
+			ret_val += i * i
+	return ret_val
+# print sum_squares_odd(12)
+
+# *************************************************** #
+
+# R-1.7 Give a single command that computes the sum from Exercise R-1.6, relying
+# on Python's comprehension syntax and the built-in sum function.
+
+sum([n * n for n in range(1, 12) if n % 2 == 1])
+
+# R-1.8 Python allows negative integers to be used as indices into a sequence,
+# such as a string. If string s has length n, and expression s[k] is used for index
+# -n <= k < 0, what is the equivalent index j >= 0 such that s[j] references
+# the same element?
+
+	#My Ans: k + n = j
+	# s = "My string", n = 9
+	# s[3] = 's',  s[-9] = 's'
+	# -9(k) + 9(n) = 3(j) 
+
+# *************************************************** #
+
+# R-1.9 What parameters should be sent to the range constructor, to produce a
+# range with values 50, 60, 70, 80?
+
+# print range(50, 81, 10)
+
+# *************************************************** #
+
+# R-1.10 What parameters should be sent to the range constructor, to produce a
+# range with values 8, 6, 4, 2, 0, -2, -4, -6, -8?
+
+# print range(8, -9, -2)
+
+# *************************************************** #
+
+# R-1.11 Demonstrate how to use Python's list comprehension syntax to produce
+# the list [1, 2, 4, 8, 16, 32, 64, 128, 256].
 
