7a8314644fd8df3174d71a6370b5e22d53034c3f
Eric Kudler <joeento@gmail.com>
2018-01-12 13:00:35 -0500

    More code cleanup

--- eric_kudler/react-navigator/src/index.js
+++ eric_kudler/react-navigator/src/index.js
@@ -103,9 +103,6 @@ class ControlPanel extends React.Component {
 	}
 }
 class Navigator extends React.Component {
-	constructor(props) {
-		super(props);
-	}
 	render() {
 		return (
 			<div>
@@ -121,23 +118,3 @@ ReactDOM.render(
 	<Navigator />,
 	document.getElementById('root')
 );
-
-function calculateWinner(squares) {
-	const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-	for (let i = 0; i < lines.length; i++) {
-		const [a, b, c] = lines[i];
-		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-			return squares[a];
-		}
-	}
-	return null;
-}
