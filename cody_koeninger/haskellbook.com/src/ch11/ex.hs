4b51e419ac0b02ce61bffec3ad8e465e62ad40b9
cody koeninger <cody@koeninger.org>
2018-01-17 16:59:42 -0600

    haskellbook p 452

--- cody_koeninger/haskellbook.com/src/ch11/ex.hs
+++ cody_koeninger/haskellbook.com/src/ch11/ex.hs
@@ -57,3 +57,46 @@ allLanguages = [Haskell, Agda, Idris, Purescript]
 
 allProgrammers :: [Programmer]
 allProgrammers = [Programmer o p | o <- allOperatingSystems, p <- allLanguages]
+
+data Quantum = Yes | No | Both deriving (Eq, Show)
+
+convert1 :: Quantum -> Bool
+convert1 Yes = False
+convert1 No = False
+convert1 Both = False
+
+convert2 :: Quantum -> Bool
+convert2 Yes = True
+convert2 No = False
+convert2 Both = False
+                
+convert3 :: Quantum -> Bool
+convert3 Yes = True
+convert3 No = True
+convert3 Both = False
+
+convert4 :: Quantum -> Bool
+convert4 Yes = True
+convert4 No = True
+convert4 Both = True
+
+convert5 :: Quantum -> Bool
+convert5 Yes = False
+convert5 No = True
+convert5 Both = True
+
+convert6 :: Quantum -> Bool
+convert6 Yes = False
+convert6 No = False
+convert6 Both = True
+
+convert7 :: Quantum -> Bool
+convert7 Yes = True
+convert7 No = False
+convert7 Both = True
+
+convert8 :: Quantum -> Bool
+convert8 Yes = False
+convert8 No = True
+convert8 Both = False
+                
