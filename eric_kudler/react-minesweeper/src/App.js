dc6828b56628e88bc2b6dce1d3bc4792106495ad
Eric Kudler <joeento@gmail.com>
2018-01-29 12:12:27 -0500

    Setup some basic wrapper classes

--- eric_kudler/react-minesweeper/src/App.js
+++ eric_kudler/react-minesweeper/src/App.js
@@ -2,20 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class Board extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      Board
       </div>
     );
   }
 }
+class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+      Header
+      </div>
+    );
+  }
+}
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Board />
+      </div>
+    );
+  }
+}
+class Minesweeper extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <Game />
+    );
+  }
+}
 
-export default App;
+export default Minesweeper;
--- eric_kudler/react-minesweeper/src/index.js
+++ eric_kudler/react-minesweeper/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Minesweeper from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Minesweeper />, document.getElementById('root'));
 registerServiceWorker();
