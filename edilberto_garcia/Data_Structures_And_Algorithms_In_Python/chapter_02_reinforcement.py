87193980226817ea21a7b6ebbdfdc9ebff6c947f
Edilberto Garcia <egarcia@federatedmedia.net>
2018-02-08 18:02:09 -0500

    Chapter 02 OOP Qs

--- edilberto_garcia/Data_Structures_And_Algorithms_In_Python/chapter_02_reinforcement.py
+++ edilberto_garcia/Data_Structures_And_Algorithms_In_Python/chapter_02_reinforcement.py
@@ -29,17 +29,33 @@
 # of each type.
 
 class Flower:
-	name = ''
-	petal_count = 0
-	price = 0.0
 
 	def __init__(self, name, petal_count, price):
-		self.name = name
-		self.petal_count = petal_count
-		self.price = price
+		self._name = name
+		self._petal_count = petal_count
+		self._price = price
+	
+	def set_name(self, name):
+		self._name = name
 
+	def set_petal_count(self, petal_count):
+		self._petal_count = petal_count
+
+	def set_price(self, price):
+			self._price = price
+
+	def get_name(self):
+		return self._name
+
+	def get_petal_count(self):
+			return self._petal_count
+
+	def get_price(self):
+			return self._price
 
 # *************************************************** #
+
+
 # *************************************************** #
 # *************************************************** #
 # *************************************************** #
