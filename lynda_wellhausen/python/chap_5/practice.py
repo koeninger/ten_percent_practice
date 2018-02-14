052370075a611058864ee8a02361733620518439
lynda <lwellhausen@kixer.com>
2018-02-14 17:40:49 -0600

    simple practice problem using range

---
+++ lynda_wellhausen/python/chap_5/practice.py
@@ -0,0 +1,27 @@
+def divisors():
+    inpt = int(input("Enter top of range"))
+    x = range(1, inpt)
+    for elem in x:
+        print elem
+
+# >>> from practice import divisors
+# >>> divisors()
+# Enter top of range4
+# 1
+# 2
+# 3
+# >>> 11
+# 11
+# >>> divisors()
+# Enter top of range11
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+# >>>
