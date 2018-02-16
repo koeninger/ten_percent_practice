dd87195063160a73c449d95c2e1b71c5da583840
Joel Jimenez <joel.jimenez@dedicatedmedia.com>
2018-02-15 19:32:01 -0800

    R Splines p293

--- joel_jimenez/r/chapter_seven.r
+++ joel_jimenez/r/chapter_seven.r
@@ -35,3 +35,32 @@ fit.5 = lm(wage~poly(age, 5), data=Wage)
 anova(fit.1, fit.2, fit.3, fit.4, fit.5)
 coef( summary(fit.5) )
 (-11.983)^2
+
+fit.1 = lm(wage~education + age, data=Wage)
+fit.2 = lm(wage~education + poly(age, 2), data=Wage)
+fit.3 = lm(wage~education + poly(age, 3), data=Wage)
+anova(fit.1, fit.2, fit.3)
+
+fit = glm( I(wage > 250)~poly(age, 4), data=Wage, family=binomial)
+preds = predict(fit, newdata=list(age=age.grid), se=T)
+pfit = exp(preds$fit) / (1 + exp(preds$fit))
+se.bands.logit = cbind(preds$fit + 2 * preds$se.fit, preds$fit - 2 * preds$se.fit)
+se.bands = exp(se.bands.logit) / (1 + exp(preds$fit))
+
+preds = predict(fit, newdata=list(age=age.grid), type="response", se=T)
+plot(age, I(wage>250), xlim=agelims, type="n", ylim=c(0,.2))
+points(jitter(age), I( (wage>250)/5 ), cex=.5, pch="|", col="darkgrey")
+matlines(age.grid, se.bands, lwd=1, col="blue", lty=3)
+
+table( cut(age, 4) )
+fit = lm(wage~cut(age, 4), data=Wage)
+coef( summary(fit) )
+
+# 7.8.2 Splines p293
+library(splines)
+fit = lm(wage~bs(age, knots=c(25, 40, 60)), data=Wage)
+pred = predict(fit, newdata=list(age=age.grid), se=T)
+plot(age, wage, col="gray")
+lines(age.grid, pred$fit, lwd=2)
+lines(age.grid, pred$fit + 2 * pred$se, lty="dashed")
+lines(age.grid, pred$fit - 2 * pred$se, lty="dashed")
