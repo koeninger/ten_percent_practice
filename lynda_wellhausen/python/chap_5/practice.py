d71358b1429bfd64a42ebf316cfd0544f3a7a22d
lynda <lwellhausen@kixer.com>
2018-02-19 13:53:00 -0600

    continue onto ch 6 - strings

--- lynda_wellhausen/python/chap_5/practice.py
+++ lynda_wellhausen/python/chap_5/practice.py
@@ -59,8 +59,8 @@ ValueError: could not convert string to float: Testing
 >>>
 
 # use python -m pdb x.py to load and debug a module. 
-# python -m pdb testmodule.py outs the following: 
-# s to step through, p <name of value> to see a 
+# python -m pdb testmodule.py outputs the following: 
+# s to step through, p <name of value> to see a (from testmodule.py)
 
 > /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(10)makelist()
 -> for i in range(1,20):
@@ -78,3 +78,88 @@ appending 18 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
 -> print "appending", i, ":", a
 (Pdb) p a
 [1, 2, 3, 4, 5, 6]
+
+# Further practice with debugging with PDB from the command line
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(11)makelist()
+-> a.append(i)
+(Pdb) s
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(12)makelist()
+-> print "appending", i, ":", a
+(Pdb) p a
+[1, 2, 3, 4]
+(Pdb) n
+appending 4 : [1, 2, 3, 4]
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(10)makelist()
+-> for i in range(1,20):
+(Pdb) n
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(11)makelist()
+-> a.append(i)
+(Pdb)
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(12)makelist()
+-> print "appending", i, ":", a
+(Pdb) n
+appending 5 : [1, 2, 3, 4, 5]
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(10)makelist()
+-> for i in range(1,20):
+(Pdb) l
+  5     def quadcube (x):
+  6         return x**2, x**3
+  7
+  8     def makelist():
+  9         a = []
+ 10  ->     for i in range(1,20):
+ 11             a.append(i)
+ 12             print "appending", i, ":", a
+ 13         return a
+ 14     makelist()
+ 15
+(Pdb) c
+appending 6 : [1, 2, 3, 4, 5, 6]
+appending 7 : [1, 2, 3, 4, 5, 6, 7]
+appending 8 : [1, 2, 3, 4, 5, 6, 7, 8]
+appending 9 : [1, 2, 3, 4, 5, 6, 7, 8, 9]
+appending 10 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+appending 11 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+appending 12 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+appending 13 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+appending 14 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+appending 15 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+appending 16 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+appending 17 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+appending 18 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+appending 19 : [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+The program finished and will be restarted
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(1)<module>()
+-> def iterquad ():
+(Pdb) a
+(Pdb) s
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(5)<module>()
+-> def quadcube (x):
+(Pdb) s
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(8)<module>()
+-> def makelist():
+(Pdb) s
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(14)<module>()
+-> makelist()
+(Pdb) s
+--Call--
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(8)makelist()
+-> def makelist():
+(Pdb) s
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(9)makelist()
+-> a = []
+(Pdb) a
+(Pdb) a
+(Pdb) s
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(10)makelist()
+-> for i in range(1,20):
+(Pdb) a
+(Pdb) b 11
+Breakpoint 1 at /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py:11
+(Pdb) c
+> /Users/wellhausen/Documents/ten_percent_practice/lynda_wellhausen/python/chap_5/testmodule.py(11)makelist()
+-> a.append(i)
+(Pdb) p a
+[]
+(Pdb)
+
---
+++ lynda_wellhausen/python/chap_6/practice.py
@@ -0,0 +1,50 @@
+# string practice
+
+>>> path = "C:\\Applications"
+>>> print path
+C:\Applications
+
+>>> path = r"C:\Applications"
+>>> path
+'C:\\Applications'
+>>> print path
+C:\Applications
+
+>> cheese="cheese available:\nblue\nchedder\nboursin"
+>>> cheese
+'cheese available:\x08lue\\chedder\x08oursin'
+
+>>> print cheese
+cheese available:
+blue
+chedder
+boursin
+
+
+>>> a="a"
+>>> b="b"
+>>> a + b
+'ab'
+>>> c="c"*3
+>>> c
+'ccc'
+>>> "5" *6
+'555555'
+>>> int(5)*6
+30
+>>> m = "mississippi"
+>>> m.count('s')
+4
+>>> string = "the quick brown fox"
+>>> 'goose' in string
+False
+>>> 'quick' in string
+True
+>>> string.endswith('fox')
+True
+>>> str('2345')
+'2345'
+>>> x="TESTING"
+>>> x.lower()
+'testing'
+
