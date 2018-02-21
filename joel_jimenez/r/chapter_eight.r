a31f7afff8aaf7fb76fe5a0945e5b4ad25a36b3a
Joel Jimenez <joel@MacBook-Pro-4.local>
2018-02-20 20:22:06 -0800

    R Fitting Regression Trees p327

--- joel_jimenez/r/chapter_eight.r
+++ joel_jimenez/r/chapter_eight.r
@@ -22,3 +22,49 @@ tree.carseats = tree(High~.-Sales, Carseats, subset=train)
 tree.pred = predict(tree.carseats, Carseats.test, type="class")
 table(tree.pred, High.test)
 (86 + 57) / 200
+
+set.seed(3)
+cv.carseats = cv.tree(tree.carseats, FUN=prune.misclass)
+names(cv.carseats)
+cv.carseats
+
+par( mfrow=c(1, 2) )
+plot(cv.carseats$size, cv.carseats$dev, type="b")
+plot(cv.carseats$k, cv.carseats$dev, type="b")
+
+prune.carseats = prune.misclass(tree.carseats, best=9)
+plot(prune.carseats)
+text(prune.carseats, pretty=0)
+tree.pred = predict(prune.carseats, Carseats.test, type="class")
+table(tree.pred, High.test)
+(94+60) / 200
+
+prune.carseats = prune.misclass(tree.carseats, best=15)
+plot(prune.carseats)
+text(prune.carseats, pretty=0)
+tree.pred = predict(prune.carseats, Carseats.test, type="class")
+table(tree.pred, High.test)
+(86+62) / 200
+
+
+# 8.3.2 Fitting Regression Trees p327
+library(MASS)
+set.seed(1)
+train = sample(1:nrow(Boston), nrow(Boston)/2)
+tree.boston = tree(medv~., Boston, subset=train)
+summary(tree.boston)
+
+plot(tree.boston)
+text(tree.boston, pretty=0)
+cv.boston = cv.tree(tree.boston)
+plot(cv.boston$size, cv.boston$dev, type='b')
+
+prune.boston = prune.tree(tree.boston, best=5)
+plot(prune.boston)
+text(prune.boston, pretty=0)
+
+yhat = predict(tree.boston, newdata=Boston[-train,])
+boston.test = Boston[-train, "medv"]
+plot(yhat, boston.test)
+abline(0, 1)
+mean( (yhat - boston.test)^2 )
