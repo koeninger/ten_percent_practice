d1e7611ff9198cf2bac95a1a44fdacda7f771341
Joel Jimenez <joel.jimenez@dedicatedmedia.com>
2018-02-21 19:33:08 -0800

    R Bagging, Random Forests and Boosting p329

--- joel_jimenez/r/chapter_eight.r
+++ joel_jimenez/r/chapter_eight.r
@@ -68,3 +68,45 @@ boston.test = Boston[-train, "medv"]
 plot(yhat, boston.test)
 abline(0, 1)
 mean( (yhat - boston.test)^2 )
+
+
+# 8.3.3 Bagging and Random Forests p328
+library(randomForest)
+set.seed(1)
+bag.boston = randomForest(medv~., data=Boston, subset=train, mtry=13, importance=TRUE)
+bag.boston
+
+yhat.bag = predict(bag.boston, newdata=Boston[-train,])
+plot(yhat.bag, boston.test)
+abline(0, 1)
+mean( (yhat.bag - boston.test)^2 )
+
+bag.boston = randomForest(medv~., data=Boston, subset=train, mtry=13, ntree=25)
+yhat.bag = predict(bag.boston, newdata=Boston[-train,])
+mean( (yhat.bag - boston.test)^2 )
+
+set.seed(1)
+rf.boston = randomForest(medv~., data=Boston, subset=train, mtry=6, importance=TRUE)
+yhat.rf = predict(rf.boston, newdata=Boston[-train,])
+mean( (yhat.rf - boston.test)^2 )
+
+importance(rf.boston)
+varImpPlot(rf.boston)
+
+
+# 8.3.4 Boosting p330
+library(gbm)
+set.seed(1)
+boost.boston = gbm(medv~., data=Boston[train,], distribution="gaussian", n.trees=5000, interaction.depth=4)
+summary(boost.boston)
+
+par(mfrow=c(1, 2))
+plot(boost.boston, i="rm")
+plot(boost.boston, i="lstat")
+
+yhat.boost = predict(boost.boston, newdata=Boston[-train,], n.trees=5000)
+mean( (yhat.boost - boston.test)^2 )
+
+boost.boston = gbm(medv~., data=Boston[train,], distribution="gaussian", n.trees=5000, interaction.depth=4, shrinkage=0.2, verbose=F)
+yhat.boost = predict(boost.boston, newdata=Boston[-train,], n.trees=5000)
+mean( (yhat.boost - boston.test)^2 )
