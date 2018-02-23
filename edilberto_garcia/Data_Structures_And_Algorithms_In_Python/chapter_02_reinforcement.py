84ae555be048d4c75db270a391da80c1851ced00
Edilberto Garcia <egarcia@federatedmedia.net>
2018-02-23 16:48:03 -0500

    Chapter 2 vector question

--- edilberto_garcia/Data_Structures_And_Algorithms_In_Python/chapter_02_reinforcement.py
+++ edilberto_garcia/Data_Structures_And_Algorithms_In_Python/chapter_02_reinforcement.py
@@ -1,5 +1,3 @@
-import 'book_samples/ch02.credit_card'
-
 # R-2.1 Give three examples of life-critical software applications.
 
     # - Medical
@@ -124,35 +122,47 @@ def __init__(self, customer, bank, acnt, limit, balance = 0):
 
 # R-2.8 Modify the declaration of the first for loop in the CreditCard tests, from
 # Code Fragment 2.3, so that it will eventually cause exactly one of the three
-# credit cards to go over its credit limit. Which credit card is it?
+# credit cards to go over a credit limit. Which credit card is it?
 
-if __name__ == '__main__' :
-    wallet = []
-    wallet.append(CreditCard('John Bowman', 'California Savings',
-    '5391 0375 9387 5309', 2500) )
-    wallet.append(CreditCard('John Bowman', 'California Federal',
-    '3485 0399 3395 1954', 3500) )
-    wallet.append(CreditCard('John Bowman', 'California Finance',
-    '5391 0375 9387 5309', 5000) )
+# if __name__ == '__main__' :
+#     wallet = []
+#     wallet.append(CreditCard('John Bowman', 'California Savings',
+#     '5391 0375 9387 5309', 2500) )
+#     wallet.append(CreditCard('John Bowman', 'California Federal',
+#     '3485 0399 3395 1954', 3500) )
+#     wallet.append(CreditCard('John Bowman', 'California Finance',
+#     '5391 0375 9387 5309', 5000) )
 
-    for val in range(1, 17):
-        wallet[0].charge(val)
-        wallet[1].charge(2*val)
-        wallet[2].charge(3*val)
+#     for val in range(1, 17):
+#         wallet[0].charge(val)
+#         wallet[1].charge(2*val)
+#         wallet[2].charge(3*val)
 
-    for c in range(3):
-        print('Customer =', wallet[c].get_customer())
-        print('Bank =', wallet[c].get_bank())
-        print('Account =', wallet[c].get_account( ))
-        print('Limit =', wallet[c].get_limit( ))
-        print('Balance =', wallet[c].get_balance())
-        while wallet[c].get_balance() > 100:
-            wallet[c].make_payment(100)
-            print('New balance =', wallet[c].get_balance())
-        print()
+#     for c in range(3):
+#         print('Customer =', wallet[c].get_customer())
+#         print('Bank =', wallet[c].get_bank())
+#         print('Account =', wallet[c].get_account( ))
+#         print('Limit =', wallet[c].get_limit( ))
+#         print('Balance =', wallet[c].get_balance())
+#         while wallet[c].get_balance() > 100:
+#             wallet[c].make_payment(100)
+#             print('New balance =', wallet[c].get_balance())
+#         print()
 
 
 # *************************************************** #
+
+# R-2.9 Implement the __sub__ method for the Vector class of Section 2.3.3, so
+# that the expression u-v returns a new vector instance representing the
+# difference between two vectors.
+
+
+
+
+# 
+# 
+# 
+# 
 # *************************************************** #
 # *************************************************** #
 # *************************************************** #
