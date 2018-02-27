c2a292f1515f8fb3aea4685d7c0dc93174cc74aa
Joel Jimenez <joel.jimenez@dedicatedmedia.com>
2018-02-26 19:34:41 -0800

    R Support Vector Machine p363

--- joel_jimenez/r/chapter_nine.r
+++ joel_jimenez/r/chapter_nine.r
@@ -46,3 +46,33 @@ plot(svmfit, dat)
 svmfit = svm(y~., data=dat, kernel="linear", cost=1)
 summary(svmfit)
 plot(svmfit, dat)
+
+
+# Support Vector Machine p363
+set.seed(1)
+x = matrix(rnorm(200*2), ncol=2)
+x[1:100,] = x[1:100,] + 2
+x[101:150,] = x[101:150,] - 2
+y = c(rep(1, 150), rep(2, 50))
+dat = data.frame(x=x, y=as.factor(y))
+
+plot( x, col=y)
+train = sample(200, 100)
+svmfit = svm(y~., data=dat[train,], kernel="radial", gamma=1, cost=1)
+plot(svmfit, dat[train,])
+summary(svmfit)
+
+svmfit = svm(y~., data=dat[train,], kernel="radial", gamma=1, cost=1e5)
+plot(svmfit, dat[train,])
+set.seed(1)
+tune.out = tune(svm, y~., data=dat[train,], kernel="radial", ranges=list(cost=c(0.1, 1, 10, 100,1000), gamma=c(0.5, 1, 2, 3, 4)))
+summary(tune.out)
+
+
+# ROC Curves
+library(ROCR)
+rocplot = function(){
+	predob = prediction(pred, truth)
+	perf = performance(predob, "tpr", "fpr")
+	plot(perf)
+}
