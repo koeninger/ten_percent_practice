33813dab188a7af68667afa1203f8d63ca405fa7
Shlomo Mounitz <shlomo.mounitz@lin-digital.com>
2018-02-27 22:57:56 -0500

    Chapter 3

---
+++ shlomo_mounitz/Python_Programming/Chapters/Ch03/sphere.py
@@ -0,0 +1,10 @@
+import math
+
+def main():
+    print("This program calculates the volume and surface are of a sphere")
+    radius = int(input("What is the sphere radius? "))
+    volume = 4/3 * math.pi * radius ** 3
+    area = 4 * math.pi * radius ** 2
+    print ("volume: {} \t surface area: {}".format(volume,area))
+
+main()
