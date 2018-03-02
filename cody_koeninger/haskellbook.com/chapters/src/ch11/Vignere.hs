38f9f40309293210fa60a3224c49f277462de48b
cody koeninger <cody@koeninger.org>
2018-03-02 16:55:22 -0600

    haskellbook p585

--- cody_koeninger/haskellbook.com/chapters/src/ch11/Vignere.hs
+++ cody_koeninger/haskellbook.com/chapters/src/ch11/Vignere.hs
@@ -1,29 +1,47 @@
 module Vignere (Keyword, encode, decode) where
 
-    import Data.Char (toUpper, ord, chr)
-        
-    newtype Keyword = Keyword String deriving (Eq, Show)
+import Data.Char (toUpper, ord, chr)
+import Test.QuickCheck
 
-    start = ord 'A'
+newtype Keyword = Keyword String deriving (Eq, Show)
 
-    shift :: (Char, Int) -> Char
-    shift (c, n) = chr $ start + mod ((ord c - start) + n) 26
+start = ord 'A'
 
-    unshift :: (Char, Int) -> Char
-    unshift (c, n) = shift (c, -n)
+shift :: (Char, Int) -> Char
+shift (c, n) = chr $ start + mod ((ord c - start) + n) 26
 
-    pairs :: Keyword -> String -> [(Char, Int)]
-    pairs (Keyword k) s =
-        let
-            ku = map toUpper k
-            su = map toUpper s
-            shifts = map (\c -> ord c - start) ku
-        in zip su $ cycle shifts
-                               
-    encode :: Keyword -> String -> String
-    encode kw s =
-        map shift $ pairs kw s
-                 
-    decode :: Keyword -> String -> String
-    decode kw s =
-        map unshift $ pairs kw s
+unshift :: (Char, Int) -> Char
+unshift (c, n) = shift (c, -n)
+
+pairs :: Keyword -> String -> [(Char, Int)]
+pairs (Keyword k) s =
+    let
+        ku = map toUpper k
+        su = map toUpper s
+        shifts = map (\c -> ord c - start) ku
+    in zip su $ cycle shifts
+                           
+encode :: Keyword -> String -> String
+encode kw s =
+    map shift $ pairs kw s
+             
+decode :: Keyword -> String -> String
+decode kw s =
+    map unshift $ pairs kw s
+
+genAz :: Gen String
+genAz = listOf1 $ elements ['A'..'Z']
+
+newtype AZString = AZString String deriving (Eq, Show)
+
+instance Arbitrary AZString where
+  arbitrary = AZString <$> genAz
+
+instance Arbitrary Keyword where
+  arbitrary = Keyword <$> genAz
+
+propRoundtrip :: Keyword -> AZString -> Bool
+propRoundtrip k (AZString s) = s == (decode k (encode k s))
+
+runChecks = do
+  quickCheck propRoundtrip
