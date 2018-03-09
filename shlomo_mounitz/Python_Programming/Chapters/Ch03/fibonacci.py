370b69c4050816a5622b6562c38ec3caa74f7c97
Shlomo Mounitz <shlomo.mounitz@lin-digital.com>
2018-03-09 11:14:30 -0500

    fix: lower fibonacci numbers

--- shlomo_mounitz/Python_Programming/Chapters/Ch03/fibonacci.py
+++ shlomo_mounitz/Python_Programming/Chapters/Ch03/fibonacci.py
@@ -2,11 +2,11 @@ def main():
     print("This program calculates the nth Fibonacci number.")
     nth = int(input("Which nth? "))
     
-    n1 = 1
-    n2 = 1
-    n3 = 2
+    n1 = 0
+    n2 = 0
+    n3 = 1
     
-    for i in range(3,nth):
+    for i in range(1,nth):
         n1 = n2
         n2 = n3
         n3 = n1 + n2
