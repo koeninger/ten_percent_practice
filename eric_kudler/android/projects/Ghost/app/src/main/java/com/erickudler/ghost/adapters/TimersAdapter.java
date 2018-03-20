eaf975f930e43bd26baf80d1f62ff03221105e43
Eric Kudler <joeento@gmail.com>
2018-03-20 10:59:41 -0400

    Adds first pass at a RecycleView for list elements

---
+++ eric_kudler/android/projects/Ghost/app/src/main/java/com/erickudler/ghost/adapters/TimersAdapter.java
@@ -0,0 +1,56 @@
+package com.erickudler.ghost.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.erickudler.ghost.R;
+
+public class TimersAdapter extends RecyclerView.Adapter<TimersAdapter.ViewHolder> {
+    private String[] mDataset;
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView mTextView;
+        public ViewHolder(TextView v) {
+            super(v);
+            mTextView = v;
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public TimersAdapter() {
+        mDataset = new String[] {"Getting Up", "Driving to Work", "Picking up Lunch", "Going to Bed"};
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public TimersAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        // create a new view
+        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.timer_view, parent, false);
+
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+}
