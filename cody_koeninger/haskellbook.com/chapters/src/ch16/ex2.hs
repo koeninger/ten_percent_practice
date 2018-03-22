3f6baa3f13664fb6fe44306a3210af5690ee0e88
cody koeninger <cody@koeninger.org>
2018-03-22 17:17:33 -0500

    haskellbook p 683

--- cody_koeninger/haskellbook.com/chapters/src/ch16/ex2.hs
+++ cody_koeninger/haskellbook.com/chapters/src/ch16/ex2.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 
+import GHC.Arr
+
 -- wrong kind for Functor
 -- data Bool = False | True
 
@@ -18,3 +20,102 @@ instance Functor BoolAndMaybeSomethingElse where
 newtype Mu f = Inf { outF :: f (Mu f) }
 
 -- Mu is (* -> *) -> *, not * -> *
+
+data D = D (Array Word Word) Int Int
+
+-- D is *, not * -> *
+
+data Sum b a = First a | Second b
+
+instance Functor (Sum e) where
+  fmap f (First a) = First (f a)
+  fmap f (Second b) = Second b
+
+data Company a c b = DeepBlue a c | Something b
+
+instance Functor (Company e e') where
+  fmap f (Something b) = Something (f b)
+  fmap _ (DeepBlue a c) = DeepBlue a c
+
+data More b a = L a b a | R b a b deriving (Eq, Show)
+
+instance Functor (More x) where
+  fmap f (L a b a') = L (f a) b (f a')
+  fmap f (R b a b') = R b (f a) b'
+
+data Quant a b
+  = Finance
+  | Desk a
+  | Bloor b
+
+instance Functor (Quant a) where
+  fmap _ Finance = Finance
+  fmap _ (Desk x) = Desk x
+  fmap f (Bloor x) = Bloor (f x)
+
+data K' a b = K' a
+
+instance Functor (K' a) where
+  fmap _ (K' x) = K' x
+
+newtype Flip f a b = Flip (f b a) deriving (Eq, Show)
+
+newtype K a b = K a
+
+instance Functor (Flip K a) where
+  fmap f (Flip (K x)) = Flip (K (f x))
+
+data EvilGoateeConst a b = GoatyConst b
+
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst x) = GoatyConst (f x)
+
+data LiftItOut f a = LiftItOut (f a)
+
+instance (Functor f) => Functor (LiftItOut f) where
+  fmap fn (LiftItOut x) = LiftItOut (fmap fn x)
+
+data Parappa f g a = DaWrappa (f a) (g a)
+
+instance (Functor f, Functor g) => Functor (Parappa f g) where
+  fmap fn (DaWrappa x y) = (DaWrappa (fmap fn x) (fmap fn y))
+
+data IgnoreOne f g a b = IgnoringSomething (f a) (g b)
+
+instance (Functor g) => Functor (IgnoreOne f g a) where
+  fmap fn (IgnoringSomething x y) = IgnoringSomething x (fmap fn y)
+
+data Notorious g o a t = Notorious (g o) (g a) (g t)
+
+instance (Functor g) => Functor (Notorious g o a) where
+  fmap fn (Notorious x y z) = Notorious x y (fmap fn z)
+
+data List a
+  = Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons x y) = Cons (f x) (fmap f y)
+
+data GoatLord a
+  = NoGoat
+  | OneGoat a
+  | MoreGoats (GoatLord a) (GoatLord a) (GoatLord a)
+
+instance Functor GoatLord where
+  fmap _ NoGoat = NoGoat
+  fmap f (OneGoat x) = OneGoat (f x)
+  fmap f (MoreGoats x y z) = MoreGoats (fmap f x) (fmap f y) (fmap f z)
+
+data TalkToMe a
+  = Halt
+  | Print String a
+  | Read (String -> a)
+
+instance Functor TalkToMe where
+  fmap _ Halt = Halt
+  fmap f (Print s x) = Print s (f x)
+--  fmap f (Read r) = Read (\s -> f (r s))
+  fmap f (Read r) = Read $ fmap f r
