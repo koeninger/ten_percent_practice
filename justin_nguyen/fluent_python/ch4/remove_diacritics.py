f1fb33fe8d92575b1a135ed373feb8f55a049acb
Justin Nguyen <jnguyen@kixer.com>
2018-03-26 19:22:53 -0500

    ore unicode work

---
+++ justin_nguyen/fluent_python/ch4/remove_diacritics.py
@@ -0,0 +1,22 @@
+import unicodedata
+import string
+
+def shave_marks(txt):
+    norm_txt = unicodedata.normalize('NFD', txt)
+    shaved = ''.join(c for c in norm_txt if not unicodedata.combining(c))
+    return unicodedata.normalize('NFC', shaved)
+
+
+def shave_marks_latin(txt):
+    norm_txt = unicodedata.normalize('NFD', txt)
+    latin_base = False
+    keepers = []
+    for c in norm_txt:
+        if unicodedata.combining(c) and latin_base:
+            continue # ignore diacritics on latin base chars
+        keepers.append(c)
+        # if not combining, it's new
+        if not unicodedata.combining(c):
+            latin_base = c in string.ascii_letters
+    shaved = ''.join(keepers)
+    return unicodedata.normalize('NFC', shaved)
