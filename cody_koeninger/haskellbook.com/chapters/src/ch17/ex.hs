392be1b52ea1928e51425f525224f91719067411
cody koeninger <cody@koeninger.org>
2018-03-28 16:50:30 -0500

    haskellbook p736

--- cody_koeninger/haskellbook.com/chapters/src/ch17/ex.hs
+++ cody_koeninger/haskellbook.com/chapters/src/ch17/ex.hs
@@ -1,5 +1,9 @@
 import Control.Applicative
 import Data.List (elemIndex)
+import Data.Monoid
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
 
 added :: Maybe Integer
 added = (+3) <$> (lookup 3 $ zip [1,2,3] [4,5,6])
@@ -57,3 +61,41 @@ instance Functor (Constant a) where
 instance Monoid a => Applicative (Constant a) where
   pure x = Constant mempty
   (Constant x) <*> (Constant y) = Constant (mappend x y)
+
+fu1 = const <$> Just "Hello" <*> pure "World"
+
+fu2 = (,,,) <$> Just 90 <*> Just 10 <*> Just "Tierness" <*> pure [1,2,3]
+
+data List a
+  = Nil
+  | Cons a (List a)
+  deriving (Eq, Show)
+
+instance Functor List where
+  fmap _ Nil = Nil
+  fmap f (Cons x r) = Cons (f x) (fmap f r)
+
+instance Monoid (List a) where
+  mempty = Nil
+  mappend Nil r = r
+  mappend (Cons x y) r = Cons x (mappend y r)
+
+instance Foldable List where
+  foldMap _ Nil = mempty
+  foldMap f (Cons x y) = mappend (f x) (foldMap f y)
+
+instance Applicative List where
+  pure x = Cons x Nil
+  fs <*> xs = foldMap (\f -> fmap f xs) fs
+
+instance (Arbitrary a) => Arbitrary (List a) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    frequency [(1, return Nil)
+              ,(2, return $ Cons a b)]
+
+instance (Eq a) => EqProp (List a) where (=-=) = eq
+
+checkAll :: IO ()
+checkAll = quickBatch (applicative (Cons ("_", "_", 1 :: Integer) Nil))
