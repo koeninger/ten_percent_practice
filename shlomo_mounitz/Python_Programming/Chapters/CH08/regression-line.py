8e1b3ca826816ef1d11e0a17e34a620fb4d6500d
Shlomo Mounitz <shlomo.mounitz@lin-digital.com>
2018-03-28 23:56:40 -0400

    more regression line

--- shlomo_mounitz/Python_Programming/Chapters/CH08/regression-line.py
+++ shlomo_mounitz/Python_Programming/Chapters/CH08/regression-line.py
@@ -4,12 +4,33 @@ def main():
     win = GraphWin("Regression Line")
     win.setCoords(0,0,10,10)
     r = drawButton(win, Point(9,1), Point(10,2))
+    count = 0
+    xTotal = 0
+    yTotal = 0
+    xyTotal = 0
+    xSqTotal = 0
+    ySqTotal = 0
+
     while True:
         point = win.getMouse()
+
         if checkBounds(point, r):
             break
         point.draw(win)
+        count = count +1
+        xTotal = xTotal + point.getX()
+        yTotal = yTotal + point.getY()
+        xyTotal = xyTotal + point.getX() * point.getY()
+        xSqTotal = xSqTotal + point.getX() * point.getX()
+        ySqTotal = ySqTotal + point.getY() * point.getY()
+    
+    t = Text(Point(2,2), getY(count, xTotal, yTotal, xyTotal, xSqTotal, ySqTotal))
+    t.draw(win)
+    win.getMouse()
     win.close()
+
+def getY(count, xTotal, yTotal, xyTotal, xSqTotal, ySqTotal):
+    return (count * xyTotal - xTotal * yTotal) / (count * xSqTotal - xTotal * xTotal) * (count * ySqTotal - yTotal * yTotal)
 
 def drawButton(win, ll, ur):
     r = Rectangle(ll, ur)
