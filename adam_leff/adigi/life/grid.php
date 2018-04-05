4a4b44ef87ed6a1b81f8da49d619c49a747c69fa
Adam Leff <adam.leff@lin-digital.com>
2018-04-05 13:51:56 -0400

    subleq working

--- adam_leff/adigi/life/grid.php
+++ adam_leff/adigi/life/grid.php
@@ -14,13 +14,14 @@ class Grid
         for($i = 0; $i < self::SIZE; $i++){
             for($j = 0; $j < self::SIZE; $j++){
                 $this->_grid[$i][$j] = new Cell();
-                $this->_grid[$i][$j]->value = mt_rand(0,1) ? mt_rand(-30,31) : 0;
+                $this->_grid[$i][$j]->value = mt_rand(-30,31);
             }
         }
         return $this->_grid;
     }
     
     public function tick(){
+        $this->display();
         $this->subleq();
         $this->display();
     }
@@ -39,7 +40,6 @@ class Grid
     public function getMemoryAt($addr){
         $x = intval($addr/self::SIZE);
         $y = $addr % self::SIZE;
-        echo "coords " . $x . 'x' . $y . ": " . $this->_grid[$x][$y]->value . "\n";
         return $this->_grid[$x][$y]->value;
     }
     
@@ -50,22 +50,38 @@ class Grid
     }
     
     public function subleq(){
+        $i = 0;
         $a_addr = $this->getMemoryAt($this->_pc);
-        if($a_addr < 0){
+        while($a_addr < 0 && $i < (self::SIZE * self::SIZE)){
             $a_addr = $this->getMemoryAt($a_addr * -1);
         }
+        if($i == self::SIZE * self::SIZE){
+            $this->_pc = 0;
+            return;
+        }
+        $i = 0;
         $b_addr = $this->getMemoryAt($this->_pc + 1);
-        if($b_addr < 0){
+        while($b_addr < 0 && $i < (self::SIZE * self::SIZE)){
             $b_addr = $this->getMemoryAt($b_addr * -1);
+        }
+        if($i == self::SIZE * self::SIZE){
+            $this->_pc = 0;
+            return;
         }
         $a_val = $this->getMemoryAt($a_addr);
         $b_val = $this->getMemoryAt($b_addr);
+        
+        $i = 0;
         $c_val = $this->getMemoryAt($this->_pc + 2);
-        if($c_val < 0){
+        while($c_val < 0 && $i < (self::SIZE * self::SIZE)){
             $c_val = $this->getMemoryAt($c_val * -1);
         }
+        if($i == self::SIZE * self::SIZE){
+            $this->_pc = 0;
+            return;
+        }
         
-        echo "Instruction: " . $a_val . " " . $b_val . " "  . $c_val . "\n";
+        echo "PC: " . $this->_pc . " Addresses: " . $a_addr . " " . $b_addr . " Instruction: " . $a_val . " " . $b_val . " "  . $c_val . "\n";
         
         $b_val = $b_val - $a_val;
         $this->setMemoryAt($b_addr,$b_val);
