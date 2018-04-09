dbbd911829206480e6e29d59c1c21864580fa571
Eric Kudler <joeento@gmail.com>
2018-04-09 13:38:54 -0400

    Added some database setup to Ghost app

---
+++ eric_kudler/android/projects/Ghost/app/src/main/java/com/erickudler/ghost/data/GhostDbHelper.java
@@ -0,0 +1,35 @@
+package com.erickudler.ghost.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ekudler on 4/9/18.
+ */
+
+public class GhostDbHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "ghost.db";
+    private static final int DATABASE_VERSION = 1;
+
+    private GhostDbHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        final String SQL_CREATE_TIMER_TABLE = "CREATE TABLE " + GhostContract.TimerEntry.TABLE_NAME + " (" +
+                GhostContract.TimerEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                GhostContract.TimerEntry.COLUMN_NAME + " TEXT NOT NULL, " +
+                GhostContract.TimerEntry.COLUMN_NUM_OF_STEPS + " INTEGER NOT NULL, " +
+                GhostContract.TimerEntry.COLUMN_TIMESTAMP + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP" +
+                "); ";
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + GhostContract.TimerEntry.TABLE_NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
