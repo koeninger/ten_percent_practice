2b590066883a25313fd775fd6f5217ffa656e199
Joel Jimenez <joel.jimenez@dedicatedmedia.com>
2018-04-10 19:11:25 -0700

    Display created campaigns

--- joel_jimenez/blockchain/crowd_funding/pages/index.js
+++ joel_jimenez/blockchain/crowd_funding/pages/index.js
@@ -1,22 +1,54 @@
 import React, {Component} from 'react';
-// import 'bulma/css/bulma.css';
 import factory from '../ethereum/factory';
+import Layout from '../components/Layout';
+import { Title, Section, Card, CardContent, Button, Columns, Column, Field, Control, Input } from 'bloomer';
 
 class CampaignIndex extends Component {
-	static getInitialProps() {
-		
+	static async getInitialProps() {
+		const campaigns = await factory.methods.getCampaigns().call();
+		return { campaigns };
 	}
 
-	async componentDidMount(){
-		const campaigns = await factory.methods.getCampaigns().call();
-		console.log(campaigns);
+	renderCampaigns() {
+		const campaigns = this.props.campaigns.map(address => {
+			return (
+				<Card key={address}>
+					<CardContent>
+						<Title isSize={5}>{address}</Title>
+						<a>View campaign</a>
+					</CardContent>
+				</Card>
+			);
+		});
+
+		return campaigns;
 	}
 
 	render() {
 		return (
-			<div>
-				<h1>Campaigns</h1>
-			</div>
+			<Layout>
+				<Section style={{ paddingBottom: 0 }}>
+					<Field hasAddons>
+						<Control isExpanded>
+							<Input type="text" placeholder="Search Campaigns" isSize="large" />
+						</Control>
+						<Control>
+							<Button isColor="info" isSize="large">Search</Button>
+						</Control>
+					</Field>
+				</Section>
+				<Section style={{ paddingBottom: 0 }}>
+					<Columns>
+						<Column isSize={{tablet: 4}}>
+							<Title isSize={3}>Campaigns</Title>
+						</Column>
+						<Column>
+							<Button isColor="info">+ Create Campaign</Button>
+						</Column>
+					</Columns>
+				</Section>
+				<Section>{this.renderCampaigns()}</Section>
+			</Layout>
 		);
 	}
 }
