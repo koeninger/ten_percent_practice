e4f542820e6037475aad54f59ffef696ad9e3c8b
Justin Nguyen <jnguyen@kixer.com>
2018-04-13 17:20:36 -0500

    done w/ ch5, start ch6

---
+++ justin_nguyen/fluent_python/ch6/strat.py
@@ -0,0 +1,66 @@
+from abc import ABC, abstractmethod
+from collections import namedtuple
+
+Customer = namedtuple('Customer', 'name fidelity')
+
+class LineItem:
+
+    def __init__(self, product, quantity, price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+
+    def total(self):
+        return self.price * self.quantity
+
+class Order:
+
+    def __init__(self, customer, cart, promotion=None):
+        self.customer = customer
+        self.cart = list(cart)
+        self.promotion = promotion
+
+    def total(self):
+        if not hasattr(self, '__total'):
+            self.__total = sm(item.total() for item in self.cart)
+        return self.__total
+
+    def due(self):
+        if self.promotion is None:
+            discount = 0
+        else:
+            discount = self.promotion.discount(self)
+        return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<Order total: {:.2f} due: {:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+    class Promotion(ABC):
+
+        @abstractmethod
+        def discount(self, order):
+            """ return discount as a positive dollar amount"""
+
+    class FidelityPromo(Promotion):
+        """ 5% discount for customers with 1000 or more points"""
+        def discount(self, order):
+
+    class BulkItemPromo(Promotion):
+        """ 10% discount for each LineItem w/ 20+ units"""
+
+        def discount(self, order):
+            discount = 0
+            for item in order.cart:
+                if item.quantity >= 20:
+                    discount += item.total() * .1
+            return discount
+
+    class LargeOrderPromo(Promotion):
+        """ 7% discount for orders with 10+ distinct items"""
+
+        def discount(self, order):
+            distinct_items = {item.product for item in order.cart}
+            if len(distinct_items) >= 10:
+                return order.total() * .07
+            return 0
