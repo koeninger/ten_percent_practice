de30c8b26f68bec6340952f651d2343acfa02cde
Justin Nguyen <jnguyen@kixer.com>
2018-04-18 16:23:20 -0500

    using first class functions + strategy

--- justin_nguyen/fluent_python/ch6/strat.py
+++ justin_nguyen/fluent_python/ch6/strat.py
@@ -22,7 +22,7 @@ class Order:
 
     def total(self):
         if not hasattr(self, '__total'):
-            self.__total = sm(item.total() for item in self.cart)
+            self.__total = sum(item.total() for item in self.cart)
         return self.__total
 
     def due(self):
@@ -36,31 +36,32 @@ class Order:
         fmt = '<Order total: {:.2f} due: {:.2f}>'
         return fmt.format(self.total(), self.due())
 
-    class Promotion(ABC):
+class Promotion(ABC):
 
-        @abstractmethod
-        def discount(self, order):
-            """ return discount as a positive dollar amount"""
+    @abstractmethod
+    def discount(self, order):
+        """ return discount as a positive dollar amount"""
 
-    class FidelityPromo(Promotion):
-        """ 5% discount for customers with 1000 or more points"""
-        def discount(self, order):
+class FidelityPromo(Promotion):
+    """ 5% discount for customers with 1000 or more points"""
+    def discount(self, order):
+        return order.total() * .05 if order.customer.fidelity >= 1000 else 0
 
-    class BulkItemPromo(Promotion):
-        """ 10% discount for each LineItem w/ 20+ units"""
+class BulkItemPromo(Promotion):
+    """ 10% discount for each LineItem w/ 20+ units"""
 
-        def discount(self, order):
-            discount = 0
-            for item in order.cart:
-                if item.quantity >= 20:
-                    discount += item.total() * .1
-            return discount
+    def discount(self, order):
+        discount = 0
+        for item in order.cart:
+            if item.quantity >= 20:
+                discount += item.total() * .1
+        return discount
 
-    class LargeOrderPromo(Promotion):
-        """ 7% discount for orders with 10+ distinct items"""
+class LargeOrderPromo(Promotion):
+    """ 7% discount for orders with 10+ distinct items"""
 
-        def discount(self, order):
-            distinct_items = {item.product for item in order.cart}
-            if len(distinct_items) >= 10:
-                return order.total() * .07
-            return 0
+    def discount(self, order):
+        distinct_items = {item.product for item in order.cart}
+        if len(distinct_items) >= 10:
+            return order.total() * .07
+        return 0
