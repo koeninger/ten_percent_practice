0afa7487afae758ec330b2a271396174c3051582
cody koeninger <cody@koeninger.org>
2018-04-19 14:56:44 -0500

    haskellbook p 868

---
+++ cody_koeninger/haskellbook.com/chapters/src/ch22/ex.hs
@@ -0,0 +1,28 @@
+import Data.Char
+
+cap :: [Char] -> [Char]
+cap xs = map toUpper xs
+
+rev :: [Char] -> [Char]
+rev xs = reverse xs
+
+composed :: [Char] -> [Char]
+composed = cap . rev
+
+fmapped :: [Char] -> [Char]
+fmapped = cap <$> rev
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = (,) <$> cap <*> rev
+
+tupled' :: [Char] -> ([Char], [Char])
+tupled' = do
+  a <- cap
+  b <- rev
+  return (a, b)
+
+tupled'' :: [Char] -> ([Char], [Char])
+tupled'' =
+  cap >>= \a ->
+            rev >>= \b ->
+                      return (a, b)
