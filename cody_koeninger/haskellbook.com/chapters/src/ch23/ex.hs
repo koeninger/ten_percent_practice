d2daff213643267621550977f0084780c1531c68
cody koeninger <cody@koeninger.org>
2018-04-26 15:01:17 -0500

    haskellbook p 922

--- cody_koeninger/haskellbook.com/chapters/src/ch23/ex.hs
+++ cody_koeninger/haskellbook.com/chapters/src/ch23/ex.hs
@@ -1,4 +1,6 @@
 import System.Random
+import Control.Monad
+import qualified Control.Monad.Trans.State as TS
 
 data Die
   = DieOne
@@ -38,3 +40,59 @@ rollsCountLogged n g = reverse <$> go 0 0 g []
       | otherwise =
         let (die, nextGen) = randomR (1, 6) gen
         in go (sum + die) (count + 1) nextGen ((intToDie die) : log)
+
+newtype State s a = State { runState :: s -> (a, s) }
+
+instance Functor (State s) where
+  fmap f (State g) = State $ \s ->
+    let (a, s2) = g s
+    in (f a, s2)
+
+instance Applicative (State s) where
+  pure a = State $ \s ->
+    (a, s)
+
+  (State f) <*> (State g) = State $ \s ->
+    let (fab, s2) = f s
+        (a, s3) = g s2
+    in (fab a, s3)
+
+instance Monad (State s) where
+  return = pure
+
+  (State f) >>= g = State $ \s ->
+    let (a, s2) = f s
+        State h = g a
+    in h s2
+
+
+fizzbuzz :: Integer -> String
+fizzbuzz n | mod n 15 == 0 = "fizzbuzz"
+           | mod n 3 == 0 = "fizz"
+           | mod n 5 == 0 = "buzz"
+           | otherwise = show n
+
+fizzbuzzFromTo :: Integer -> Integer -> [String]
+fizzbuzzFromTo from to =
+  TS.execState (mapM_ addResult (enumFromThenTo to (to - 1) from)) []
+
+addResult :: Integer -> TS.State [String] ()
+addResult n = do
+  xs <- TS.get
+  let result = fizzbuzz n
+  TS.put (result : xs)
+
+get :: State s s
+get = State $ \s -> (s, s)
+
+put :: s -> State s ()
+put s = State $ \_ -> ((), s)
+
+exec :: State s a -> s -> s
+exec (State sa) s = snd $ sa s
+
+eval :: State s a -> s -> a
+eval (State sa) s = fst $ sa s
+
+modify :: (s -> s) -> State s ()
+modify fn = State $ \s -> ((), fn s)
---
+++ cody_koeninger/haskellbook.com/chapters/src/ch24/LearnParsers.hs
@@ -0,0 +1,27 @@
+module LearnParsers where
+
+import Text.Trifecta
+
+stop :: Parser a
+stop = unexpected "stop"
+
+one = char '1'
+
+one' = one >> stop
+
+oneTwo = char '1' >> char '2'
+
+oneTwo' = oneTwo >> stop
+
+testParse :: Parser Char -> IO ()
+testParse p =
+  print $ parseString p mempty "123"
+
+pNL s =
+  putStrLn ('\n' : s)
+
+main = do
+  pNL "stop:"
+  testParse stop
+  pNL "one:"
+  testParse one
