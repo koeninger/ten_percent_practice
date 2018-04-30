99e5b812fee3562aa200dd352216c28b896d39b0
jvelis <jvelis@kixer.com>
2018-04-30 10:14:01 -0500

    ch 4 - service and partial

--- juan_velis/angular_by_example/ch04/js/app.js
+++ juan_velis/angular_by_example/ch04/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout','WorkoutBuilder', 'mediaPlayer', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate']).
+angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'WorkoutBuilder', 'mediaPlayer', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate']).
 config(function ($routeProvider, $sceDelegateProvider) {
     $routeProvider.when('/start', { templateUrl: 'partials/start.html' });
     $routeProvider.when('/workout', { templateUrl: 'partials/workout.html', controller: 'WorkoutController' });
@@ -25,12 +25,28 @@ config(function ($routeProvider, $sceDelegateProvider) {
     $routeProvider.when('/builder/workouts/new', {
         templateUrl: 'partials/workoutbuilder/workout.html',
         leftNav: 'partials/workoutbuilder/left-nav-exercises.html',
-        topNav: 'partials/workoutbuilder/top-nav.html'
+        topNav: 'partials/workoutbuilder/top-nav.html',
+        controller: 'WorkoutDetailController',
+        resolve: {
+            selectedWorkout: ['WorkoutBuilderService', function (WorkoutBuilderService) {
+                return WorkoutBuilderService.startBuilding();
+            }],
+        }
     });
     $routeProvider.when('/builder/workouts/:id', {
         templateUrl: 'partials/workoutbuilder/workout.html',
         leftNav: 'partials/workoutbuilder/left-nav-exercises.html',
-        topNav: 'partials/workoutbuilder/top-nav.html'
+        controller: 'WorkoutDetailController',
+        topNav: 'partials/workoutbuilder/top-nav.html',
+        resolve: {
+            selectedWorkout: ['WorkoutBuilderService', '$route', '$location', function (WorkoutBuilderService, $route, $location) {
+                var workout = WorkoutBuilderService.startBuilding($route.current.params.id);
+                if (!workout) {
+                    $location.path('/builder/workouts');    //If the workout not found redirect to workout list
+                }
+                return workout;
+            }],
+        }
     });
     $routeProvider.when('/builder/exercises/new', { templateUrl: 'partials/workoutbuilder/exercise.html' });
     $routeProvider.when('/builder/exercises/:id', { templateUrl: 'partials/workoutbuilder/exercise.html' });
