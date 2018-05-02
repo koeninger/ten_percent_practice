9fd35d9895566b24001ebccdb5451b046484cb3c
Chris Larson <clarson@kixer.com>
2018-05-02 16:34:09 -0500

    ch5 wip

--- chris_larson/learning-scalajs/ch5-1.scala
+++ chris_larson/learning-scalajs/ch5-1.scala
@@ -19,3 +19,26 @@ maximize(50, 30)
 def logStart() = "=" * 50 + "\nStarting NOW\n" + "=" * 50
 val start: () => String = logStart
 println( start())
+
+def safeStringOp(s: String, f: String => String) = {
+  if (s != null) f(s) else s
+}
+def reverser(s: String) = s.reverse
+safeStringOp(null, reverser)
+safeStringOp("Ready", reverser)
+
+val doubler = (x: Int) => x * 2
+val doubled = doubler(22)
+
+val greeter = (name: String) => s"Hello, $name"
+val hi = greeter("World")
+
+val maximize = (a: Int, b: Int) => if (a > b) a else b
+maximize(84, 96)
+
+def start = () => "=" * 50 + "\nStarting NOW\n" + "=" * 50
+println(start())
+
+safeStringOp("Ready", (s: String) => s.reverse)
+safeStringOp(null, s => s.reverse)
+safeStringOp("Ready", s => s.reverse)
