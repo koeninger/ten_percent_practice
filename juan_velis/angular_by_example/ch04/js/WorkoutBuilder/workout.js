3c8799c1f02215498efd5e5fcf3482d1d88d27a5
jvelis <jvelis@kixer.com>
2018-05-02 14:59:12 -0500

    Using scope watch

--- juan_velis/angular_by_example/ch04/js/WorkoutBuilder/workout.js
+++ juan_velis/angular_by_example/ch04/js/WorkoutBuilder/workout.js
@@ -17,6 +17,20 @@ angular.module('WorkoutBuilder')
           WorkoutBuilderService.removeExercise(exercise);
       };
 
+      $scope.$watch('formWorkout.exerciseCount', function (newValue) {
+          if (newValue) {
+              newValue.$setValidity("count", $scope.workout.exercises.length > 0);
+          }
+      });
+
+      $scope.$watch('workout.exercises.length', function (newValue, oldValue) {
+          if (newValue != oldValue) {
+              $scope.formWorkout.exerciseCount.$dirty = true;
+              $scope.formWorkout.$setDirty();
+              $scope.formWorkout.exerciseCount.$setValidity("count", newValue > 0);
+          }
+      });
+
       //var restWatch = $scope.$watch('formWorkout.restBetweenExercise', function (newValue) {
       //    // Conversion logic courtesy http://stackoverflow.com/questions/596467/how-do-i-convert-a-number-to-an-integer-in-javascript
       //    if (newValue) {
