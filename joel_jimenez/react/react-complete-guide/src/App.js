ac38149332523272a5030d49a1c1fdf57ff8ed2e
Joel Jimenez <JJimenez@nexstardigital.com>
2018-05-08 22:29:34 -0700

    Include react-router for url path routing

--- joel_jimenez/react/react-complete-guide/src/App.js
+++ joel_jimenez/react/react-complete-guide/src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import Header from './Header';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <BrowserRouter>
         <Header />
-      </div>
+		{/* 
+			Account selection
+			List of transactions
+		*/}
+      </BrowserRouter>
     );
   }
 }
