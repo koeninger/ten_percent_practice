a7f9ec6d7f10dbff680d60919b7303619fd27f36
Joel Jimenez <JJimenez@nexstardigital.com>
2018-05-11 13:43:04 -0700

    Two-way binding between components

---
+++ joel_jimenez/react/react-complete-guide/src/UserInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Input } from 'semantic-ui-react';
+
+const UserInput = (props) => {
+	return (
+		<Input 
+			placeholder='User Input' 
+			onChange={props.change} 
+			value={props.username} 
+		/>
+	);
+};
+
+export default UserInput;
