390f016eee61529fa4e3e9598adce631c3d3c66c
Justin Nguyen <jnguyen@kixer.com>
2018-05-14 23:38:00 -0500

    decorators (lru + firstdispatch)

--- justin_nguyen/fluent_python/ch7/clockdecorator.py
+++ justin_nguyen/fluent_python/ch7/clockdecorator.py
@@ -4,7 +4,7 @@ def clock(func):
     def clocked(*args):
         t0 = time.perf_counter()
         result = func(*args)
-        elapsed = time.perf_counter()
+        elapsed = time.perf_counter() - t0
         name = func.__name__
         arg_str = ', '.join(repr(arg) for arg in args)
         print('[%0.8fs] %s(%s) -> %r' % (elapsed, name, arg_str, result))
