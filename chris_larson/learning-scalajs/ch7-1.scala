c0ef8ee6b8d8ec74c86e343cd53c21ea625acb98
Chris Larson <clarson@kixer.com>
2018-05-17 10:00:05 -0500

    ch7 wip

--- chris_larson/learning-scalajs/ch7-1.scala
+++ chris_larson/learning-scalajs/ch7-1.scala
@@ -9,3 +9,12 @@ val nums = collection.mutable.Buffer[Int]()
 for (i <- 1 to 10) nums += i 
 println(nums)
 val l = nums.toList
+
+val m =Map("AAPL" -> 597, "MSFT" -> 40)
+val b = m.toBuffer
+b trimStart 1
+b += ("GOOG" -> 521)
+val n = b.toMap
+b += ("GOOG" -> 521)
+val l = b.toList
+val s = b.toSet
