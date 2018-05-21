0190d9756199c28651865b5c39817f70d5975be5
Chris Larson <clarson@kixer.com>
2018-05-21 09:58:52 -0500

    ch7 wip

--- chris_larson/learning-scalajs/ch7-1.scala
+++ chris_larson/learning-scalajs/ch7-1.scala
@@ -18,3 +18,33 @@ val n = b.toMap
 b += ("GOOG" -> 521)
 val l = b.toList
 val s = b.toSet
+
+val b= Set.newBuilder[Char]
+b += 'h'
+b ++= List('e', 'l', 'l', 'o')
+val helloSet = b.result
+
+val colors = Array("red", "green", "blue")
+colors(0) = "purple"
+
+//println("Very purple: " + colors)
+val files = new java.io.File(".").listFiles
+val scala = files map (_.getName) filter(_ endsWith "scala")
+
+val inks = Seq('C', 'M', 'Y', 'K')
+
+val hi = "Hello, " ++ "worldy" take 12 replaceAll ( "w","W")
+
+def inc(i: Int): Stream[Int] = Stream.cons(i, inc(i+1))
+val s = inc(1)
+val l = s.take(5).toList
+
+def inc(head: Int): Stream[Int] = head #:: inc(head+1)
+inc(10).take(10).toList
+
+def to(head: Char, end:Char): Stream[Char] = (head > end) match {
+  case true => Stream.empty
+  case false => head #:: to((head+1).toChar, end)
+}
+
+val hexChars = to('A', 'F').take(20).toList
