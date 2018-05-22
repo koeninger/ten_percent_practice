88b8624d2600a877a89a4480c9429659c3550c2f
Chris Larson <clarson@kixer.com>
2018-05-22 09:46:31 -0500

    ch7 wip

--- chris_larson/learning-scalajs/ch7-1.scala
+++ chris_larson/learning-scalajs/ch7-1.scala
@@ -48,3 +48,29 @@ def to(head: Char, end:Char): Stream[Char] = (head > end) match {
 }
 
 val hexChars = to('A', 'F').take(20).toList
+
+var x: String = "Indeed"
+var a = Option(x)
+x = null
+var b = Option(x)
+println(s"a is defined? ${a.isDefined}")
+println(s"b is not defined? ${b.isEmpty}")
+def divide(amt: Double, divisor: Double): Option[Double] = {
+  if (divisor == 0) None
+  else Option(amt / divisor)
+}
+
+val legit = divide(5, 2)
+val illegit = divide(3, 0)
+
+val odds = List(1, 3, 5)
+val firstOdd = odds.headOption
+val evens = odds filter (_ % 2 == 0)
+val firstEven = evens.headOption
+
+val words = List("risible", "scavenger", "gist")
+val upperase = words find (w => w == w.toUpperCase)
+val lowercase = words find (w => w == w.toLowerCase)
+
+val filtered = lowercase filter (_ endsWith "ible") map (_.toUpperCase)
+val exactSize = filtered filter (_.size > 15) map (_.size)
