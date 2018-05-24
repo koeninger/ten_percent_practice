09ad7fb58f6b431bb587a72b141da40918f035dd
Chris Larson <clarson@kixer.com>
2018-05-24 10:05:57 -0500

    ch7  wip

--- chris_larson/learning-scalajs/ch7-2.scala
+++ chris_larson/learning-scalajs/ch7-2.scala
@@ -20,3 +20,15 @@ def loopAndFail(end: Int, failAt: Int): Int = {
 loopAndFail(10, 3)
 val t1 = util.Try( loopAndFail(2, 3) )
 val t2 = util.Try( loopAndFail(4, 2) )
+
+def nextError = util.Try{ 1 / util.Random.nextInt(2) }
+val x = nextError
+val y = nextError
+
+nextError flatMap { _ => nextError }
+nextError foreach(x => println("success!" + x))
+nextError getOrElse 0
+nextError orElse nextError
+nextError.toOption
+nextError map (_ * 2)
+nextError match { case util.Success(x) => x; case util.Failure(error) => -1 }
