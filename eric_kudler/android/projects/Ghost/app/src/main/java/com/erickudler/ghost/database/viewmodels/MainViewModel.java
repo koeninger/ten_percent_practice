d7a45d33fdeeaf36cb37383e9336cd92eefa8847
Eric Kudler <joeento@gmail.com>
2018-05-30 16:40:15 -0400

    Forgot the MainViewModel Class

---
+++ eric_kudler/android/projects/Ghost/app/src/main/java/com/erickudler/ghost/database/viewmodels/MainViewModel.java
@@ -0,0 +1,33 @@
+package com.erickudler.ghost.database.viewmodels;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.util.Log;
+
+import com.erickudler.ghost.database.Timer;
+import com.erickudler.ghost.database.accessors.AppDatabase;
+
+import java.util.List;
+
+/**
+ * Created by ekudler on 5/30/18.
+ */
+
+public class MainViewModel extends AndroidViewModel {
+
+    // Constant for logging
+    private static final String TAG = MainViewModel.class.getSimpleName();
+
+    private LiveData<List<Timer>> tasks;
+
+    public MainViewModel(Application application) {
+        super(application);
+        AppDatabase database = AppDatabase.getInstance(this.getApplication());
+        tasks = database.timerDao().loadAllTimers();
+    }
+
+    public LiveData<List<Timer>> getTasks() {
+        return tasks;
+    }
+}
