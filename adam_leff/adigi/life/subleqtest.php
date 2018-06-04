d5b8ec86ef83788d776abf69ff59b5d9e0adb9ba
Adam Leff <adam.leff@lin-digital.com>
2018-06-04 11:27:19 -0400

    update test

--- adam_leff/adigi/life/subleqtest.php
+++ adam_leff/adigi/life/subleqtest.php
@@ -1,33 +1,33 @@
 <?php 
 function subleq(){
     $i = 0;
-    $a_addr = $this->getMemoryAt($this->_pc);
+    $a_addr = $this->getMemoryAt($_pc);
     while($a_addr < 0 && $i < (self::SIZE * self::SIZE)){
         $a_addr = $this->getMemoryAt($a_addr * -1);
     }
     if($i == self::SIZE * self::SIZE){
-        $this->_pc++;
+        $_pc++;
         return;
     }
     $i = 0;
-    $b_addr = $this->getMemoryAt($this->_pc + 1);
+    $b_addr = $this->getMemoryAt($_pc + 1);
     while($b_addr < 0 && $i < (self::SIZE * self::SIZE)){
         $b_addr = $this->getMemoryAt($b_addr * -1);
     }
     if($i == self::SIZE * self::SIZE){
-        $this->_pc++;
+        $_pc++;
         return;
     }
     $a_val = $this->getMemoryAt($a_addr);
     $b_val = $this->getMemoryAt($b_addr);
     
     $i = 0;
-    $c_val = $this->getMemoryAt($this->_pc + 2);
+    $c_val = $this->getMemoryAt($_pc + 2);
     while($c_val < 0 && $i < (self::SIZE * self::SIZE)){
         $c_val = $this->getMemoryAt($c_val * -1);
     }
     if($i == self::SIZE * self::SIZE){
-        $this->_pc++;
+        $_pc++;
         return;
     }
     
@@ -37,8 +37,8 @@ function subleq(){
     
     
     if($b_val <= 0){
-        $this->_pc = $c_val;
+        $_pc = $c_val;
     } else {
-        $this->_pc += 3;
+        $_pc += 3;
     }
 }
