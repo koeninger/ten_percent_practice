c1e07de8b3abf10d922979e40c07d9ac541abeed
Chris Larson <clarson@kixer.com>
2018-06-04 09:59:51 -0500

    ch8 wip

--- chris_larson/learning-scalajs/ch8-2.scala
+++ chris_larson/learning-scalajs/ch8-2.scala
@@ -19,3 +19,26 @@ class Car(val make: String, var reserved: Boolean = true, val year: Int = 2018) 
 val a = new Car("Acura")
 val l = new Car("Lexus", year = 2010)
 val p = new Car(reserved = false, make = "Porsche")
+
+class Singular[A](element: A) extends Traversable[A] {
+  def foreach[B](f: A => B) = f(element)
+}
+val p = new Singular("Planes")
+p foreach println
+
+abstract class Car {
+  val year: Int
+  val automatic: Boolean = true
+  def color: String
+}
+new Car()
+
+class RedMini(val year: Int) extends Car {
+  def color = "Red"
+}
+
+val m: Car = new RedMini(2005)
+
+class Mini(val year: Int, val color: String) extends Car
+val redMini: Car = new Mini(2005, "Red")
+println(s"Got a ${redMini.color} Mini")
