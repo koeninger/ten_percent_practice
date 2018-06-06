f65d05d7ed7caffe3acfbd63adf822fd7798de11
Justin Nguyen <jnguyen@kixer.com>
2018-06-06 18:25:54 -0500

    deep/shallow copies and mutable default args

---
+++ justin_nguyen/fluent_python/ch8/twilightbus.py
@@ -0,0 +1,14 @@
+
+class TwilightBus:
+
+    def __init__(self, passengers=None):
+        if passengers is None:
+            self.passengers = []
+        else:
+            self.passengers = passengers
+
+    def pick(self, name):
+        self.passengers.append(name)
+
+    def drop(self, name):
+        self.passengers.remove(name)
