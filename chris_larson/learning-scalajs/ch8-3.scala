aac9ea0b3633a13a2355b6e2c767c952469c04ea
Chris Larson <clarson@kixer.com>
2018-06-09 13:41:44 -0500

    ch8 wip

---
+++ chris_larson/learning-scalajs/ch8-3.scala
@@ -0,0 +1,13 @@
+val d = new java.util.Date
+import java.util.Date
+val d= new Date
+println("Ypur new UUID is " + {import java.util.UUID; UUID.randomUUID})
+import java.util
+val d = new util.Date
+import collection.mutable._
+val b = new ArrayBuffer[String]
+b += "HEllo"
+val q = new Queue[Int]
+q.enqueue(3, 4, 5)
+val pop = q.dequeue
+println(q)
