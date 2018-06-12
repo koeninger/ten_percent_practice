61cd79ba947a1339080990009ae9ddbdb1a5312c
cody koeninger <cody@koeninger.org>
2018-06-12 14:27:30 -0500

    plai ch4

--- cody_koeninger/plai/plai.rkt
+++ cody_koeninger/plai/plai.rkt
@@ -5,6 +5,13 @@
   [plusC (l : ArithC) (r : ArithC)]
   [multC (l : ArithC) (r : ArithC)])
 
+(define-type ArithS
+  [numS (n : number)]
+  [plusS (l : ArithS) (r : ArithS)]
+  [bminusS (l : ArithS) (r : ArithS)]
+  [uminusS (e : ArithS)]
+  [multS (l : ArithS) (r : ArithS)])
+
 (define (parse [s : s-expression]) : ArithC
   (cond
     [(s-exp-number? s) (numC (s-exp->number s))]
@@ -21,3 +28,11 @@
   [numC (n) n]
   [plusC (l r) (+ (interp l) (interp r))]
   [multC (l r) (* (interp l) (interp r))]))
+
+(define (desugar [as : ArithS]) : ArithC
+  (type-case ArithS as
+    [numS (n) (numC n)]
+    [plusS (l r) (plusC (desugar l) (desugar r))]
+    [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
+    [uminusS (e) (multC (numC -1) (desugar e))]
+    [multS (l r) (multC (desugar l) (desugar r))]))
