e5babd1d9dc590fab6324b6aebc2f2393f6f48ca
cody koeninger <cody@koeninger.org>
2018-06-15 15:56:48 -0500

    plai ch 5 done

--- cody_koeninger/plai/plai-test.rkt
+++ cody_koeninger/plai/plai-test.rkt
@@ -3,12 +3,16 @@
 (require rackunit
          "plai.rkt")
 
+(define fds (list))
+
 (check-equal? (parse '(+ 23 42)) (plusC (numC 23) (numC 42)) "parse plus")
 
-(check-equal? (interp (numC 5)) 5 "interp num")
+(check-equal? (interp (numC 5) fds) 5 "interp num")
 
-(check-equal? (interp (plusC (numC 23) (numC 42))) 65 "interp plus")
+(check-equal? (interp (plusC (numC 23) (numC 42)) fds) 65 "interp plus")
 
-(check-equal? (interp (multC (numC 5) (numC 23))) 115 "interp mult")
+(check-equal? (interp (multC (numC 5) (numC 23)) fds) 115 "interp mult")
 
-(check-equal? (interp (parse '(if (+ 0 0) (* 1 2) (* 3 4)))) 12 "interp if false")
+(check-equal? (interp (parse '(if (+ 0 0) (* 1 2) (* 3 4))) fds) 12 "interp if false")
+
+(check-equal? (interp (parse '(if (+ 0 1) (* 1 2) (* 3 4))) fds) 2 "interp if true")
--- cody_koeninger/plai/plai.rkt
+++ cody_koeninger/plai/plai.rkt
@@ -1,21 +1,26 @@
 #lang plai-typed
 
 
-(define-type ArithC
+(define-type ExprC
   [numC (n : number)]
-  [plusC (l : ArithC) (r : ArithC)]
-  [multC (l : ArithC) (r : ArithC)]
-  [ifC (b : ArithC) (t : ArithC) (f : ArithC)])
+  [plusC (l : ExprC) (r : ExprC)]
+  [multC (l : ExprC) (r : ExprC)]
+  [idC (s : symbol)]
+  [appC (fun : symbol) (arg : ExprC)]
+  [ifC (b : ExprC) (t : ExprC) (f : ExprC)])
 
-(define-type ArithS
+(define-type ExprS
   [numS (n : number)]
-  [plusS (l : ArithS) (r : ArithS)]
-  [bminusS (l : ArithS) (r : ArithS)]
-  [uminusS (e : ArithS)]
-  [multS (l : ArithS) (r : ArithS)]
-  [ifS (b : ArithS) (t : ArithS) (f : ArithS)])
+  [plusS (l : ExprS) (r : ExprS)]
+  [bminusS (l : ExprS) (r : ExprS)]
+  [uminusS (e : ExprS)]
+  [multS (l : ExprS) (r : ExprS)]
+  [ifS (b : ExprS) (t : ExprS) (f : ExprS)])
 
-(define (parse [s : s-expression]) : ArithC
+(define-type FunDefC
+  [fdC (name : symbol) (arg : symbol) (body : ExprC)])
+
+(define (parse [s : s-expression]) : ExprC
   (cond
     [(s-exp-number? s) (numC (s-exp->number s))]
     [(s-exp-list? s)
@@ -27,15 +32,39 @@
          [else (error 'parse "invalid list input")]))]
     [else (error 'parse "invalid input")]))
 
-(define (interp [a : ArithC]) : number
-  (type-case ArithC a
-    [numC (n) n]
-    [plusC (l r) (+ (interp l) (interp r))]
-    [multC (l r) (* (interp l) (interp r))]
-    [ifC (b t f) (if (eq? (interp b) 0) (interp f) (interp t))]))
+(define (subst [what : number] [for : symbol] [in : ExprC]) : ExprC
+  (subst_ (numC what) for in))
 
-(define (desugar [as : ArithS]) : ArithC
-  (type-case ArithS as
+(define (subst_ [what : ExprC] [for : symbol] [in : ExprC]) : ExprC
+  (type-case ExprC in
+    [numC (_) in]
+    [idC (s) (cond
+               [(symbol=? s for) what]
+               [else in])]
+    [appC (fun arg) (appC fun (subst_ what for arg))]
+    [plusC (l r) (plusC (subst_ what for l) (subst_ what for r))]
+    [multC (l r) (multC (subst_ what for l) (subst_ what for r))]
+    [ifC (b t f) (ifC (subst_ what for b) (subst_ what for t) (subst_ what for f))]))
+
+(define (get-fundef [n : symbol] [fds : (listof FunDefC)]) : FunDefC
+  (cond
+    [(empty? fds) (error 'get-fundef "reference to undefined function")]
+    [(cons? fds) (cond
+                   [(equal? n (fdC-name (first fds))) (first fds)]
+                   [else (get-fundef n (rest fds))])]))
+
+(define (interp [e : ExprC] [fds : (listof FunDefC)]) : number
+  (type-case ExprC e
+    [numC (n) n]
+    [plusC (l r) (+ (interp l fds) (interp r fds))]
+    [multC (l r) (* (interp l fds) (interp r fds))]
+    [appC (f a) (local ([define fd (get-fundef f fds)])
+                  (interp (subst (interp a fds) (fdC-arg fd) (fdC-body fd)) fds))]
+    [idC (_) (error 'interp "unbound identifier")]
+    [ifC (b t f) (if (eq? (interp b fds) 0) (interp f fds) (interp t fds))]))
+
+(define (desugar [as : ExprS]) : ExprC
+  (type-case ExprS as
     [numS (n) (numC n)]
     [plusS (l r) (plusC (desugar l) (desugar r))]
     [bminusS (l r) (plusC (desugar l) (multC (numC -1) (desugar r)))]
