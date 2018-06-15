436618a35ab29d1a6a606a9dfa538f08964a3a47
Joel Jimenez <JJimenez@nexstardigital.com>
2018-06-15 16:06:53 -0700

    Nested queries and bidirectional relations

--- joel_jimenez/graphql/users/schema/schema.js
+++ joel_jimenez/graphql/users/schema/schema.js
@@ -1,20 +1,57 @@
 const graphql = require('graphql');
 const axios = require('axios');
+const data_url = "http://10.116.53.33:3004"
 
 const {
 	GraphQLObjectType,
 	GraphQLInt,
 	GraphQLString,
-	GraphQLSchema
+	GraphQLSchema,
+	GraphQLList
 } = graphql;
+
+const CompanyType = new GraphQLObjectType({
+	name: "Company",
+	fields: () => ({
+		id: {
+			type: GraphQLString
+		},
+		name: {
+			type: GraphQLString
+		},
+		description: {
+			type: GraphQLString
+		},
+		users: {
+			type: new GraphQLList(UserType),
+			resolve(parentValue, args){
+				return axios.get(`${data_url}/companies/${parentValue.id}/users`)
+						.then(response => response.data);
+			}
+		}
+	})
+});
 
 const UserType = new GraphQLObjectType({
 	name: 'User',
-	fields: {
-		id: { type: GraphQLString },
-		name: { type: GraphQLString },
-		age: { type: GraphQLInt }
-	}
+	fields: () => ({
+		id: {
+			type: GraphQLString
+		},
+		name: {
+			type: GraphQLString
+		},
+		age: {
+			type: GraphQLInt
+		},
+		company: {
+			type: CompanyType,
+			resolve(parentValue, args) {
+				return axios.get(`${data_url}/companies/${parentValue.companyId}`)
+						.then(response => response.data);
+			}
+		}
+	})
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -28,8 +65,20 @@ const RootQuery = new GraphQLObjectType({
 				}
 			},
 			resolve(parentValue, args) {
-				const url = `http://10.116.53.33:3004/users/${args.id}`;
-				return axios.get(url).then(response => response.data);
+				return axios.get(`${data_url}/users/${args.id}`)
+						.then(response => response.data);
+			}
+		},
+		company: {
+			type: CompanyType,
+			args: {
+				id: {
+					type: GraphQLString
+				}
+			},
+			resolve(parentValue, args) {
+				return axios.get(`${data_url}/companies/${args.id}`)
+						.then(response => response.data);
 			}
 		}
 	}
