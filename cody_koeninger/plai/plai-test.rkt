b7016ac6b0760908805c535ba122aec9917bb10e
cody koeninger <cody@koeninger.org>
2018-06-20 13:35:22 -0500

    plai 8.2

--- cody_koeninger/plai/plai-test.rkt
+++ cody_koeninger/plai/plai-test.rkt
@@ -7,41 +7,42 @@
 (test (parse '(+ 23 42))
       (plusC (numC 23) (numC 42)))
 
-(test (interp (numC 5) mt-env)
+(test (v*s-v (interp (numC 5) mt-env mt-store))
       (numV 5))
 
-(test (interp (plusC (numC 23) (numC 42)) mt-env)
+(test (v*s-v (interp (plusC (numC 23) (numC 42)) mt-env mt-store))
       (numV 65))
 
-(test (interp (multC (numC 5) (numC 23)) mt-env)
+(test (v*s-v (interp (multC (numC 5) (numC 23)) mt-env mt-store))
       (numV 115))
 
-(test (interp (parse '(if (+ 0 0) (* 1 2) (* 3 4))) mt-env)
+(test (v*s-v (interp (parse '(if (+ 0 0) (* 1 2) (* 3 4))) mt-env mt-store))
       (numV 12))
 
-(test (interp (parse '(if (+ 0 1) (* 1 2) (* 3 4))) mt-env)
+(test (v*s-v (interp (parse '(if (+ 0 1) (* 1 2) (* 3 4))) mt-env mt-store))
       (numV 2))
 
  
-(test (interp (appC (lamC 'x (appC (lamC 'y (plusC (idC 'x) (idC 'y)))
+(test (v*s-v (interp (appC (lamC 'x (appC (lamC 'y (plusC (idC 'x) (idC 'y)))
                                           (numC 4)))
                         (numC 3))
-                  mt-env)
+                  mt-env mt-store))
           (numV 7))
 
-(test (interp
+(test (v*s-v (interp
        (appC (appC (lamC 'x
                         (lamC 'y
                              (plusC (idC 'x) (idC 'y))))
                    (numC 4))
              (numC 5))
-       mt-env)
+       mt-env mt-store))
       (numV 9))
 
-(test/exn (interp
+(test/exn (v*s-v (interp
        (appC
         (appC (lamC 'f (lamC 'x (appC (idC 'f) (numC 10))))
               (lamC 'y (plusC (idC 'x) (idC 'y))))
         (numC 23))
-       mt-env)
+       mt-env mt-store))
       "lookup: unbound identifier")
+
--- cody_koeninger/plai/plai.rkt
+++ cody_koeninger/plai/plai.rkt
@@ -8,6 +8,10 @@
   [idC (s : symbol)]
   [appC (fun : ExprC) (arg : ExprC)]
   [lamC (arg : symbol) (body : ExprC)]
+  [boxC (arg : ExprC)]
+  [unboxC (arg : ExprC)]
+  [setboxC (b : ExprC) (v : ExprC)]
+  [seqC (b1 : ExprC) (b2 : ExprC)]
   [ifC (b : ExprC) (t : ExprC) (f : ExprC)])
 
 (define-type ExprS
@@ -20,10 +24,13 @@
 
 (define-type Value
   [numV (n : number)]
-  [closV (arg : symbol) (body : ExprC) (env : Env)])
+  [closV (arg : symbol) (body : ExprC) (env : Env)]
+  [boxV (l : Location)])
+
+(define-type-alias Location number)
 
 (define-type Binding
-  [bind (name : symbol) (val : Value)])
+  [bind (name : symbol) (val : Location)])
 
 (define-type-alias Env (listof Binding))
 (define mt-env empty)
@@ -34,7 +41,19 @@
       ; [__ (display "\n\n")]
        )
     ret))
-    
+
+(define-type Storage
+  [cell (location : Location) (val : Value)])
+(define-type-alias Store (listof Storage))
+(define mt-store empty)
+(define (override-store [c : Storage] [s : Store]) : Store
+  (cond
+    [(empty? s) (list c)]
+    [(equal? (cell-location c) (cell-location (first s))) (cons c (rest s))]
+    [else (cons (first s) (override-store c (rest s)))]))
+
+(define-type Result
+  [v*s (v : Value) (s : Store)])
 
 (define (parse [s : s-expression]) : ExprC
   (cond
@@ -48,11 +67,17 @@
          [else (error 'parse "invalid list input")]))]
     [else (error 'parse "invalid input")]))
 
-(define (lookup [i : symbol] [env : Env]) : Value
+(define (lookup [for : symbol] [env : Env]) : Location
   (cond
     [(empty? env) (error 'lookup "unbound identifier")]
-    [(equal? i (bind-name (first env))) (bind-val (first env))]
-    [else (lookup i (rest env))]))
+    [(equal? for (bind-name (first env))) (bind-val (first env))]
+    [else (lookup for (rest env))]))
+
+(define (fetch [loc : Location] [sto : Store]) : Value
+  (cond
+    [(empty? sto) (error 'fetch "unbound location")]
+    [(equal? loc (cell-location (first sto))) (cell-val (first sto))]
+    [else (fetch loc (rest sto))]))
 
 (define (num+ [l : Value] [r : Value]) : Value
   (cond
@@ -66,20 +91,65 @@
      (numV (* (numV-n l) (numV-n r)))]
     [else (error 'num* "argument was not a number")]))
 
-(define (interp [e : ExprC] [env : Env]) : Value
-  (type-case ExprC e
-    [numC (n) (numV n)]
-    [plusC (l r) (num+ (interp l env) (interp r env))]
-    [multC (l r) (num* (interp l env) (interp r env))]
-    [lamC (a b) (closV a b env)]
-    [appC (f a) (local ([define f-value (interp f env)])
-                  (interp (closV-body f-value)
-                          (extend-env (bind (closV-arg f-value)
-                                            (interp a env))
-                                       (closV-env f-value))))]
-    [idC (i) (lookup i env)]
-    [ifC (b t f) (if (equal? (interp b env) (numV 0)) (interp f env) (interp t env))]))
+(define new-loc
+  (let ([n (box 0)])
+    (lambda ()
+      (begin
+        (set-box! n (add1 (unbox n)))
+        (unbox n)))))
 
+(define (interp [e : ExprC] [env : Env] [sto : Store]) : Result
+  (type-case ExprC e
+    [numC (n) (v*s (numV n) sto)]
+    [plusC (l r) (type-case Result (interp l env sto)
+                   [v*s (v-l s-l)
+                        (type-case Result (interp r env s-l)
+                          [v*s (v-r s-r)
+                               (v*s (num+ v-l v-r) s-r)])])]
+    [multC (l r) (type-case Result (interp l env sto)
+                   [v*s (v-l s-l)
+                        (type-case Result (interp r env s-l)
+                          [v*s (v-r s-r)
+                               (v*s (num* v-l v-r) s-r)])])]
+    [lamC (a b) (v*s (closV a b env) sto)]
+    [appC (f a) (type-case Result (interp f env sto)
+                  [v*s (v-f s-f)
+                       (type-case Result (interp a env s-f)
+                         [v*s (v-a s-a)
+                              (let ([where (new-loc)])
+                                (interp (closV-body v-f)
+                                        (extend-env (bind (closV-arg v-f)
+                                                          where)
+                                                    (closV-env v-f))
+                                        (override-store (cell where v-a) s-a)))])])]
+    [idC (n) (v*s (fetch (lookup n env) sto) sto)]
+    [boxC (a) (type-case Result (interp a env sto)
+                [v*s (v-a s-a)
+                     (let ([where (new-loc)])
+                       (v*s (boxV where)
+                            (override-store (cell where v-a)
+                                            s-a)))])]
+    [unboxC (a) (type-case Result (interp a env sto)
+                  [v*s (v-a s-a)
+                       (v*s (fetch (boxV-l v-a) s-a) s-a)])]
+    [seqC (b1 b2) (type-case Result (interp b1 env sto)
+                    [v*s (v-b1 s-b1)
+                         (interp b2 env s-b1)])]
+    [setboxC (b v) (type-case Result (interp b env sto)
+                     [v*s (v-b s-b)
+                          (type-case Result (interp v env s-b)
+                            [v*s (v-v s-v)
+                                 (v*s v-v
+                                      (override-store (cell (boxV-l v-b)
+                                                            v-v)
+                                                      s-v))])])]
+    [ifC (b t f) (type-case Result (interp b env sto)
+                   [v*s (v-b s-b)
+                        (if (equal? v-b (numV 0))
+                            (interp f env s-b)
+                            (interp t env s-b))])]))
+    ;[ifC (b t f) (if (equal? (interp b env) (numV 0)) (interp f env) (interp t env))]))
+    
 (define (desugar [as : ExprS]) : ExprC
   (type-case ExprS as
     [numS (n) (numC n)]
