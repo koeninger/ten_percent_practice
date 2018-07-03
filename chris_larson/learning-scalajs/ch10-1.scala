5192c5ce5b12518dfa5a21e50fb89dc8ce013133
Chris Larson <clarson@kixer.com>
2018-07-03 09:51:42 -0500

    ch10 wip

--- chris_larson/learning-scalajs/ch10-1.scala
+++ chris_larson/learning-scalajs/ch10-1.scala
@@ -2,3 +2,20 @@ val t1: (Int, Char) = (1, 'a')
 val t2: (Int, Char) = Tuple2[Int, Char](1, 'a')
 val f1: Int=>Int = _ + 2
 val f2: Int=>Int = new Function1[Int, Int] { def apply(x: Int) = x * 2 }
+
+object ImplicitClasses {
+  implicit class Hello(s: String) { def hello = s"Hello, $s" }
+  def test = {
+    println( "World".hello )
+  }
+}
+ImplicitClasses.test
+
+object ImplicitParams {
+  def greet(name: String)(implicit greeting: String) = s"greeting, $name"
+  implicit val hi = "Hello"
+  def test = {
+    println( greet("Developers") )
+  }
+}
+ImplicitParams.test
