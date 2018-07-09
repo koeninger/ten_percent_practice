c14c668574e031b9a5093b484b2df95d7c11317f
Chris Larson <clarson@kixer.com>
2018-07-09 10:20:11 -0500

    slideshow from template

--- chris_larson/apache-poi/src/main/scala/ppt.scala
+++ chris_larson/apache-poi/src/main/scala/ppt.scala
@@ -1,12 +1,18 @@
 import org.apache.poi.xslf.usermodel._
-import java.io.{File, FileOutputStream}
+import java.io.{File, FileOutputStream, FileInputStream}
 
 object Hello {
   def main(args: Array[String]) {
     import scala.collection.JavaConversions._
 
-    val ppt = new XMLSlideShow();
-    val blankSlide = ppt.createSlide();
+    val ppt = new XMLSlideShow(new FileInputStream("slideshow-template.pptx"));
+    println("Available slide layouts: ")
+    for( master <- ppt.getSlideMasters()){
+      for( layout <- master.getSlideLayouts()) {
+        println(layout.getType())
+      }
+    }
+    val slide1 = ppt.createSlide();
     val fos = new FileOutputStream("SampleSS-updated.pptx")
     ppt.write(fos)
     ppt.close()
