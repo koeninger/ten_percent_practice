d0fe92d337e01d7a4e327103c319c6efcef11b66
Chris Larson <clarson@kixer.com>
2018-07-09 13:19:30 -0500

    create table slide

--- chris_larson/apache-poi/src/main/scala/ppt.scala
+++ chris_larson/apache-poi/src/main/scala/ppt.scala
@@ -1,5 +1,9 @@
 import org.apache.poi.xslf.usermodel._
+import org.apache.poi.sl.usermodel.TableCell.BorderEdge
+import org.apache.poi.sl.usermodel.TextParagraph.TextAlign
 import java.io.{File, FileOutputStream, FileInputStream}
+import java.awt.Color
+import java.awt.Rectangle
 
 object Hello {
   def main(args: Array[String]) {
@@ -14,17 +18,39 @@ object Hello {
     
     val defaultMaster = ppt.getSlideMasters().get(0)
     val slides = ppt.getSlides()
-    val chart = slides.get(1)
-    chart.getPlaceholder(0).setText("Chart Title")
 
     val titleLayout = defaultMaster.getLayout(SlideLayout.TITLE_AND_CONTENT);
     val slide1 = ppt.createSlide(titleLayout)
+
     val title1 = slide1.getPlaceholder(0)
     title1.setText("New Slide Title")
+
     val body1 = slide1.getPlaceholder(1)
     body1.clearText()
     body1.addNewTextParagraph().addNewTextRun().setText("First Paragraph")
-    val fos = new FileOutputStream("SampleSS-updated.pptx")
+
+    val blankLayout = defaultMaster.getLayout(SlideLayout.BLANK);
+    val slide2 = ppt.createSlide(blankLayout)
+    val table2 = slide2.createTable()
+    table2.setAnchor(new Rectangle(50, 50, 450, 300));
+    val headerRow = table2.addRow();
+    for (i <- 0 to 2) {
+      val th = headerRow.addCell()
+      val p = th.addNewTextParagraph()
+      p.setTextAlign(TextAlign.CENTER)
+      val r = p.addNewTextRun()
+      r.setText("Header " + i)
+      r.setBold(true)
+      r.setFontColor(Color.white)
+      th.setFillColor(new Color(79, 129, 189))
+      th.setBorderWidth(BorderEdge.bottom, 2.0)
+      th.setBorderColor(BorderEdge.bottom, Color.white)
+
+      table2.setColumnWidth(i, 150)
+    }
+
+
+    val fos = new FileOutputStream("slideshow.pptx")
     ppt.write(fos)
     ppt.close()
   }
