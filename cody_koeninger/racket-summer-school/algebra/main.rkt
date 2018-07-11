0878b7f48c29ca78ef434626707793413ae7f09b
cody koeninger <cody@koeninger.org>
2018-07-10 21:07:23 -0600

    racket summer school end of day 2

--- cody_koeninger/racket-summer-school/algebra/main.rkt
+++ cody_koeninger/racket-summer-school/algebra/main.rkt
@@ -1,57 +1,69 @@
 #lang racket
 (require (for-syntax syntax/parse racket/list))
 
-(module reader syntax/module-reader simple)
+(module reader syntax/module-reader
+  algebra
+  #:wrapper1 (lambda (t)
+               (parameterize ([read-decimal-as-inexact #f])
+                 (t))))
 
 (provide #%module-begin
          define-function
          function-app
+         exactly
+         inexactly
          (rename-out [literal #%datum]
                      [my-if if]
                      [complain-app #%app]))
 
+(define-for-syntax exacting #f)
+
+(define-syntax (exactly stx)
+  (syntax-parse stx
+    [(_ e:expr)
+     (begin
+       (set! exacting #t)
+       #'e)]))
+
+
+(define-syntax (inexactly stx)
+  (syntax-parse stx
+    [(_ e:expr)
+     (begin
+       (set! exacting #f)
+       #'e)]))
+
 (define-syntax (literal stx)
   (syntax-parse stx
     [(_ . s:string) #'(quote s)]
-    [(_ . n:number) #'(#%datum . n)]
+    [(_ . n:number) (let ((num ((if exacting inexact->exact exact->inexact)
+                                (syntax-e #'n))))
+                      #`(#%datum . #,num))]
     [(_ . other)
      (raise-syntax-error
       'simple
       "you can only write numbers or strings"
       #'other)]))
 
-; Works but zero points
-#;
-(define-syntax (op stx)
-  (syntax-parse stx
-   #:datum-literals (+ - / *)
-   [(+ n1 n2) #`(+ n1 n2)]
-   [(- n1 n2) #`(- n1 n2)]
-   [(/ n1 n2) #`(/ n1 n2)]
-   [(* n1 n2) #`(* n1 n2)]
-    ))
 
 (define-syntax (make-operator stx)
   (syntax-parse stx
-    ([_ operator]  
-#'(begin
-    (define-syntax (op stx)
-      (syntax-parse stx
-        [(o n1 n2) #`(#,operator n1 n2)]))
-    (provide (rename-out [op operator]))))))
-(make-operator +)
-(make-operator -)
-(make-operator /)
-(make-operator *)
+    ([_ (opname opfn) ...]  
+     #'(begin
+         (begin
+           (define-syntax (opname stx)
+             (syntax-parse stx
+               [(o n1 n2) #`(#,opfn n1 n2)]))
+           (provide (rename-out [opname opfn]) )) ...))))
 
+(make-operator
+ (plus +)
+ (minus -)
+ (mult *)
+ (div /)
+ (gt >)
+ (gte >=))
     
-#;
-(define-syntax (op stx)
-  (define ids (syntax->list stx))
-  (define operation (first ids))
-  (define operands (rest ids))
-  #`(#,operation #,@operands))
- 
 (define-syntax (complain-app stx)
   (define (complain msg src-stx)
     (raise-syntax-error 'parentheses msg src-stx))
@@ -93,18 +105,3 @@
   (syntax-parse stx
     [(_ p:expr then:expr else:expr)
     #'(if p then else)]))
-
-#;
-(define-syntax f (cons 2 #'(lambda (x y) (+ x y))))
-#;
-(define-function (f x y ) (+ x y ))
-#;
-(function-app f 1 2)
-#;
-(define-function (g x)
-  (if (zero? x)
-      1
-      (function-app g 0)))
-#;
-(function-app g 1)
-           
