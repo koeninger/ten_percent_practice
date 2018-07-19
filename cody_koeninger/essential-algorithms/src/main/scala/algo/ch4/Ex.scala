b06d226d3f75dde9b2cf429296afe77a67c419da
cody koeninger <cody@koeninger.org>
2018-07-19 17:09:16 -0500

    essential algo ch4 ex4

--- cody_koeninger/essential-algorithms/src/main/scala/algo/ch4/Ex.scala
+++ cody_koeninger/essential-algorithms/src/main/scala/algo/ch4/Ex.scala
@@ -1,5 +1,7 @@
 package algo.ch4
 
+import scala.util.Sorting
+
 object Ex {
   def mean[T](a: Array[T])(implicit f: Fractional[T], n: Numeric[T]): T = {
     f.div(a.sum, n.fromInt(a.size))
@@ -16,5 +18,29 @@ object Ex {
 
   def stdDev[T](a: Array[T])(implicit f: Fractional[T], n: Numeric[T]): Double = {
     math.sqrt(f.toDouble(variance(a)))
+  }
+
+  /* destructively sorts a */
+  def median[T](a: Array[T])(implicit f: Fractional[T], n: Numeric[T], o: math.Ordering[T]): T = {
+    Sorting.quickSort(a)
+    val mid = a.size / 2.0
+    val flr = math.floor(mid)
+    if (mid == flr) {
+      f.div(n.plus(a(mid.toInt), a(mid.toInt - 1)), n.fromInt(2))
+    } else {
+      a(flr.toInt)
+    }
+  }
+
+  def remove[T >: Null](a: Array[T], i: Int): Unit = {
+    var j = i
+    val stop = a.size - 1
+    while (j < stop) {
+      val n = j + 1
+      a(j) = a(n)
+      j = n
+    }
+    // can't resize an array in jvm, set last entry to null
+    a(j) = null
   }
 }
