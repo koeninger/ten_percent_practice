44f4c63d173f1041024e5ada376af3fff1adc283
Chris Larson <clarson@kixer.com>
2018-07-19 15:27:46 -0500

    ch10 wip

---
+++ chris_larson/learning-scalajs/ch10-2.scala
@@ -0,0 +1,39 @@
+object IntUtils {
+  implicit class Fishies(val x: Int) {
+    def fishes = "Fish" * x
+  }
+}
+
+import IntUtils._
+println(3.fishes)
+
+implicit class ArrowAssoc[A](x: A) {
+  def ->[B](y: B) = Tuple2(x, y)
+}
+
+1 -> "a"
+
+object TypeFun {
+  type Whole = Int
+  val x: Whole = 5
+
+  type UserInfo = Tuple2[Int, String]
+  val u: UserInfo = new UserInfo(123, "George")
+
+  type T3[A,B,C] = Tuple3[A,B,C]
+  val things = new T3(1, 'a', true)
+}
+
+val x = TypeFun.x
+val u = TypeFun.u 
+val things = TypeFun.things 
+
+class User(val name: String)
+trait Factory { type A; def create: A }
+trait UserFactory extends Factory {
+  type A = User 
+  def create = new User("") 
+}
+
+trait Factory[A] {def create: A }
+trait UserFactory extends Factory[User] { def create = new User("") }
