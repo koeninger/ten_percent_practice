1263afcc186e5181ad2040de9f558f4f03ae4e3c
jvelis <jvelis@kixer.com>
2018-07-20 10:01:44 -0500

    ch 01 - file input/output

--- juan_velis/competitive_programmer/ch01/test.cpp
+++ juan_velis/competitive_programmer/ch01/test.cpp
@@ -4,16 +4,31 @@ using namespace std;
 
 int main() {
 
-	// g++ -std=c++11 -O2 -Wall test.cpp -o test
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
 	cout<<"Hello World"<<endl;
 
-	int a, b;
-	string x;
+	int a, b, c, d;
+	string s, x, e;
 	cin >> a >> b >> x; // 123 456 food
 
 	cout << a << " " << b << " " << x << "\n";
 
+	scanf("%d %d", &c, &d);
+	printf("%d %d\n", c, d);
+
 	double y = 0.3*3+0.1;
 	printf("%.20f\n", y);
+
+	freopen("input.txt", "r", stdin); // redirects standard input
+	freopen("output2.txt", "w", stdout); // redirects standard output
+
+	// Get each line from input.txt and write it to output2.txt
+	while (cin >> x) {
+		getline(cin, e); // r
+		cout << e << endl; // w
+	}
+
 	return 0;
 }
