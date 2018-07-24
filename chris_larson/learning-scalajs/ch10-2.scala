c4ce1312a30860b133fc7d0eeb41939e30eaed3d
Chris Larson <clarson@kixer.com>
2018-07-23 20:25:29 -0500

    ch10 wip

--- chris_larson/learning-scalajs/ch10-2.scala
+++ chris_larson/learning-scalajs/ch10-2.scala
@@ -46,3 +46,29 @@ class PreferredCustomer(name: String) extends BaseUser(name)
 def check[A <: BaseUser](u: A) { if (u.name.isEmpty) println("Fail!") }
 check(new Customer("Fred"))
 check(new Admin("", "strict"))
+
+def recruit[A >: Customer](u: Customer): A = u match {
+  case p: PreferredCustomer => new PreferredCustomer(u.name)
+  case c: Customer => new Customer(u.name)
+}
+val customer = recruit(new Customer("Fred"))
+val preferred = recruit(new PreferredCustomer("George"))
+
+abstract class Card {
+  type UserType <: BaseUser
+  def verify(u: UserType): Boolean
+}
+
+class SecurityCard extends Card {
+  type UserType = Admin
+  def verify(u: Admin) = true 
+}
+
+val v1 = new SecurityCard().verify(new Admin("George", "high"))
+
+class GiftCard extends Card {
+  type UserType = Customer
+  def verify(u: Customer) = true 
+}
+
+val v2 = new GiftCard().verify(new Customer("Fred"))
