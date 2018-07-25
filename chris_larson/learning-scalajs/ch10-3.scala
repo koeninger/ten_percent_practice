d0489c3f5270b4d60a7e77a2d9343f3b12a88f2d
Chris Larson <clarson@kixer.com>
2018-07-25 15:59:29 -0500

    ch10 wip

--- chris_larson/learning-scalajs/ch10-3.scala
+++ chris_larson/learning-scalajs/ch10-3.scala
@@ -8,3 +8,19 @@ val c: Item[Car] = new Item[Volvo](new Volvo)
 case class Item[+A](a: A) { def get: A = a }
 val c: Item[Car] = new Item[Volvo](new Volvo)
 val auto = c.get
+
+class Check[+A] { def check(a: A) = {} }
+class Check[-A] { def check(a: A) = {} }
+
+class Car; class Volvo extends Car; class VolvoWagon extends Volvo
+class Item[+A](a: A) { def get: A = a }
+class Check[-A] { def check(a: A) = {} }
+def item(v: Item[Volvo]) { val c: Car = v.get }
+def check(v: Check[Volvo]) { v.check(new VolvoWagon)}
+
+item( new Item[Car](new Car()) )
+item( new Item[Volvo](new Volvo) )
+item( new Item[VolvoWagon](new VolvoWagon()) )
+check( new Check[Car]() )
+check( new Check[Volvo]() )
+check( new Check[VolvoWagon]() )
