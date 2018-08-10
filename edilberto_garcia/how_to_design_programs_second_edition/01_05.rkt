4767a78e0ab3fd0141813fff6d20f188f0f20a70
Edilberto Garcia <egarcia@federatedmedia.net>
2018-08-10 16:05:36 -0400

    Chapter 1 cont

---
+++ edilberto_garcia/how_to_design_programs_second_edition/01_05.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 01_05) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+
+(overlay
+ 
+(overlay/xy
+ (star-polygon 20 10 3 "solid" "green")
+ 18 50
+ (rectangle 25 120 "solid" "orange")
+)
+
+(empty-scene 70 200)
+
+)
