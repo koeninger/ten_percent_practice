48b67cd84a2ac9ab4572b9c71600e9f5a131a829
hokey <mike.hochanadel@lin-digital.com>
2018-08-13 15:07:40 -0500

    HTDP2ed exercise 1,2 completed. At tail end of 1.3

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch1/ex1_interactions.rkt
@@ -0,0 +1,8 @@
+Welcome to DrRacket, version 7.0 [3m].
+Language: Beginning Student [custom]; memory limit: 128 MB.
+Teachpacks: 2htdp/image.rkt and universe.rkt.
+> (sqrt (+ (sqr x) (sqr y)))
+13
+> (distance x y)
+13
+> 
