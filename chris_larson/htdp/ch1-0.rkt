cb2f25c9273c171e58e45608d6cf89b33e038c1e
Chris Larson <clarson@kixer.com>
2018-08-13 14:51:45 -0500

    ch1

---
+++ chris_larson/htdp/ch1-0.rkt
@@ -0,0 +1,9 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ch1-0) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(+ 1 2)
+(+ 1 (+ 1 (+ 1 1) 2) 3 4 5)
+(+ 3 4)
+(sin 0)
+(define x 3)
+(define y 4)
