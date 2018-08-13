a86da171317855595bdeace245451f9ecfa08854
Edilberto Garcia <egarcia@federatedmedia.net>
2018-08-13 16:12:18 -0400

    chapter 1 cont

---
+++ edilberto_garcia/how_to_design_programs_second_edition/01_07.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 01_07) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define sunny #true)
+(define friday #false)
+
+;Now create an expression that computes whether sunny is false or friday is true.
+;So in this particular case, the answer is #false. (Why?) => false OR false  is false
+
+(or (not sunny) friday)
