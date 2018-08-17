b52c7a98735d2b117da2e730bcee8b5cd0dd9794
hokey <mike.hochanadel@lin-digital.com>
2018-08-17 15:08:06 -0500

    HTDP2ed chapter 1 exercises 5-7

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch1/ex5.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex5) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(overlay/align "center" "bottom"
+     (rectangle 60 30 "solid" "black")
+     (rectangle 30 60 "solid" "red"))
