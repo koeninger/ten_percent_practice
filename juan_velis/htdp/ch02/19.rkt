6ff547103fb549652898d0f708780565813471e2
jvelis <jvelis@kixer.com>
2018-08-21 15:59:18 -0500

    ch02

---
+++ juan_velis/htdp/ch02/19.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |19|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define str "helloworld")
+(define i 5)
+
+(define (string-join string x)
+  (string-append (substring string 0 x) " " (substring string x)))
+
+(string-join str i)
