098ec8a2b848ad7ede5a4161a1a32c5eebecdc35
hokey <mike.hochanadel@lin-digital.com>
2018-08-22 14:57:31 -0500

    HTDP2ed chapter 2 exercises 11-15

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch2/ex12_interactions.rkt
@@ -0,0 +1,16 @@
+Welcome to DrRacket, version 7.0 [3m].
+Language: Beginning Student [custom]; memory limit: 128 MB.
+Teachpacks: 2htdp/image.rkt and universe.rkt.
+> (cvolume 3)
+27
+> (cvolume 4)
+64
+> (cvolume 5)
+125
+> (csurface 3)
+54
+> (csurface 4)
+96
+> (csurface 5)
+150
+> 
