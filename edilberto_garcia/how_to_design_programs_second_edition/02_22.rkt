5eaaf51329f251e569fb4d84ca92f75a0b7d07c3
Edilberto Garcia <egarcia@federatedmedia.net>
2018-08-22 16:07:23 -0400

    Chapter 2 Cont

---
+++ edilberto_garcia/how_to_design_programs_second_edition/02_22.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 02_22) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Exercise 22. Use DrRacket’s stepper on this program fragment:
+
+
+(define (distance-to-origin x y)
+  (sqrt (+ (sqr x) (sqr y))))
+(distance-to-origin 3 4)
+
+; Does the explanation match your intuition?
+
+; Yes, it does
+
