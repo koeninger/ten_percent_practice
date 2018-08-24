156a9884ad21cbacae13f492ade4b6144068164b
Justin Nguyen <jnguyen@kixer.com>
2018-08-24 15:09:34 -0500

    27 - 30

---
+++ justin_nguyen/htdp/ch2/ex27.rkt
@@ -0,0 +1,24 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex27) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+
+(define BASE-ATTENDANCE 120)
+(define BASE-TICKET-PRICE 5)
+(define AVG-ATTENDANCE-VARIATION 15.0)
+(define AVG-PRICE-VARIATION 0.1)
+(define FIXED-COST 180)
+(define COST-PER-ATTENDEE 0.04)
+
+
+(define (attendees ticket-price)
+  (- BASE-ATTENDANCE (* (- ticket-price BASE-TICKET-PRICE) (/ AVG-ATTENDANCE-VARIATION AVG-PRICE-VARIATION))))
+
+(define (revenue ticket-price)
+  (* ticket-price (attendees ticket-price)))
+
+(define (cost ticket-price)
+  (+ FIXED-COST (* COST-PER-ATTENDEE (attendees ticket-price))))
+
+(define (profit ticket-price)
+  (- (revenue ticket-price)
+     (cost ticket-price)))
