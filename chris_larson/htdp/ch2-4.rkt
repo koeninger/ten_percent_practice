88588e1ad78f7c1888f211f27cea2755fa8d8e7e
Chris Larson <clarson@kixer.com>
2018-08-24 14:48:29 -0500

    3.1

--- chris_larson/htdp/ch2-4.rkt
+++ chris_larson/htdp/ch2-4.rkt
@@ -6,6 +6,7 @@
 (define DOT (circle 3 "solid" "red"))
 
 ; functions
+; what does the function compute?
 (define (number->square s)
   (square s "solid" "red"))
 (define (reset s ke)
@@ -20,6 +21,25 @@
   (place-image DOT 50 y BACKGROUND))
 (define (stop y ke)
   0)
+; Number String Image -> Image
+; adds s to img, y pixels from top, 10 pixels to the left
+; given: 
+;    5 for y, 
+;    "hello" for s, and
+;    (empty-scene 100 100) for img
+; expected: 
+;    (place-image (text "hello" 10 "red") 10 5 ...)
+;    where ... is (empty-scene 100 100)
+(define (add-image y s img)
+  (place-image (text s 10 "red") 10 y img))
+
+; Number -> Number
+; computes the area of a square with side len
+; given: 2, expect: 4
+; given: 7, expect: 49
+(define (area-of-square len)
+  (sqr len))
+
 
 ; scratch
 (number->square 5)
@@ -33,4 +53,6 @@
 ;  [on-key reset])
 (place-dot-at 89)
 (stop 89 "q")
-(main 90)
+; (main 90)
+(area-of-square 2)
+(area-of-square 7)
