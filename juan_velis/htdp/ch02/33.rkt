d3e5ca2e61a399108db62f4885c0e883982f5fc4
jvelis <jvelis@kixer.com>
2018-08-31 16:07:38 -0500

    ch02ex33

--- juan_velis/htdp/ch02/33.rkt
+++ juan_velis/htdp/ch02/33.rkt
@@ -1,5 +1,22 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |33|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
-; test file
-; #33
+(require 2htdp/universe)
+(require 2htdp/image)
+
+; Number String Image -> Image 
+; adds s to img,
+; y pixels from the top and 10 from the left 
+(define (add-image y s img)
+  (empty-scene 100 100))
+
+; Number -> Number
+; computes the area of a square with side len 
+; given: 2, expect: 4
+; given: 7, expect: 49
+(define (area-of-square len)
+  (sqr len))
+
+(area-of-square 2)
+(area-of-square 7)
+(area-of-square 8)
