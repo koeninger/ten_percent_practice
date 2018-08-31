7127802cbf434ec64495225e32158d9946450d1d
hokey <mike.hochanadel@lin-digital.com>
2018-08-31 15:05:11 -0500

    HTDP2ed chapter 2 exercises 28-30, fixed exercise 27

--- mike_hochanadel/HowToDesignPrograms2ed/ch2/ex30.rkt
+++ mike_hochanadel/HowToDesignPrograms2ed/ch2/ex30.rkt
@@ -1,16 +1,17 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex27) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex30) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 (define AVERAGE_ATTENDANCE 120)
 (define AVERAGE_TICKET_PRICE 5.0)
 (define PRICE_CHANGE_DIVISOR 0.1)
 (define ATTENDANCE_PER_PRICE_CHANGE 15)
+(define ATTENDANCE_PRICE_CHANGE (/ ATTENDANCE_PER_PRICE_CHANGE PRICE_CHANGE_DIVISOR))
 (define FIXED_COST 180)
 (define COST_PER_ATTENDEE 0.04)
 
 (define (attendees ticket-price)
   (- AVERAGE_ATTENDANCE
-     (* (- ticket-price AVERAGE_TICKET_PRICE) (/ ATTENDANCE_PER_PRICE_CHANGE PRICE_CHANGE_DIVISOR))))
+     (* (- ticket-price AVERAGE_TICKET_PRICE) ATTENDANCE_PRICE_CHANGE )))
 
 (define (revenue ticket-price)
   (* ticket-price (attendees ticket-price)))
