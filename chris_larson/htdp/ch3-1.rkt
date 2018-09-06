94f7e01221705d1ea5fcb6ea5033450a86133856
Chris Larson <clarson@kixer.com>
2018-09-06 11:58:53 -0500

    exercise 40

--- chris_larson/htdp/ch3-1.rkt
+++ chris_larson/htdp/ch3-1.rkt
@@ -2,12 +2,15 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname ch3-1) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 ; definitions
-(define WHEEL-RADIUS 10)
+(define WIDTH-OF-WORLD 300)
+(define WHEEL-RADIUS 5)
 (define WHEEL-DISTANCE (* WHEEL-RADIUS 2))
 (define BUMPER-DISTANCE (* WHEEL-RADIUS 8))
 (define TRUNK-HEIGHT (* WHEEL-RADIUS 2))
 (define ROOF-HEIGHT WHEEL-RADIUS)
 (define ROOF-WIDTH (* WHEEL-RADIUS 4))
+(define BACKGROUND (empty-scene WIDTH-OF-WORLD 30))
+(define Y-CAR 15)
 
 (define WHEEL
   (circle WHEEL-RADIUS "solid" "black"))
@@ -26,18 +29,27 @@
    BODY
    0 (* WHEEL-RADIUS 3/2)
    BOTH-WHEELS))
-CAR
+
 ; WorldState: data that represents the state of the world (cw)
+; A WorldState is a Number.
+; interpretation the number of pixels between
+; the left border of the scene and the car
 
 ; WorldState -> Image
 ; when needed, big-bang obtains the image of the current
 ; state of the world by evaluating (render cw)
-(define (render ws) ...)
+(define (render ws)
+  (place-image CAR ws Y-CAR BACKGROUND))
 
 ; World State -> WorldState
 ; for each tick of the clock, big-bang obtains the next
 ; state of the world from (clock-tick-handler cw)
-(define (clock-tick-handler cw) ...)
+; given: 20, expect 23
+; given: 78, expect 81
+(define (tock ws)
+  (+ ws 3))
+(check-expect (tock 20) 23)
+(check-expect (tock 78) 81)
 
 ; WorldState String -> WorldState
 ; for each keystroke, big-bang obtains the next state
@@ -53,3 +65,10 @@ CAR
 ; WorldState -> Boolean
 ; after each event, big-bang evaluates (end? cw)
 (define (end? cw) ...)
+
+; WorldState -> WorldState
+; launches the program form some initial state
+(define (main ws)
+  (big-bang ws
+    [on-tick tock]
+    [to-draw render]))
