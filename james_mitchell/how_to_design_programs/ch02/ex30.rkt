cc64d573a0aad0670bc7d6733a1e27ec5dd653b1
jmitchell <jmitchell@hyfnlocal.com>
2018-09-07 14:27:16 -0500

    ex29 updates, ex30 and 31

--- james_mitchell/how_to_design_programs/ch02/ex30.rkt
+++ james_mitchell/how_to_design_programs/ch02/ex30.rkt
@@ -1,15 +1,16 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex29) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex30) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
 (define base-attendance 120)
 (define base-ticket-price 5.0)
 (define attendance-variation 15)
 (define average-price-variation 0.1)
 (define fixed-cost 0)
 (define cost-variation-per-attendee 1.50)
+(define price-sensitivity (/ attendance-variation average-price-variation))
 
 (define (attendees ticket-price)
-  (- base-attendance (* (- ticket-price base-ticket-price) (/ attendance-variation average-price-variation))))
+  (- base-attendance (* (- ticket-price base-ticket-price) price-sensitivity)))
 
 (define (revenue ticket-price)
   (* ticket-price (attendees ticket-price)))
