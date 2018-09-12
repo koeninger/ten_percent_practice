9fd88979ffaab18d0ea42f1332979e5dec5749fa
cody koeninger <cody@koeninger.org>
2018-09-12 14:47:31 -0500

    htdp ex100

--- cody_koeninger/htdp2e/6.1_design_itemizations.rkt
+++ cody_koeninger/htdp2e/6.1_design_itemizations.rkt
@@ -16,8 +16,9 @@
 (define MISSLE (triangle (/ WORLD-WIDTH 20) "solid" "red"))
 (define INITIAL-TANK-POSN (make-posn (/ WORLD-WIDTH 2) (- WORLD-HEIGHT TANK-HEIGHT)))
 (define INITIAL-UFO-POSN (make-posn (/ WORLD-WIDTH 2) UFO-HEIGHT))
-(define COLLISION-PROXIMITY 4)
-  
+(define COLLISION-PROXIMITY (/ UFO-WIDTH 2))
+(define MOVE-PER-TICK 4)
+
 (define INITIAL-SCENE
   (place-images
    (list UFO
@@ -84,6 +85,7 @@
          (make-posn 22 103))
    BACKGROUND))
 
+(define INITIAL-STATE (make-aim INITIAL-UFO-POSN (make-tank (posn-y INITIAL-TANK-POSN) 0)))
 
 ; SIGS -> Image
 ; renders the given game state on top of BACKGROUND 
@@ -112,14 +114,14 @@
 (check-expect (ufo-render (make-posn 10 20) BACKGROUND)
               (place-image UFO 10 20 BACKGROUND))
 (define (ufo-render u im)
-  (place-image UFO (posn-x u) (posn-y u) BACKGROUND))
+  (place-image UFO (posn-x u) (posn-y u) im))
 
 ; Missile Image -> Image 
 ; adds m to the given image im
 (check-expect (missile-render (make-posn 20 40) BACKGROUND)
               (place-image MISSLE 20 40 BACKGROUND))
 (define (missile-render m im)
-  (place-image MISSLE (posn-x m) (posn-y m) BACKGROUND))
+  (place-image MISSLE (posn-x m) (posn-y m) im))
 
 
 ; SIGS -> Boolean
@@ -157,13 +159,58 @@
 ; SIGS -> SIGS
 ; move objects per clock tick, non-testable randomness for ufo move
 (define (si-move s)
-  (si-move-proper s (random COLLISION-PROXIMITY)))
+  (si-move-proper s (* (random MOVE-PER-TICK) (if (even? (random 2)) 1 -1))))
 
 ; SIGS -> number -> SIGS
 ; move objects per clock tick, with number indicating amount of ufo movement
 (check-expect (si-move-proper (make-aim (make-posn 0 0) (make-tank 100 3)) 4)
-              (make-aim (make-posn 4 4) (make-tank 100 3)))
-(check-expect (si-move-proper (make-fired (make-posn 10 0) (make-tank 100 3) (make-posn 100 200)) 3)
-              (make-fired (make-posn 7 4) (make-tank 100 3) (make-posn 100 196)))
+              (make-aim (make-posn 4 4) (make-tank 103 3)))
+(check-expect (si-move-proper (make-fired (make-posn 10 0) (make-tank 100 -3) (make-posn 100 200)) -3)
+              (make-fired (make-posn 7 4) (make-tank 97 -3) (make-posn 100 196)))
 (define (si-move-proper s n)
-  )
+  (cond
+    [(aim? s)
+     (make-aim
+      (make-posn (+ n (posn-x (aim-ufo s))) (+ (posn-y (aim-ufo s)) MOVE-PER-TICK))
+      (make-tank (+ (tank-loc (aim-tank s)) (tank-vel (aim-tank s))) (tank-vel (aim-tank s))))]
+    [(fired? s)
+     (make-fired
+      (make-posn (+ n (posn-x (fired-ufo s))) (+ (posn-y (fired-ufo s)) MOVE-PER-TICK))
+      (make-tank (+ (tank-loc (fired-tank s)) (tank-vel (fired-tank s))) (tank-vel (fired-tank s)))
+      (make-posn (posn-x (fired-missile s)) (- (posn-y (fired-missile s)) MOVE-PER-TICK)))]))
+
+; SIGS -> Key -> SIGS
+; new game state in response to key
+(check-expect (si-control (make-aim (make-posn 0 0) (make-tank 100 3)) "left")
+              (make-aim (make-posn 0 0) (make-tank 100 (* -1 MOVE-PER-TICK))))
+(check-expect (si-control (make-aim (make-posn 0 0) (make-tank 100 -3)) "right")
+              (make-aim (make-posn 0 0) (make-tank 100 MOVE-PER-TICK)))
+(check-expect (si-control (make-aim (make-posn 0 0) (make-tank 100 -3)) " ")
+              (make-fired (make-posn 0 0) (make-tank 100 -3) (make-posn 100 (- WORLD-HEIGHT TANK-HEIGHT))))
+(check-expect (si-control (make-fired (make-posn 0 0) (make-tank 100 3) (make-posn 0 0)) "left")
+              (make-fired (make-posn 0 0) (make-tank 100 (* -1 MOVE-PER-TICK)) (make-posn 0 0)))
+(check-expect (si-control (make-fired (make-posn 0 0) (make-tank 100 -3) (make-posn 0 0)) "right")
+              (make-fired (make-posn 0 0) (make-tank 100 MOVE-PER-TICK) (make-posn 0 0)))
+(check-expect (si-control (make-fired (make-posn 0 0) (make-tank 100 -3) (make-posn 100 10)) " ")
+              (make-fired (make-posn 0 0) (make-tank 100 -3) (make-posn 100 10)))
+(define (si-control s k)
+  (cond
+    [(and (aim? s) (key=? k "left"))
+     (make-aim (aim-ufo s) (make-tank (tank-loc (aim-tank s)) (* -1 MOVE-PER-TICK)))]
+    [(and (aim? s) (key=? k "right"))
+     (make-aim (aim-ufo s) (make-tank (tank-loc (aim-tank s)) MOVE-PER-TICK))]
+    [(and (aim? s) (key=? k " "))
+     (make-fired (aim-ufo s) (aim-tank s) (make-posn (tank-loc (aim-tank s)) (- WORLD-HEIGHT TANK-HEIGHT)))]
+    [(and (fired? s) (key=? k "left"))
+     (make-fired (fired-ufo s) (make-tank (tank-loc (fired-tank s)) (* -1 MOVE-PER-TICK)) (fired-missile s))]
+    [(and (fired? s) (key=? k "right"))
+     (make-fired (fired-ufo s) (make-tank (tank-loc (fired-tank s)) MOVE-PER-TICK) (fired-missile s))]
+    [else s]))
+
+
+(define (si-main s)
+  (big-bang s
+    (on-tick si-move 0.1)
+    (on-key si-control)
+    (to-draw si-render)
+    (stop-when si-game-over? si-render-final)))
