f0a9c89726eb85cf0958bc986a0840dfed0c190b
hokey <mike.hochanadel@lin-digital.com>
2018-09-19 15:00:55 -0500

    HTDP2ed chapter 3 exercises 34-35

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch3/35_interactions.rkt
@@ -0,0 +1,12 @@
+Welcome to DrRacket, version 7.0 [3m].
+Language: Beginning Student [custom]; memory limit: 128 MB.
+Teachpacks: 2htdp/image.rkt, universe.rkt, and batch-io.rkt.
+> (last-string "String")
+"g"
+> (last-string "42")
+"2"
+> (last-string "(#SDLKF23lksdg)")
+")"
+> (last-string "B")
+"B"
+> 
