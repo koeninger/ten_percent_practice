00cdf1dc259191d7724386331852bb8d5e2d09f6
Chris Larson <clarson@kixer.com>
2018-09-19 14:30:49 -0500

    50

--- chris_larson/htdp/Exercise 48.rkt
+++ chris_larson/htdp/Exercise 48.rkt
@@ -9,3 +9,13 @@
     [else "gold"]))
 (define y 210)
 (- 200 (cond [(> y 200) 0] [else y]))
+; TrafficLight -> TrafficLight
+; yields the next state given current state s
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
