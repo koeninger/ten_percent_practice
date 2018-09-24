368578de761c78ba2598b7c523fc19878b6f3eb2
cody koeninger <cody@koeninger.org>
2018-09-24 16:32:43 -0500

    plc 1.3.1

---
+++ cody_koeninger/sestoft-plc/sestoft-plc/Introduction.fsx
@@ -0,0 +1,12 @@
+﻿type expr =
+    | CstI of int
+    | Prim of string * expr * expr
+
+let rec eval (e : expr) : int =
+    match e with
+    | CstI i -> i
+    | Prim("+", e1, e2) -> eval e1 + eval e2
+    | Prim("*", e1, e2) -> eval e1 * eval e2
+    | Prim("-", e1, e2) -> eval e1 - eval e2
+    | Prim _ -> failwith "unknown primitive"
+
