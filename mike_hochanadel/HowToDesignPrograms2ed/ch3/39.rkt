762e9dcc505957ce585a61d9bd0cc293090550e1
hokey <mike.hochanadel@lin-digital.com>
2018-09-26 14:58:25 -0500

    HTDP2ed chapter 3 exercise 39

--- mike_hochanadel/HowToDesignPrograms2ed/ch3/39.rkt
+++ mike_hochanadel/HowToDesignPrograms2ed/ch3/39.rkt
@@ -1,7 +1,7 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |39|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
-(define WHEEL-RADIUS 15)
+(define WHEEL-RADIUS 50)
 (define WHEEL-DISTANCE (* WHEEL-RADIUS 5))
 (define WHEEL
   (circle WHEEL-RADIUS "solid" "black"))
@@ -9,3 +9,13 @@
     (rectangle WHEEL-DISTANCE WHEEL-RADIUS "solid" "white"))
 (define BOTH-WHEELS
     (beside WHEEL SPACE WHEEL))
+(define BODY-DISTANCE (+ (* WHEEL-RADIUS 4) WHEEL-DISTANCE))
+(define BODY-HEIGHT (* WHEEL-RADIUS 1.5))
+(define BODY-MIDDLE
+  (rectangle BODY-DISTANCE BODY-HEIGHT "solid" "red"))
+(define BODY-TOP
+  (rectangle WHEEL-DISTANCE BODY-HEIGHT "solid" "red"))
+(define BODY
+  (above BODY-TOP BODY-MIDDLE))
+(define CAR
+  (above BODY BOTH-WHEELS))
