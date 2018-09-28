10063cd7d101516b4391b4f4db4ab3437bea927f
cody koeninger <cody@koeninger.org>
2018-09-28 13:48:20 -0500

    plc 2.9

--- cody_koeninger/sestoft-plc/sestoft-plc/InterpretersCompilers.fsx
+++ cody_koeninger/sestoft-plc/sestoft-plc/InterpretersCompilers.fsx
@@ -172,4 +172,24 @@ type rinstr =
     | RDup
     | RSwap
 
+let rec reval (inss : rinstr list) (stack : int list) : int =
+    match (inss, stack) with
+    | ([], v :: _) -> v
+    | ([], []) -> failwith "reval: no result on stack"
+    | (RCstI i :: insr, stk) -> reval insr (i :: stk)
+    | (RAdd :: insr, i2 :: i1 :: stkr) -> reval insr ((i1 + i2) :: stkr)
+    | (RSub :: insr, i2 :: i1 :: stkr) -> reval insr ((i1 - i2) :: stkr)
+    | (RMul :: insr, i2 :: i1 :: stkr) -> reval insr ((i1 * i2) :: stkr)
+    | (RDup :: insr, i1 :: stkr) -> reval insr (i1 :: i1 :: stkr)
+    | (RSwap :: insr, i2 :: i1 :: stkr) -> reval insr (i1 :: i2 :: stkr)
+    | _ -> failwith "reval: too few operands on stack"
+
+type sinstr =
+    | SCStI of int
+    | SVar of int
+    | SAdd
+    | SSub
+    | SMul
+    | SPop
+    | SSwap
 
