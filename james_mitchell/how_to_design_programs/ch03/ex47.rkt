173d41f7ee63d015077745fc9e9e685bf973cad8
jmitchell <jmitchell@hyfnlocal.com>
2018-09-28 14:33:18 -0500

    ex47 WIP

---
+++ james_mitchell/how_to_design_programs/ch03/ex47.rkt
@@ -0,0 +1,22 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex47) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define WIDTH-OF-WORLD 400)
+(define HEIGHT-OF-WORLD 100)
+
+(define decrease 0.1)
+(define down-increase 1.2)
+(define up-increase 1.33)
+
+
+
+; WorldState -> WorldState
+; launches the program from some initial state
+(define (main ws)
+   (big-bang ws
+     [on-tick tock]
+     [to-draw render]
+     [
+
+
+
