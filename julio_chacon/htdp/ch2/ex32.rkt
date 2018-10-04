70f55a13be9d5b4f9f64d344c7e5329032abd787
Julio Chacon <julio.chacon@lin-digital.com>
2018-10-04 15:18:45 -0500

    ex38

--- julio_chacon/htdp/ch2/ex32.rkt
+++ julio_chacon/htdp/ch2/ex32.rkt
@@ -10,3 +10,19 @@
 
 (define (reset s ke)
   100)
+
+(define BACKGROUND (empty-scene 100 100))
+(define DOT (circle 3 "solid" "red"))
+ 
+(define (main y)
+  (big-bang y
+    [on-tick sub1]
+    [stop-when zero?]
+    [to-draw place-dot-at]
+    [on-key stop]))
+ 
+(define (place-dot-at y)
+  (place-image DOT 50 y BACKGROUND))
+ 
+(define (stop y ke)
+  0)
