e78f74f0adacbc5f48a8c05070f6c8d1ff512942
Justin Nguyen <jnguyen@kixer.com>
2018-10-22 15:23:21 -0500

    ch5

---
+++ justin_nguyen/htdp/ch5/posn.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname posn) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; (define one-posn (make-posn 8 6))
+
+; one-posn is a point in and xy coordinate system represented by two numbers: (8, 6) 
