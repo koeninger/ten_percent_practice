6e892e6762c757949ccf52d84b2a3b626827201c
jmitchell <jmitchell@hyfnlocal.com>
2018-10-26 14:45:25 -0500

    ex53

--- james_mitchell/how_to_design_programs/ch04/ex53.rkt
+++ james_mitchell/how_to_design_programs/ch04/ex53.rkt
@@ -3,9 +3,12 @@
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname ex53) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 (define HEIGHT 300) ; distances in pixels 
 (define WIDTH  100)
+(define YDELTA 3)
  
-(define BACKGROUND  (empty-scene WIDTH HEIGHT))
+(define BACKG  (empty-scene WIDTH HEIGHT))
 (define ROCKET (rectangle 5 30 "solid" "red"))
+ 
+(define CENTER (/ (image-height ROCKET) 2))
 
 
 ; An LR (short for launching rocket) is one of:
@@ -15,3 +18,12 @@
 ; a number denotes the height of a rocket in flight
 (define (LR height)
   (place-image ROCKET (/ WIDTH 2) (- HEIGHT height 5) BACKGROUND))
+
+
+; An LRCD (for launching rocket countdown) is one of:
+; – "resting"
+; – a Number between -3 and -1
+; – a NonnegativeNumber 
+; interpretation a grounded rocket, in countdown mode,
+; a number denotes the number of pixels between the
+; top of the canvas and the rocket (its height)
