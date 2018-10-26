ef34502bb4557e6243b2758bb0f84a1921645e1d
jmitchell <jmitchell@hyfnlocal.com>
2018-10-26 14:44:16 -0500

    ex53

---
+++ james_mitchell/how_to_design_programs/ch04/ex53.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex53) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define HEIGHT 300) ; distances in pixels 
+(define WIDTH  100)
+ 
+(define BACKGROUND  (empty-scene WIDTH HEIGHT))
+(define ROCKET (rectangle 5 30 "solid" "red"))
+
+
+; An LR (short for launching rocket) is one of:
+; – "resting"
+; – NonnegativeNumber
+; interpretation "resting" represents a grounded rocket
+; a number denotes the height of a rocket in flight
+(define (LR height)
+  (place-image ROCKET (/ WIDTH 2) (- HEIGHT height 5) BACKGROUND))
