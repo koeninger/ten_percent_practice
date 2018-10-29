2b18146cd721d9a461421d4db8753eaa74f48276
jvelis <jvelis@kixer.com>
2018-10-29 14:57:53 -0500

    ex70 wip

---
+++ juan_velis/htdp/ch02/69.rkt
@@ -0,0 +1,27 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |69|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; entry
+; Name, Phone, Email
+; John Smith, 111-1111, john@fubar.com
+
+
+; movie
+; title, producer, year]
+; Gladiator, John Smith, 2000
+
+; person
+; name, hair, eyes, phone
+; John Smith, Brown, 123-4567
+
+; pet
+; name, number
+; Rocky, 1
+
+; CD
+; artist, title, price
+; John, Album title, 15.99
+
+; sweater
+; material, size, producer
+; cotton, M, good threads
