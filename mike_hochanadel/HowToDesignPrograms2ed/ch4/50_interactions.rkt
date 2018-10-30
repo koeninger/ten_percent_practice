b8a22aaa16170159ab7370e9a5568fa5769e6395
hokey <mike.hochanadel@lin-digital.com>
2018-10-30 15:44:41 -0500

    HTDP2ed chapter 4, exercise 50. Also move 48 - 49 to ch4 folder

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch4/50_interactions.rkt
@@ -0,0 +1,9 @@
+Welcome to DrRacket, version 7.0 [3m].
+Language: Beginning Student [custom]; memory limit: 128 MB.
+Teachpacks: 2htdp/image.rkt, universe.rkt, and batch-io.rkt.
+All 4 tests passed!
+> (traffic-light-next "green")
+"yellow"
+> (traffic-light-next "sdfdsf")
+cond: all question results were false
+> 
