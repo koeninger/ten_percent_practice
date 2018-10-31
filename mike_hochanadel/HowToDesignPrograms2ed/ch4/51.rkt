e2c753e2d3bee99588c0f9f7bd4604407afb3e35
hokey <mike.hochanadel@lin-digital.com>
2018-10-31 15:20:41 -0500

    HTDP2ed chapter 4 exercise 51 starting

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch4/51.rkt
@@ -0,0 +1,41 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |51|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; A TrafficLight is one of the following Strings:
+; – "red"
+; – "green"
+; – "yellow"
+; interpretation the three strings represent the three 
+; possible states that a traffic light may assume
+(define TrafficLight "red")
+
+; TrafficLight -> TrafficLight
+; yields the next state given current state s
+(check-expect (traffic-light-next "red") "green")
+(check-expect (traffic-light-next "green") "yellow")
+(check-expect (traffic-light-next "yellow") "red")
+(check-error (traffic-light-next "bob"))
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
+
+; WorldState -> Image
+; prints a circle with the given circle
+(define (render s)
+    (circle 20 "solid" s))
+
+; WorldState -> WorldState
+; subtracts the given number by one
+(define (tock ws)
+  (- ws 1))
+
+; WorldState -> WorldState
+; launches the program from some initial state
+(define (traffic-light-prog ws)
+  (big-bang ws
+    [on-tick tock]
+    [to-draw render]
+    [stop-when zero?]))
+    
