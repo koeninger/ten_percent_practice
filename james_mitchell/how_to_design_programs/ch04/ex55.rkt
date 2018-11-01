e59034f0132e5fc81fb01119f510868ceccfbd35
jmitchell <jmitchell@hyfnlocal.com>
2018-11-01 14:46:17 -0500

    ex55

---
+++ james_mitchell/how_to_design_programs/ch04/ex55.rkt
@@ -0,0 +1,34 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex55) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define HEIGHT 300) ; distances in pixels 
+(define WIDTH  100)
+(define YDELTA 3)
+ 
+(define BACKG  (empty-scene WIDTH HEIGHT))
+(define ROCKET (rectangle 5 30 "solid" "red"))
+ 
+(define CENTER (/ (image-height ROCKET) 2))
+
+(define (show x)
+  (cond
+    [(string? x)
+     (place-rocket HEIGHT)]
+    [(<= -3 x -1)
+     (place-image (text (number->string x) 20 "red")
+                  10 (* 3/4 WIDTH)
+                  (place-rocket HEIGHT))]
+    [(>= x 0)
+     (place-rocket x)]))
+
+(define (place-rocket x)
+  (place-image ROCKET 10 (- x CENTER) BACKG))
+(check-expect
+ (show HEIGHT)
+ (place-image ROCKET 10 (- HEIGHT CENTER) BACKG))
+(check-expect
+ (show HEIGHT)
+ (place-image ROCKET 10 (- HEIGHT CENTER) BACKG))
+(check-expect
+ (show 53)
+ (place-image ROCKET 10 (- 53 CENTER) BACKG))
