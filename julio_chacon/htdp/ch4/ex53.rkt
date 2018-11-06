7534bbd2dd0bf7ad3195dbbd30e34e31b9caeb81
Julio Chacon <jchacon@hyfnlocal.com>
2018-11-05 23:45:10 -0600

    ex53

---
+++ julio_chacon/htdp/ch4/ex53.rkt
@@ -0,0 +1,32 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex53) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define HEIGHT 300) ; distances in pixels 
+(define WIDTH  100)
+(define YDELTA 3)
+ 
+(define BACKG  (empty-scene WIDTH HEIGHT))
+(define ROCKET (rectangle 5 30 "solid" "red"))
+ 
+(define CENTER (/ (image-height ROCKET) 2))
+
+; An LR (short for launching rocket) is one of:
+; – "resting"
+; – NonnegativeNumber
+; interpretation "resting" represents a grounded rocket
+; a number denotes the height of a rocket in flight
+
+(define (LR rh)
+  (cond
+    [(false? (string->number rh))  0]
+    [ (> rh 0) (+ rh 3)]) 
+  )
+
+
+(check-expect (LR "resting") 0)
+
+;(check-expect (LR HEIGHT) 303)
+
