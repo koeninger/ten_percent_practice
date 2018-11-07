f1e5737caa4e9e2d990b192e63de231b20d628ef
hokey <mike.hochanadel@lin-digital.com>
2018-11-07 15:31:56 -0600

    HTDP2ed chapter 4 exercise 54

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch4/54.rkt
@@ -0,0 +1,20 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |54|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; An LRCD (for launching rocket countdown) is one of:
+; – "resting"
+; – a Number between -3 and -1
+; – a NonnegativeNumber 
+; interpretation a grounded rocket, in countdown mode,
+; a number denotes the number of pixels between the
+; top of the canvas and the rocket (its height)
+
+; the state of the LRCD when resting is any type of value that is not a Number or negative Number
+; the label itself is "resting" and if thinking in intervals any string type would be like a open
+; ended bracket for all strings
+
+(define (show x)
+  (cond
+    [(string? x) ...]
+    [(<= -3 x -1) ...]
+    [(>= x 0) ...]))
