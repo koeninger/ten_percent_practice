c7c2c2007e7b7372e77a337d2aeca88226840eee
Edilberto Garcia <egarcia@federatedmedia.net>
2018-11-07 15:34:22 -0500

    fix 54

--- edilberto_garcia/how_to_design_programs_second_edition/04_54.rkt
+++ edilberto_garcia/how_to_design_programs_second_edition/04_54.rkt
@@ -13,8 +13,23 @@
     [(<= -3 x -1) ...]
     [(>= x 0) ...]))
 
+; string=? checks for string equality, not that the input is a string
 
-; You're passing in the variable HEIGHT to show which is a number not a
-;    string
+
+(define (show x)
+  (cond
+    [(string? x)
+     (place-image ROCKET 10 (- HEIGHT CENTER) BACKG)]
+    [(<= -3 x -1)
+     (place-image (text (number->string x) 20 "red")
+                  10 (* 3/4 WIDTH)
+                  (place-image ROCKET
+                               10 (- HEIGHT CENTER)
+                               BACKG))]
+    [(>= x 0)
+     (place-image ROCKET 10 (- x CENTER) BACKG)]))
+
+
+
 
 
