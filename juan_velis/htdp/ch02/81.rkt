28a964edc399851c736742487a96af01a71d2f73
jvelis <jvelis@kixer.com>
2018-11-12 15:11:01 -0600

    ex82 wip

---
+++ juan_velis/htdp/ch02/81.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |81|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define-struct time-point [hours minutes seconds])
+; (make-time-pt Number Number Number))
+
+(define midnight (make-time-point 00 00 00))  ; midnight
+(define time (make-time-point 12 30 02))  ; 
+
+
+(define (time->seconds t)
+  (+ (* (time-point-hours t) 3600)
+       (* (time-point-minutes t) 60)
+       (time-point-seconds t))
+  )
+
+(time->seconds time)
