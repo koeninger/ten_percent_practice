e91eaaa5d0f400b21c081bc3f3626cee9740b287
Julio Chacon <jchacon@hyfnlocal.com>
2018-11-20 17:13:46 -0600

    start of ex59

---
+++ julio_chacon/htdp/ch4/ex59.rkt
@@ -0,0 +1,31 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex59) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+; TrafficLight -> TrafficLight
+; yields the next state, given current state cs
+(define (tl-next cs) cs)
+ 
+; TrafficLight -> Image
+; renders the current state cs as an image
+(define (tl-render current-state)
+  (empty-scene 90 30))
+
+; TrafficLight -> TrafficLight
+; simulates a clock-based American traffic light
+(define (traffic-light-simulation initial-state)
+  (big-bang initial-state
+    [to-draw tl-render]
+    [on-tick tl-next 1]))
+
+
+; TrafficLight -> TrafficLight
+; yields the next state given current state s
+(check-expect (traffic-light-next "red") "green")
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
