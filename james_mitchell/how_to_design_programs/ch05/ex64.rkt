e91931322fe5ed168097f4166ffc0647b00c1acc
jmitchell <jmitchell@hyfnlocal.com>
2018-11-21 14:55:39 -0600

    ex64

---
+++ james_mitchell/how_to_design_programs/ch05/ex64.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex64) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+;Compute the manhattan-distance from a posn to origin
+(check-expect (manhattan-distance (make-posn 0 0)) 0)
+(check-expect (manhattan-distance (make-posn 15 4)) 19)
+
+(define (manhattan-distance p)
+  (+ (posn-x p)
+     (posn-y p)))
