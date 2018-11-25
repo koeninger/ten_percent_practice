0717112d2048923e4ff56ef3023c71f013453053
Eric Kudler <joeento@gmail.com>
2018-11-25 02:49:26 -0500

    Ex 51

---
+++ eric_kudler/htdp/chapter2/ex51.rkt
@@ -0,0 +1,25 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex51) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define (render x)
+  (circle 20 "solid" x))
+
+(define (traffic-light-next s)
+  (cond
+    [(string=? "red" s) "green"]
+    [(string=? "green" s) "yellow"]
+    [(string=? "yellow" s) "red"]))
+
+(define (tock x)
+  (traffic-light-next x))
+
+(define (main ws)
+   (big-bang ws
+     [on-tick tock]
+     [to-draw render]
+    ))
+
+(main "green")
