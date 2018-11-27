4ed2ac3c075f21f49c6f39ef3d2849d5a9c470f1
Edilberto Garcia <egarcia@federatedmedia.net>
2018-11-27 15:47:20 -0500

    Door sample problem

---
+++ edilberto_garcia/how_to_design_programs_second_edition/04_61.5_sample_problem_door_lock.rkt
@@ -0,0 +1,71 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname 04_61.5_sample_problem_door_lock) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; Sample Problem Design a world program that simulates the working of
+;    a door with an automatic door closer. If this kind of door is locked,
+;    you can unlock it with a key. An unlocked door is closed, but someone
+;    pushing at the door opens it. Once the person has passed through the door
+;    and lets go, the automatic door takes over and closes the door again. When
+;    a door is closed, it can be locked again.
+
+
+
+(require 2htdp/universe)
+(require 2htdp/image)
+                         ; A DoorState is one of:
+(define LOCKED "locked") ; – LOCKED
+(define CLOSED "closed") ; – CLOSED
+(define OPEN "open")     ; – OPEN
+   
+
+
+; DoorState -> DoorState
+; closes an open door over the period of one tick
+(check-expect (door-closer LOCKED) LOCKED)
+(check-expect (door-closer CLOSED) CLOSED)
+(check-expect (door-closer OPEN) CLOSED)
+(define (door-closer state-of-door)
+  (cond
+    [(string=? LOCKED state-of-door) LOCKED]
+    [(string=? CLOSED state-of-door) CLOSED]
+    [(string=? OPEN state-of-door) CLOSED]))
+
+
+; DoorState KeyEvent -> DoorState
+; turns key event k into an action on state s 
+(check-expect (door-action LOCKED "u") CLOSED)
+(check-expect (door-action CLOSED "l") LOCKED)
+(check-expect (door-action CLOSED " ") OPEN)
+(check-expect (door-action OPEN "a") OPEN)
+(check-expect (door-action CLOSED "a") CLOSED)
+ 
+(define (door-action s k)
+  (cond
+    [(and (string=? LOCKED s) (string=? "u" k))
+     CLOSED]
+    [(and (string=? CLOSED s) (string=? "l" k))
+     LOCKED]
+    [(and (string=? CLOSED s) (string=? " " k))
+     OPEN]
+    [else s]))
+
+
+; DoorState -> Image
+; translates the state s into a large text image
+(check-expect (door-render CLOSED)
+              (text CLOSED 40 "red"))
+(define (door-render s)
+  (text s 40 "red"))
+
+
+
+; DoorState -> DoorState
+; simulates a door with an automatic door closer
+(define (door-simulation initial-state)
+  (big-bang initial-state
+    [on-tick door-closer]
+    [on-key door-action]
+    [to-draw door-render]))
+
+
+
