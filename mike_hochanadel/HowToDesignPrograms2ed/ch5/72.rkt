4bb4a665a46febefaf911dd1ad8c3c0f0a6c0a38
hokey <mike.hochanadel@lin-digital.com>
2018-11-30 14:01:24 -0600

    HTDP2ed chapter 5 exercise 72

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch5/72.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |72|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define-struct phone number)
+; a phone is a structure:
+;    (make-phone String)
+; interpretation a string representing a number
+
+
+(define-struct phone# [area switch num])
+; a phone# is a structure:
+;    (make-phone# Number Number Number)
+; interpretation where values are [000-999] [000-000] [0000-0000]
