c69aef9cea8cf247f866fda8fbf232ddbc6819f6
jmitchell <jmitchell@hyfnlocal.com>
2018-12-03 15:14:02 -0600

    ex72

---
+++ james_mitchell/how_to_design_programs/ch05/ex72.rkt
@@ -0,0 +1,10 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex72) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define-struct phone# [area switch num])
+; A Phone Number is a structure: 
+;   (make-phone# Number Number Number)
+; interpretation (make-phone# area switch num) means a phone number of
+; area code made of 3 digits 000-999
+; phone switch (exchange) made of 3 digits 000-999 
+; phone number made of 4 digits 0000-9999
