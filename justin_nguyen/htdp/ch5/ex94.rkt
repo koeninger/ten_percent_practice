b61fd182d8f0a5e01a145d097bc9f089e6100e04
Justin Nguyen <jnguyen@kixer.com>
2018-12-03 15:30:46 -0600

    94+95

--- justin_nguyen/htdp/ch5/ex94.rkt
+++ justin_nguyen/htdp/ch5/ex94.rkt
@@ -4,11 +4,17 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 
-(define SCENE-WIDTH 500)
-(define SCENE-HEIGHT 300)
+(define SCENE-WIDTH 400)
+(define SCENE-HEIGHT 200)
+(define BUILD-WIDTH 20)
+(define BUILD-COLOR "dimgray")
+(define BUILD-WINDOW-COLOR "gray")
+(define WINDOW (rectangle 15 15 "solid" BUILD-WINDOW-COLOR))
+(define BUILDING-UNIT-WINDOW (overlay/offset WINDOW
+                                             0 0
+                                             (rectangle BUILD-WIDTH BUILD-WIDTH "solid" BUILD-COLOR)))
+(define BUILDING-UNIT (rectangle BUILD-WIDTH BUILD-WIDTH "solid" BUILD-COLOR))
 
-(define BACKGROUND SCENE-WIDTH SCENE-HEIGHT "blue")
-  (empty-scene
 (define TANK (rectangle 70 20 "solid" "green"))
 (define UFO (overlay
              (beside (circle 4 "solid" "white")
@@ -17,3 +23,86 @@
                      (circle 4 "solid" "white")
                      (circle 4 "solid" "white"))
              (ellipse 80 20 "solid" "gray")))
+
+(define BUILDING1
+  (beside/align "bottom"
+                (above BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT
+                       BUILDING-UNIT-WINDOW)
+                (above BUILDING-UNIT
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT)))
+
+(define BUILDING2
+  (beside/align "bottom"
+                (above BUILDING-UNIT
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT-WINDOW)
+                (above BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT)
+                (above BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT)))
+
+(define BUILDING3
+  (beside/align "bottom"
+                (above BUILDING-UNIT
+                       BUILDING-UNIT-WINDOW)
+                (above BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT-WINDOW)))
+(define BUILDING4
+  (beside/align "bottom"
+                (above BUILDING-UNIT
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT
+                       BUILDING-UNIT)
+                (above BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT
+                       BUILDING-UNIT-WINDOW
+                       BUILDING-UNIT)))
+
+(define BACKGROUND
+  (underlay/align/offset "left" "bottom" (empty-scene SCENE-WIDTH SCENE-HEIGHT "blue")
+                         0 0
+                         (beside/align "bottom"
+                                       BUILDING1
+                                       BUILDING2
+                                       BUILDING3
+                                       BUILDING1
+                                       BUILDING3
+                                       BUILDING2
+                                       BUILDING1
+                                       BUILDING4
+                                       BUILDING3)))
+
+
+
+; a UFO is a Posn
+; interpretation (make-posn x y) is the UFO's location
+; (using top-down, left-right convention)
+
+(define-struct tank [loc vel])
+; a Tank is a structure:
+;  (make-tank Number Number)
+; interpretation (make-tank x dx) specifies the pos:
+; (x, HEIGHT) and the tank's speed: dx pixels/tick
+
+; a Missle is a Posn
+; interpretation (make-posn x y) is the missle's location
+
+
+; a SIGS is one of:
+; - (make-aim UFO Tank)
+; - (make-fired UFO Tank Missle)
+; interpretation represents complete state of a space invader game
+
+
+
+
