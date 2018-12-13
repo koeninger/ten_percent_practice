78ab74e43761a09c9d6b1a0d5ed76c3f906b3020
Chris Larson <clarson@kixer.com>
2018-12-13 14:57:59 -0600

    140

--- chris_larson/htdp/Exercise 129.rkt
+++ chris_larson/htdp/Exercise 129.rkt
@@ -46,3 +46,32 @@
 ; - (cons Boolean List-of-booleans)
 ; interpretation a list of booleans
 
+
+(define-struct pair [left right])
+; A ConsPair is a structure:
+;   (make-pair Any Any).
+ 
+; A ConsOrEmpty is one of: 
+; – '()
+; – (make-pair Any ConsOrEmpty)
+; interpretation ConsOrEmpty is the class of all lists
+ 
+; Any Any -> ConsOrEmpty
+(define (our-cons a-value a-list)
+  (cond
+    [(empty? a-list) (make-pair a-value a-list)]
+    [(pair? a-list) (make-pair a-value a-list)]
+    [else (error "cons: second argument ...")]))
+
+; ConsOrEmpty -> Any
+; extracts the left part of the given pair
+(define (our-first a-list)
+  (if (empty? a-list)
+      (error 'our-first "...")
+      (pair-left a-list)))
+
+; ConsOrEmpty -> List
+; extracts the right part of the given pair
+(define (our-rest a-list)
+  (pair-right a-list))
+
