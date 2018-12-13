d2c89cd768cdbfe4c8849b62ac04f54a71d9dd8a
cody koeninger <cody@koeninger.org>
2018-12-13 12:21:23 -0600

    htdp ex203

--- cody_koeninger/htdp2e/12.2_itunes.rkt
+++ cody_koeninger/htdp2e/12.2_itunes.rkt
@@ -4,6 +4,9 @@
 (require 2htdp/itunes)
 
 (define example-date (create-date 2018 12 1 11 23 45))
+(define example-date2 (create-date 2018 12 2 11 23 45))
+(define example-date3 (create-date 2018 12 3 11 23 45))
+
 
 (define example-track
   (create-track
@@ -25,7 +28,7 @@
    22
    example-date
    3
-   example-date))
+   example-date2))
 
 
 (define example-track3
@@ -35,9 +38,9 @@
    "laugh? I nearly bought one"
    666
    22
-   example-date
+   example-date2
    3
-   example-date))
+   example-date3))
 
 (define example-Ltrack (list example-track))
 
@@ -91,3 +94,48 @@
      (if (string=? a (track-album (first ts)))
          (cons (first ts) (select-album a (rest ts)))
          (select-album a (rest ts)))]))
+
+; ex 203
+; date date -> boolean
+; is a before b
+(check-expect (date-before? (create-date 2018 12 1 11 23 45) (create-date 2018 12 1 12 23 45))
+              #true)
+(check-expect (date-before? (create-date 2018 12 1 11 23 45) (create-date 2018 12 1 11 23 45))
+              #false)
+(define (date-before? a b)
+  (cond
+   [(> (date-year a) (date-year b)) #f]
+   [(< (date-year a) (date-year b)) #t]
+   [else
+    (cond
+      [(> (date-month a) (date-month b)) #f]
+      [(< (date-month a) (date-month b)) #t]
+      [else
+       (cond
+         [(> (date-day a) (date-day b)) #f]
+         [(< (date-day a) (date-day b)) #t]
+         [else
+          (cond
+            [(> (date-hour a) (date-hour b)) #f]
+            [(< (date-hour a) (date-hour b)) #t]
+            [else
+             (cond
+               [(> (date-minute a) (date-minute b)) #f]
+               [(< (date-minute a) (date-minute b)) #t]
+               [else
+                (< (date-second a) (date-second b))])])])])]))
+
+; string date ltracks -> ltracks
+; tracks that belong to album and have been played after date
+(check-expect (select-album-date (track-album example-track) example-date (list example-track example-track2 example-track3))
+              (list example-track2))
+(check-expect (select-album-date (track-album example-track) example-date2 (list example-track example-track2 example-track3))
+              '())
+(define (select-album-date a d ts)
+  (cond
+    [(empty? ts) '()]
+    [(cons? ts)
+     (if (and (string=? a (track-album (first ts)))
+              (date-before? d (track-played (first ts))))
+         (cons (first ts) (select-album-date a d (rest ts)))
+         (select-album-date a d (rest ts)))]))
