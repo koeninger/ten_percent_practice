f2c3e8bfebc8619c5c5d005ae13b26aa90e0fda4
mtruong92 <mistermichaeltruong@gmail.com>
2018-12-16 22:38:04 -0600

    82

--- michael_truong/How to Design Programs 2e/Chapter 5/Exercise 77.rkt
+++ michael_truong/How to Design Programs 2e/Chapter 5/Exercise 77.rkt
@@ -1,4 +1,3 @@
 #lang racket
 
 (define-struct time [hours minutes seconds])
-; Time is (make-time Number Number Number)
--- michael_truong/How to Design Programs 2e/Chapter 5/Exercise 78.rkt
+++ michael_truong/How to Design Programs 2e/Chapter 5/Exercise 78.rkt
@@ -1,4 +1,3 @@
 #lang racket
 
-(define-struct 3String [l1 l2 l3])
-; 3String is (make 3String (1String | #false) (1String | #false) (1String | #false))
+(define-struct 3String [c1 c2 c3])
