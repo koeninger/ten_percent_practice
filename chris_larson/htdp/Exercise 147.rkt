b52443a245634062cb630e4b52d5fd8fbc0a7f61
Chris Larson <clarson@kixer.com>
2018-12-18 14:56:44 -0600

    153

--- chris_larson/htdp/Exercise 147.rkt
+++ chris_larson/htdp/Exercise 147.rkt
@@ -56,3 +56,30 @@
   (cond
     [(zero? n) 0]
     [(positive? n) (+ x (multiply (sub1 n) x))]))
+
+
+; NaturalNumber Image -> Image
+; produces a column of vertical arragnement of n copies of the image
+(define (col n img)
+  (cond
+    [(zero? n) (square 1 "solid" "white")]
+    [(positive? n) (above/align "left" img (col (sub1 n) img))]))
+
+
+; NaturalNumber Image -> Image
+; produces a column of horizontal arragnement of n copies of the image
+(define (row n img)
+  (cond
+    [(zero? n) (square 1 "solid" "white")]
+    [(positive? n) (beside/align "top" img (row (sub1 n) img))]))
+
+
+; List-of-posn image -> image
+; places red dots at the list of posn
+(define (add-balloons lop img)
+  (cond
+    [(empty? lop) img]
+    [else (place-image (circle 5 "solid" "red") (posn-x (first lop)) (posn-y (first lop)) (add-balloons (rest lop) img))]))
+
+(place-image (row 8 (col 18 (square 20 "outline" "black"))) (* 4 20) (* 9 20) (empty-scene (* 8 20) (* 18 20)))
+(add-balloons (cons (make-posn 10 20) (cons (make-posn 80 60) '())) (place-image (row 8 (col 18 (square 20 "outline" "black"))) (* 4 20) (* 9 20) (empty-scene (* 8 20) (* 18 20))))
