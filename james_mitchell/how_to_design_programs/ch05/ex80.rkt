1aed25a36e2fe3c7d3cd7a0f1003b2d47a20df71
jmitchell <jmitchell@hyfnlocal.com>
2018-12-19 15:45:17 -0600

    ex80

---
+++ james_mitchell/how_to_design_programs/ch05/ex80.rkt
@@ -0,0 +1,30 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex80) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define-struct movie [title producer year])
+(define-struct movie [title producer year])
+; movie is (make-movie String String Number)
+(define film (make-movie "Se7en" "Brad Pitt" 1995))
+(movie-title film)
+(movie-producer film)
+(movie-year film)
+
+(define-struct pet [name number])
+; pet is (make-pet String Number)
+(define dog (make-pet "Hans" 2008))
+(pet-name dog)
+(pet-number dog)
+
+(define-struct CD [artist title price])
+; CD is (make-CD String String Number)
+(define album (make-CD "Offspring" "Ixnay on the Hombre" 17.99))
+(CD-artist album)
+(CD-title album)
+(CD-price album)
+
+(define-struct sweater [material size producer])
+; sweater is (make-sweater String String String)
+(define mysweater (make-sweater "Wool" "extra large" "Kohls"))
+(sweater-material mysweater)
+(sweater-size mysweater)
+(sweater-producer mysweater)
