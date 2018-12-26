e3873233bfa3e13f9c1f6ef56dd2562e03c49777
hokey <mike.hochanadel@lin-digital.com>
2018-12-26 15:03:56 -0600

    HTDP2ed chapter 5 exercise 87 continued

--- mike_hochanadel/HowToDesignPrograms2ed/ch5/87.rkt
+++ mike_hochanadel/HowToDesignPrograms2ed/ch5/87.rkt
@@ -15,75 +15,101 @@
 (define CURSOR (rectangle 1 20 "solid" "red"))
 
 
-; String -> 1String
-; string-first extracts the first character from a non-empty string
-(check-expect (string-first "String") "S")
-(check-expect (string-first "42") "4")
-(check-expect (string-first "") "")
-(define (string-first string)
-  (if (and (string? string) (> (string-length string) 0))
-      (string-ith string 0)
-      ""
-      ))
-
-; String -> 1String
-; string-last extracts the last character from a non-empty string
-(check-expect (string-last "String") "g")
-(check-expect (string-last "42") "2")
-(check-expect (string-last "") "")
-(define (string-last string)
-  (if (and (string? string) (> (string-length string) 0))
-      (string-ith string (- (string-length string) 1))
-      ""
-      ))
-
-; String -> String
-; string-rest will take in a string and return a string with the 1st character
-; removed
-(check-expect (string-rest "String") "tring")
-(check-expect (string-rest "42") "2")
-(check-expect (string-rest "") "")
-(define (string-rest str)
-    (if (> (string-length str) 0)
-      (substring str 1 (string-length str))
-      ""
-    ))
-
-; String -> String
-; string-remove-last will take in a string and return a string with the last
-; character removed
-(check-expect (string-remove-last "String") "Strin")
-(check-expect (string-remove-last "42") "4")
-(check-expect (string-remove-last "") "")
-(define (string-remove-last str)
-    (if (> (string-length str) 0)
-      (substring str 0 (- (string-length str) 1))
-      ""
-    ))
-
 ; Editor -> Number
 ; cursor-location gets the x pixel location of the
 ; cursor-position in an Editor
-(define (cursor-location ed)
-  (image-width
-   (text (string-ith (editor-editor-text ed) (editor-cursor-position))
+(check-expect (cursor-location (make-editor "hello world" 0)) 0)
+(check-expect (cursor-location (make-editor "hello world" 3)) (image-width
+   (text (substring (editor-editor-text (make-editor "hello world" 3))
+                    0
+                    (editor-cursor-position (make-editor "hello world" 3)))
          EDITOR_TEXT_SIZE
          "black")))
- 
+(define (cursor-location ed)
+  (image-width
+   (text (substring (editor-editor-text ed) 0 (editor-cursor-position ed))
+         EDITOR_TEXT_SIZE
+         "black")))
 
+; Editor, KeyEvent -> Editor
+; edit takes in a KeyEvent and modifies an Editor
+;   given a valid event
+
+; Editor, KeyEvent -> Editor
+; edit takes in a KeyEvent and modifies an Editor
+;   given a valid event
+(check-expect (edit (make-editor "a" "b") "c") (make-editor "ac" "b"))
+(check-expect (edit (make-editor "" "b") "c") (make-editor "c" "b"))
+(check-expect (edit (make-editor "a" "") "c") (make-editor "ac" ""))
+(check-expect (edit (make-editor "hello" " world") ",") (make-editor "hello," " world"))
+(check-expect (edit (make-editor "hello" "world") " ") (make-editor "hello " "world"))
+(check-expect (edit (make-editor "a" "b") "\b") (make-editor "" "b"))
+(check-expect (edit (make-editor "a" "") "\b") (make-editor "" ""))
+(check-expect (edit (make-editor "" "b") "\b") (make-editor "" "b"))
+(check-expect (edit (make-editor "a" "b") "\t") (make-editor "a" "b"))
+(check-expect (edit (make-editor "a" "b") "\r") (make-editor "a" "b"))
+(check-expect (edit (make-editor "a" "b") "left") (make-editor "" "ab"))
+(check-expect (edit (make-editor "" "ab") "left") (make-editor "" "ab"))
+(check-expect (edit (make-editor "" "") "left") (make-editor "" ""))
+(check-expect (edit (make-editor "a" "b") "right") (make-editor "ab" ""))
+(check-expect (edit (make-editor "ab" "") "right") (make-editor "ab" ""))
+(check-expect (edit (make-editor "" "") "right") (make-editor "" ""))
+(check-expect (edit (make-editor "a" "b") "up") (make-editor "a" "b"))
+(check-expect (edit (make-editor "a" "b") "down") (make-editor "a" "b"))
+(check-expect (edit (make-editor "a" "b") "f1") (make-editor "a" "b"))
+(check-expect (edit (make-editor "a" "b") "shift") (make-editor "a" "b"))
+(check-expect (edit (make-editor "a" "b") "escape") (make-editor "a" "b"))
+(define (edit ed ke)
+  (cond [(= (string-length ke) 1)
+         (cond [(or (string=?  ke "\t") (string=?  ke "\r")) ed]
+               [(> (editor-text-size (make-editor
+                    (string-append (editor-pre ed) ke)
+                    (editor-post ed)))
+                   EDITOR_WIDTH) ed]
+               [(string=?  ke "\b") (make-editor
+                             (string-remove-last (editor-pre ed))
+                             (editor-post ed))]
+               [else (make-editor
+                     (string-append (editor-pre ed) ke)
+                     (editor-post ed))]
+         )]
+        [(string=?  "left" ke)
+         (cond [(= (editor-cursor-position ed) 0) ed]
+               [else (make-editor (editor-editor-text ed) (- (editor-cursor-position ed) 1))])
+        [(string=?  "right" ke) (make-editor
+         (cond [(= (editor-cursor-position ed) (string-length (editor-editor-text))) ed]
+               [else (make-editor (editor-editor-text ed) (+ (editor-cursor-position ed) 1))])
+        [else ed]))
 
 ; Editor -> image
 ; render displays the Editor
 (check-expect (render (make-editor "hello world" 0))  (overlay/align "left" "center"
-               (beside
-                (text (editor-pre (make-editor "hello " "world")) EDITOR_TEXT_SIZE "black")
-                CURSOR
-                (text (editor-post (make-editor "hello " "world")) EDITOR_TEXT_SIZE "black"))
-               (empty-scene EDITOR_WIDTH EDITOR_HEIGHT))) 
+               (overlay/align/offset "left" "center"
+                                     (text (editor-editor-text
+                                            (make-editor "hello world" 0))
+                                           EDITOR_TEXT_SIZE "black")
+                                     (cursor-location (make-editor "hello world" 0)) 0
+                                     CURSOR)
+                (empty-scene EDITOR_WIDTH EDITOR_HEIGHT)))
+(check-expect (render (make-editor "hello world" 6))  (overlay/align "left" "center"
+               (overlay/align/offset "left" "center"
+                                     (text (editor-editor-text
+                                            (make-editor "hello world" 6))
+                                           EDITOR_TEXT_SIZE "black")
+                                     (cursor-location (make-editor "hello world" 6)) 0
+                                     CURSOR)
+                (empty-scene EDITOR_WIDTH EDITOR_HEIGHT))) 
 (define (render ed)
   (overlay/align "left" "center"
-               (beside
-                (text (editor-pre ed) EDITOR_TEXT_SIZE "black")
-                CURSOR
-                (text (editor-post ed) EDITOR_TEXT_SIZE "black"))
+               (overlay/align/offset "left" "center"
+                                     (text (editor-editor-text ed) EDITOR_TEXT_SIZE "black")
+                                     (cursor-location ed) 0
+                                     CURSOR)
                (empty-scene EDITOR_WIDTH EDITOR_HEIGHT)))
+
+; Editor -> Editor
+; launches the program from an initial Editor
+(define (run editor-text)
+  (big-bang (make-editor editor-text 0)
+    [on-key edit]
+    [on-draw render]))
