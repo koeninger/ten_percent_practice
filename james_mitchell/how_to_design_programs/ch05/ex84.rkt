9b842366242669e82a4840281149d8c096c131dd
jmitchell <jmitchell@hyfnlocal.com>
2018-12-28 13:23:45 -0600

    ex84 WIP

---
+++ james_mitchell/how_to_design_programs/ch05/ex84.rkt
@@ -0,0 +1,50 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex84) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+(define WIDTH 200)
+
+(define-struct editor [pre post])
+; An Editor is a structure:
+;   (make-editor String String)
+; interpretation (make-editor s t) describes an editor
+; whose visible text is (string-append s t) with 
+; the cursor displayed between s and t
+
+(define cursor (rectangle 1 20 "solid" "red"))
+(define editor1 (make-editor "hello" "world"))
+
+; Editor -> Image
+; renders text in an empty scene with a red bar between pre and post
+(define (render editor)
+  (overlay/align "left" "center"
+               (beside/align "middle"
+                             (text (editor-pre editor) 16 "black")
+                             (rectangle 1 20 "solid" "red")
+                             (text (editor-post editor) 16 "black"))
+               (empty-scene WIDTH 20)))
+
+
+(check-expect (render editor1)
+              (overlay/align "left" "center"
+                  (beside/align "middle"
+                          (text (editor-pre editor1) 16 "black")
+                          (rectangle 1 20 "solid" "red")
+                          (text (editor-post editor1) 16 "black"))
+                  (empty-scene WIDTH 20)))
+
+; Edit
+; Editor, KeyEvent -> Editor
+; Adds a character (0-1,a-z) ke to the end of the pre field of ed
+; If backspace, removes a character to the left
+(define (edit ed ke)
+  (cond
+    [(and (= (string-length ke) 1) (or (string<=? "a" ke "z") (string<=? "0" ke "9")))
+     (make-editor (string-append (editor-pre ed) ke) (editor-post ed))]
+    [(string=? ke "\b") (backspace ed)]
+    [(string=? "left" ke) (move-left ed)]
+    [(string=? "right" ke) (move-right ed)]    
+    [else ed]
+    )
+)
+
+(render editor1)
