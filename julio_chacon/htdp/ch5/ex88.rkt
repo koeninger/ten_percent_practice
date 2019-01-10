a98f9a668a10f0af874ea0bb6aa71df3e80305cc
Julio Chacon <jchacon@hyfnlocal.com>
2019-01-09 23:14:10 -0600

    ex88

---
+++ julio_chacon/htdp/ch5/ex88.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex88) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(require 2htdp/image)
+(require 2htdp/universe)
+
+(define-struct cat [posX h])
+; A cat is a structure
+; (make-cat Number Number)
+; posX is the x coordinate and h is the hapines
+
+(define vcat (make-cat 10 70))  
+
