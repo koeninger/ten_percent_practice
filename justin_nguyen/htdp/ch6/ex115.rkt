87faafb34c73c1e582c7cecae7b046b40d2c7f51
Justin Nguyen <jnguyen@kixer.com>
2019-01-15 16:36:25 -0600

    finish part 1

---
+++ justin_nguyen/htdp/ch6/ex115.rkt
@@ -0,0 +1,31 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex115) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define MESSAGE1
+  "traffic light expected for first value, given some other value")
+
+(define MESSAGE
+  "traffic light expected for second value, given some other value")
+
+; A TrafficLight is one of the following Strings:
+; – "red"
+; – "green"
+; – "yellow"
+; interpretation the three strings represent the three 
+; possible states that a traffic light may assume 
+
+; Any Any -> Boolean
+; are the two values elements of TrafficLight and, 
+; if so, are they equal
+ 
+(check-expect (light=? "red" "red") #true)
+(check-expect (light=? "red" "green") #false)
+(check-expect (light=? "green" "green") #true)
+(check-expect (light=? "yellow" "yellow") #true)
+ 
+(define (light=? a-value another-value)
+  (if (and (light? a-value) (light? another-value))
+      (string=? a-value another-value)
+      (if (not (light? a-value))
+          (error MESSAGE1)
+          (error MESSAGE2))))
---
+++ justin_nguyen/htdp/ch6/ex116.rkt
@@ -0,0 +1,6 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex116) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; x is a variable, which is a valid expression
+; (= y z) is a function application, which is a valid expr
+; (= (= y z) 0) is a valid expression; it uses the previous example as an expression in a function application
