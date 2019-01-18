957d3fa777299a46f0923cfe779729a66a8545e2
Eric Kudler <joeento@gmail.com>
2019-01-17 23:04:01 -0500

    Iterate over barcodes in multiple images and generate new ones to be put into a pdf

--- eric_kudler/opencv-barcode/main.py
+++ eric_kudler/opencv-barcode/main.py
@@ -15,7 +15,15 @@ ap.add_argument("-i", "--image", required=False,
                 help="path to input image")
 ap.add_argument("-x", "--input", required=False,
                 help="path to collection of barcodes to be analyzed in bulk")
+
+total_barcodes = []
 args = vars(ap.parse_args())
+pdf = FPDF()
+pdf.add_page()
+
+image_x = pdf.w / 4
+image_y = 20
+
 for filename in os.listdir(args["input"]):
     if not filename.endswith('.png') and not filename.endswith('.jpeg') and not filename.endswith('.jpg'):
         continue
@@ -26,25 +34,36 @@ for filename in os.listdir(args["input"]):
     # find the barcodes in the image and decode each of the barcodes
     barcodes = pyzbar.decode(image)
 
-# loop over the detected barcodes
-for barcode in barcodes:
-    # extract the bounding box location of the barcode and draw the
-    # bounding box surrounding the barcode on the image
-    (x, y, w, h) = barcode.rect
-    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    total_barcodes += barcodes
 
+    # loop over the detected barcodes
+    for barcode in barcodes:
+        # the barcode data is a bytes object so if we want to draw it on
+        # our output image we need to convert it to a string first
+        barcodeData = barcode.data.decode("utf-8")
+        barcodeType = barcode.type
+
+        # print the barcode type and data to the terminal
+        print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+
+for barcode in total_barcodes:
     # the barcode data is a bytes object so if we want to draw it on
     # our output image we need to convert it to a string first
     barcodeData = barcode.data.decode("utf-8")
     barcodeType = barcode.type
 
-    # draw the barcode data and barcode type on the image
-    text = "{} ({})".format(barcodeData, barcodeType)
-    cv2.putText(image, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+    timestamp = int(round(time.time() * 1000))
+    image_name = barcode_builder.generate(barcodeType, barcodeData, output=str(timestamp), writer=barcode_builder.writer.ImageWriter())
+    pdf.image(image_name, x=image_x - (BARCODE_IMAGE_WIDTH / 2), y=image_y, w=BARCODE_IMAGE_WIDTH)
+    os.remove(image_name)
 
-    # print the barcode type and data to the terminal
-    print("[INFO] Found {} barcode: {}".format(barcodeType, barcodeData))
+    image_x += pdf.w / 4
+    if image_x >= pdf.w:
+        image_x = pdf.w / 4
+        image_y += 100
+
+pdf.output("test.pdf")
 
 # show the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+# cv2.imshow("Image", image)
+# cv2.waitKey(0)
