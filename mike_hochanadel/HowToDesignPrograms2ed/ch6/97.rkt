1839b95a339463d7dc5150b8a98c7eee2f3d464d
hokey <mike.hochanadel@lin-digital.com>
2019-01-21 17:14:30 -0600

    HTDP2ed chapter 6 exercise 97 finished

--- mike_hochanadel/HowToDesignPrograms2ed/ch6/97.rkt
+++ mike_hochanadel/HowToDesignPrograms2ed/ch6/97.rkt
@@ -17,17 +17,28 @@
 ; A Missile is a Posn. 
 ; interpretation (make-posn x y) is the missile's place
 
-(define BACKGROUND (empty-scene 200 200))
+(define BACKGROUND_WIDTH 200)
+(define BACKGROUND_HEIGHT 200)
+(define BACKGROUND (empty-scene BACKGROUND_WIDTH BACKGROUND_HEIGHT))
 
-(define MISSILE (triangle 10 "solid" "red"))
+(define MISSILE_SIZE 10)
+(define MISSILE (triangle MISSILE_SIZE "solid" "red"))
+
+(define UFO_WIDTH 30)
+(define UFO_HEIGHT 10)
 (define UFO (overlay
-             (circle 10 "solid" "green")
-             (rectangle 30 10 "solid" "green")))
+             (circle UFO_HEIGHT "solid" "green")
+             (rectangle UFO_WIDTH UFO_HEIGHT "solid" "green")))
+
+(define TANK_HEIGHT 20)
+(define TANK_WIDTH 30)
+(define TANK_TOP_WIDTH 10)
 (define TANK (overlay/align
               "center"
               "bottom"
-              (rectangle 10 20 "solid" "black")
-              (rectangle 30 10 "solid" "black")))
+              (rectangle TANK_TOP_WIDTH TANK_HEIGHT "solid" "black")
+              (rectangle TANK_WIDTH (/ TANK_HEIGHT  2) "solid" "black")))
+
 
 ; A SIGS is one of: 
 ; – (make-aim UFO Tank)
@@ -37,19 +48,88 @@
 
 ; tank, Image -> Image
 ; renders the tank on top of the given Image
-(define (tank-render t im) im)
+(check-expect (tank-render (make-tank 60 3) BACKGROUND) (place-image
+   TANK
+   (tank-loc (make-tank 60 3))
+   (- (image-height BACKGROUND) (/ TANK_HEIGHT 2))
+   BACKGROUND))
+(check-expect (tank-render (make-tank 0 56) BACKGROUND) (place-image
+   TANK
+   (tank-loc (make-tank 0 56))
+   (- (image-height BACKGROUND) (/ TANK_HEIGHT 2))
+   BACKGROUND))
+(check-expect (tank-render (make-tank BACKGROUND_WIDTH 0) BACKGROUND) (place-image
+   TANK
+   (tank-loc (make-tank BACKGROUND_WIDTH 0))
+   (- (image-height BACKGROUND) (/ TANK_HEIGHT 2))
+   BACKGROUND))
+(define (tank-render t im)
+  (place-image
+   TANK
+   (tank-loc t)
+   (- (image-height im) (/ TANK_HEIGHT 2))
+   im))
 
 ; UFO, Image -> Image
 ; renders UFO on top of the given Image
-(define (ufo-render u im) im)
+(check-expect (ufo-render (make-posn 100 35) BACKGROUND) (place-image
+   UFO
+   (posn-x (make-posn 100 35))
+   (- (image-height BACKGROUND) (posn-y (make-posn 100 35)))
+   BACKGROUND))
+(check-expect (ufo-render (make-posn 0 0) BACKGROUND) (place-image
+   UFO
+   (posn-x (make-posn 0 0))
+   (- (image-height BACKGROUND) (posn-y (make-posn 0 0)))
+   BACKGROUND))
+(check-expect (ufo-render (make-posn BACKGROUND_WIDTH BACKGROUND_HEIGHT) BACKGROUND) (place-image
+   UFO
+   (posn-x (make-posn BACKGROUND_WIDTH BACKGROUND_HEIGHT))
+   (- (image-height BACKGROUND) (posn-y (make-posn  BACKGROUND_WIDTH BACKGROUND_HEIGHT)))
+   BACKGROUND))
+(define (ufo-render u im)
+  (place-image
+   UFO
+   (posn-x u)
+   (- (image-height im) (posn-y u))
+   im))
 
 ; Missile, Image -> Image
 ; renders Missile on top of the given Image
-(define (missile-render m im) im)
+(check-expect (missile-render (make-posn 67 34) BACKGROUND) (place-image
+   MISSILE
+   (posn-x (make-posn 67 34))
+   (- (image-height BACKGROUND) (posn-y (make-posn 67 34)))
+   BACKGROUND))
+(check-expect (missile-render (make-posn 0 0) BACKGROUND) (place-image
+   MISSILE
+   (posn-x (make-posn 0 0))
+   (- (image-height BACKGROUND) (posn-y (make-posn 0 0)))
+   BACKGROUND))
+(check-expect (missile-render (make-posn BACKGROUND_WIDTH BACKGROUND_HEIGHT) BACKGROUND) (place-image
+   MISSILE
+   (posn-x (make-posn BACKGROUND_WIDTH BACKGROUND_HEIGHT))
+   (- (image-height BACKGROUND) (posn-y (make-posn BACKGROUND_WIDTH BACKGROUND_HEIGHT)))
+   BACKGROUND))
+(define (missile-render m im)
+  (place-image
+   MISSILE
+   (posn-x m)
+   (- (image-height im) (posn-y m))
+   im))
 
 ; SIGS -> Image
 ; renders the given game state on top of BACKGROUND 
 ; for examples see figure 32
+(check-expect (si-render (make-aim (make-posn 34 40) (make-tank 150 3)))
+              (tank-render (aim-tank (make-aim (make-posn 34 40) (make-tank 150 3)))
+                  (ufo-render (aim-ufo (make-aim (make-posn 34 40) (make-tank 150 3))) BACKGROUND)))
+(check-expect (si-render (make-fired (make-posn 180 140) (make-tank 100 34) (make-posn 180 100)))
+              (tank-render
+       (fired-tank (make-fired (make-posn 180 140) (make-tank 100 34) (make-posn 180 100)))
+       (ufo-render (fired-ufo (make-fired (make-posn 180 140) (make-tank 100 34) (make-posn 180 100)))
+                   (missile-render (fired-missile (make-fired (make-posn 180 140) (make-tank 100 34) (make-posn 180 100)))
+                                   BACKGROUND))))
 (define (si-render s)
   (cond
     [(aim? s)
