e1bc2ad35dcf0aee2bf55b3bbea73f94c9ea7b80
Chris Larson <clarson@kixer.com>
2019-01-28 15:03:27 -0600

    194

--- chris_larson/htdp/Exercise 191.rkt
+++ chris_larson/htdp/Exercise 191.rkt
@@ -21,22 +21,8 @@
 ; a plain background image 
 (define MT (empty-scene 50 50))
  
-; Image Polygon -> Image
-; renders the given polygon p into img
-(define (render-poly img p)
-  (cond
-    [(empty? (rest (rest (rest p))))
-     (render-line
-       (render-line
-         (render-line MT (first p) (second p))
-         (second p) (third p))
-       (third p) (first p))]
-    [else
-     (render-line (render-poly img (rest p))
-                  (first p)
-                  (second p))]))
 (check-expect
-  (render-poly MT triangle-p)
+  (render-polygon MT triangle-p)
   (scene+line
     (scene+line
       (scene+line MT 20 10 20 20 "red")
@@ -44,7 +30,7 @@
     30 20 20 10 "red"))
 
 (check-expect
-  (render-poly MT square-p)
+  (render-polygon MT square-p)
   (scene+line
     (scene+line
       (scene+line
@@ -53,12 +39,117 @@
       20 20 10 20 "red")
     10 20 10 10 "red"))
 
-; Image Posn Posn -> Image 
-; renders a line from p to q into img
-(define (render-line img p q)
-  (scene+line
-    img
-    (posn-x p) (posn-y p) (posn-x q) (posn-y q)
-    "red"))
+; Image Polygon -> Image 
+; adds an image of p to MT
+(define (render-polygon img p)
+  (render-line (connect-dots img p) (first p) (last p)))
 
+; cons last item onto p and then call connect-dots
+(define (render-poly.v1 img p)
+  (connect-dots img (cons (last p) p)))
+
+(check-expect
+  (render-poly.v1 MT triangle-p)
+  (scene+line
+    (scene+line
+      (scene+line MT 20 10 20 20 "red")
+      20 20 30 20 "red")
+    30 20 20 10 "red"))
+
+(check-expect
+  (render-poly.v1 MT square-p)
+  (scene+line
+    (scene+line
+      (scene+line
+        (scene+line MT 10 10 20 10 "red")
+        20 10 20 20 "red")
+      20 20 10 20 "red")
+    10 20 10 10 "red"))
+
+; add first item of p to end
+(define (render-poly.v2 img p)
+  (connect-dots img (add-at-end p (first p))))
+
+(define (add-at-end l s)
+  (cond
+    [(empty? l) (cons s '())]
+    [else
+     (cons (first l) (add-at-end (rest l) s))]))
+
+(check-expect
+  (render-poly.v2 MT triangle-p)
+  (scene+line
+    (scene+line
+      (scene+line MT 20 10 20 20 "red")
+      20 20 30 20 "red")
+    30 20 20 10 "red"))
+
+(check-expect
+  (render-poly.v2 MT square-p)
+  (scene+line
+    (scene+line
+      (scene+line
+        (scene+line MT 10 10 20 10 "red")
+        20 10 20 20 "red")
+      20 20 10 20 "red")
+    10 20 10 10 "red"))
+
+; connect-dots with posn
+(define (render-poly.v3 img p)
+  (connect-dots.v1 img p (first p)))
+
+(check-expect
+  (render-poly.v3 MT triangle-p)
+  (scene+line
+    (scene+line
+      (scene+line MT 20 10 20 20 "red")
+      20 20 30 20 "red")
+    30 20 20 10 "red"))
+
+(check-expect
+  (render-poly.v3 MT square-p)
+  (scene+line
+    (scene+line
+      (scene+line
+        (scene+line MT 10 10 20 10 "red")
+        20 10 20 20 "red")
+      20 20 10 20 "red")
+    10 20 10 10 "red"))
+
+; Image NELoP -> Image
+; connects the Posns in p in an image
+(define (connect-dots img p)
+  (cond
+    [(empty? (rest p)) MT]
+    [else (render-line (connect-dots img (rest p))
+                       (first p)
+                       (second p))]))
+(check-expect
+  (connect-dots MT square-p)
+    (scene+line
+      (scene+line
+        (scene+line MT 10 10 20 10 "red")
+        20 10 20 20 "red")
+      20 20 10 20 "red"))
+
+(define (connect-dots.v1 img p f)
+  (cond
+    [(empty? (rest p)) (render-line MT (first p) f)]
+    [else (render-line (connect-dots.v1 img (rest p) f)
+                       (first p)
+                       (second p))]))
+
+; Image Posn Posn -> Image 
+; draws a red line from Posn p to Posn q into im
+(define (render-line im p q)
+  (scene+line
+    im (posn-x p) (posn-y p) (posn-x q) (posn-y q) "red"))
+ 
+; Polygon -> Posn
+; extracts the last item from p
+(define (last p)
+  (cond
+    [(empty? (rest (rest (rest p)))) (third p)]
+    [else (last (rest p))]))
+(check-expect (render-line MT (make-posn 0 10) (make-posn 10 10)) (scene+line MT 0 10 10 10 "red"))
 
