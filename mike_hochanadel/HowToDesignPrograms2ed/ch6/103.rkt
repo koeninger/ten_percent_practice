024fb076e29370f7ee646f9743b136402e56a849
hokey <mike.hochanadel@lin-digital.com>
2019-02-05 16:00:35 -0600

    HTDP2ed chapter 6 exercise 103 continuing may start over

--- mike_hochanadel/HowToDesignPrograms2ed/ch6/103.rkt
+++ mike_hochanadel/HowToDesignPrograms2ed/ch6/103.rkt
@@ -1,15 +1,38 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname |103|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
-; An Animal has a body and other attributes
+(define-struct body [x y z])
+; A Body is a structure
+;   (make-body Number Number Number),
+; interpretation (make-body x y z) specifies the relative
+; space it fills
 
-; A Spider has a body and up to 8 remaining circular legs
+(define-struct circular-body [x radius])
+; A Circular Body is a structure
+;   (make-circular-body Number Number),
+; interpretation (make-circular-body x radius) specifies the
+; relative space it fills where x is the length and
+; the radius is the length from the center of the body
+; the circumference of the body
+
+; A Zoo Animal has is one of:
+; - Body
+; - Circular Body
+; - 1 or more Bodies and 1 or more Circular Bodies
+; interpretation 
+
+; A Spider is a structure has a body
+;   and up to 8 remaining circular body parts called legs
+
 
 ; An Elephant has a body
+;  interpretation represents a body
 
 ; A Boa Constrictor has a circular body
+;  interpretation represents a circular body
 
-; An Armadillo has a body and a circular tail
+; An Armadillo has a body and a circular body called a tail
+;  interpretation 
 
 ; A Cage is a structure:
 ; (make-cage Number Number Number).
