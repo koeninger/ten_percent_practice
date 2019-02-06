623fae354f1e6e53059954950d9c0e601276fdc3
Justin Nguyen <jnguyen@kixer.com>
2019-02-06 16:45:41 -0600

    135/136

---
+++ justin_nguyen/htdp/ch8/ex135.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex135) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+(define (contains-flatt? alon)
+  (cond
+    [(empty? alon) #false]
+    [(cons? alon)
+     (or (string=? (first alon) "Flatt")
+         (contains-flatt? (rest alon)))]))
+
+(contains-flatt? (cons "Flatt" (cons "C" '())))
+
+(contains-flatt?
+  (cons "A" (cons "Flatt" (cons "C" '()))))
+
+; we should get false.
