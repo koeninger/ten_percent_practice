853ce8c095a4bf89bedc871b5b43735f01fca47f
Joel Jimenez <JJimenez@nexstardigital.com>
2019-02-18 19:46:42 +0000

    htdp ex 177

--- joel_jimenez/how_to_design_programs/ch10.rkt
+++ joel_jimenez/how_to_design_programs/ch10.rkt
@@ -578,3 +578,55 @@
     [(empty? lln) '()]
     [(empty? (rest (first lln))) '()]
     [else (cons (rest (first lln)) (rest* (rest lln)))]))
+
+
+; 10.4 A Graphical Editor, Revisited
+
+(define-struct editor [pre post])
+; An Editor is a structure:
+;   (make-editor Lo1S Lo1S)
+; An Lo1S is one of:
+; – '()
+; – (cons 1String Lo1S)
+
+(define good
+  (cons "g" (cons "o" (cons "o" (cons "d" '())))))
+(define all
+  (cons "a" (cons "l" (cons "l" '()))))
+(define lla
+  (cons "l" (cons "l" (cons "a" '()))))
+
+; data example 1:
+(make-editor all good)
+; data example 2:
+(make-editor lla good)
+
+; Lo1s -> Lo1s
+; produces a reverse version of the given list
+(check-expect
+  (rev (cons "a" (cons "b" (cons "c" '()))))
+  (cons "c" (cons "b" (cons "a" '()))))
+(define (rev l)
+  (cond
+    [(empty? l) '()]
+    [else (add-at-end (rev (rest l)) (first l))]))
+
+; Lo1s 1String -> Lo1s
+; creates a new list by adding s to the end of l
+(check-expect
+  (add-at-end (cons "c" (cons "b" '())) "a")
+  (cons "c" (cons "b" (cons "a" '()))))
+(define (add-at-end l s)
+  (cond
+    [(empty? l) (cons s '())]
+    [else (cons (first l) (add-at-end (rest l) s))]))
+
+
+; Exercise 177.
+
+; String, String -> Editor
+; consumes two strings and produces an Editor
+(check-expect (create-editor "HELLO" "WORLD")
+  (make-editor (list "O" "L" "L" "E" "H")(list "W" "O" "R" "L" "D")))
+(define (create-editor s1 s2)
+  (make-editor (rev (explode s1)) (explode s2)))
