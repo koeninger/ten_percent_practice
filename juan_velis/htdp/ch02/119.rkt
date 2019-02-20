fafa091f44e26bf7cf601e7cb9a0245c8bdb7bec
jvelis <jvelis@kixer.com>
2019-02-20 15:26:29 -0600

    ex 120

---
+++ juan_velis/htdp/ch02/119.rkt
@@ -0,0 +1,13 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |119|) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+; ex 119
+
+; (define (f "x") x)
+; "x" is a constant.
+; (define (variable "constant") expresion)
+
+
+; (define (f x y z) (x))
+; too many parameters
+; (variable expr expr ...)
