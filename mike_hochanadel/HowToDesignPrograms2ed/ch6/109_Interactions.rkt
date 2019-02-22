24e639264f02a2d2031714f64eeec4ff07ca882d
hokey <mike.hochanadel@lin-digital.com>
2019-02-22 15:11:08 -0600

    HTDP2ed chapter 6 exercise 109

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch6/109_Interactions.rkt
@@ -0,0 +1,9 @@
+Welcome to DrRacket, version 7.0 [3m].
+Language: Beginning Student [custom]; memory limit: 128 MB.
+Teachpacks: 2htdp/image.rkt, batch-io.rkt, and universe.rkt.
+All 13 tests passed!
+> (abcd AA)
+"error, illegal key"
+> (abcd AA)
+"finished"
+> 
