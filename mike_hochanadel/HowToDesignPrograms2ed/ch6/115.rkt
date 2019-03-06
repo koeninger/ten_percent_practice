56d20753ada02099c1afa0e4aded2dd27f0d6fa7
hokey <mike.hochanadel@lin-digital.com>
2019-03-06 14:51:04 -0600

    HTDP2ed chapter 6 exercise 115

---
+++ mike_hochanadel/HowToDesignPrograms2ed/ch6/115.rkt
@@ -0,0 +1,28 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |115|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")) #f)))
+(define ARG1_MESSAGE "traffic light expected for first arguement, given some other value")
+(define ARG2_MESSAGE "traffic light expected for second arguement, given some other value")
+
+; Any -> Boolean
+; is the given value an element of TrafficLight
+(define (light? x)
+  (cond
+    [(string? x) (or (string=? "red" x)
+                     (string=? "green" x)
+                     (string=? "yellow" x))]
+    [else #false]))
+
+; Any Any -> Boolean
+; are the two values elements of TrafficLight and, 
+; if so, are they equal
+(check-expect (light=? "red" "red") #true)
+(check-expect (light=? "red" "green") #false)
+(check-expect (light=? "green" "green") #true)
+(check-expect (light=? "yellow" "yellow") #true)
+(define (light=? a-value another-value)
+  (cond
+    [(light? a-value) (cond
+      [(light? another-value) (string=? a-value another-value)]
+      [else (error ARG2_MESSAGE)])]
+    [else (error ARG1_MESSAGE)]))
