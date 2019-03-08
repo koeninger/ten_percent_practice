29ece49e6c708a982d7a94e6e8b577f1ca12b9a4
Chris Larson <clarson@kixer.com>
2019-03-08 17:00:24 -0600

    217 wip

--- chris_larson/htdp/Exercise 215.rkt
+++ chris_larson/htdp/Exercise 215.rkt
@@ -5,7 +5,7 @@
 (define WIDTH 600)
 (define STEP 15)
 (define BACKGROUND (empty-scene WIDTH HEIGHT))
-(define DOT (circle STEP "solid" "red"))
+(define DOT (circle (/ STEP 2) "solid" "red"))
 
 ; WorldState: data that represents the state of the world (cw)
 ; Worm p, Direction d
@@ -36,16 +36,29 @@
 ; state of the world from (clock-tick-handler cw) 
 (define (clock-tick-handler cw)
   (cond
-    [(string=? (WorldState-d cw) "down") (make-WorldState (list (make-posn (posn-x (first (WorldState-p cw))) (+ (posn-y (first (WorldState-p cw))) STEP))) (WorldState-d cw))]
-    [(string=? (WorldState-d cw) "up") (make-WorldState (list (make-posn (posn-x (first (WorldState-p cw))) (- (posn-y (first (WorldState-p cw))) STEP))) (WorldState-d cw))]
-    [(string=? (WorldState-d cw) "right") (make-WorldState (list (make-posn (+ (posn-x (first (WorldState-p cw))) STEP) (posn-y (first (WorldState-p cw))))) (WorldState-d cw))]
-    [(string=? (WorldState-d cw) "left") (make-WorldState (list (make-posn (- (posn-x (first (WorldState-p cw))) STEP) (posn-y (first (WorldState-p cw))))) (WorldState-d cw))]
-    [else (make-WorldState (list (make-posn (+ (posn-x (first (WorldState-p cw))) STEP) (posn-y (first (WorldState-p cw))))) "right")]))
+    [(string=? (WorldState-d cw) "down") (make-WorldState (pop-push (make-posn (posn-x (first (WorldState-p cw))) (+ (posn-y (first (WorldState-p cw))) STEP)) (WorldState-p cw)) (WorldState-d cw))]
+    [(string=? (WorldState-d cw) "up") (make-WorldState (pop-push (make-posn (posn-x (first (WorldState-p cw))) (- (posn-y (first (WorldState-p cw))) STEP)) (WorldState-p cw)) (WorldState-d cw))]
+    [(string=? (WorldState-d cw) "right") (make-WorldState (pop-push (make-posn (+ (posn-x (first (WorldState-p cw))) STEP) (posn-y (first (WorldState-p cw)))) (WorldState-p cw)) (WorldState-d cw))]
+    [(string=? (WorldState-d cw) "left") (make-WorldState (pop-push (make-posn (- (posn-x (first (WorldState-p cw))) STEP) (posn-y (first (WorldState-p cw)))) (WorldState-p cw)) (WorldState-d cw))]
+    [else (make-WorldState (pop-push (make-posn (+ (posn-x (first (WorldState-p cw))) STEP) (posn-y (first (WorldState-p cw)))) (WorldState-p cw)) "right")]))
 (check-expect (clock-tick-handler (make-WorldState (list (make-posn 300 300)) "")) (make-WorldState (list (make-posn (+ STEP 300) 300)) "right"))
 (check-expect (clock-tick-handler (make-WorldState (list (make-posn 300 300)) "right")) (make-WorldState (list (make-posn (+ STEP 300) 300)) "right"))
 (check-expect (clock-tick-handler (make-WorldState (list (make-posn 300 300)) "down")) (make-WorldState (list (make-posn 300 (+ STEP 300))) "down"))
 (check-expect (clock-tick-handler (make-WorldState (list (make-posn 300 300)) "up")) (make-WorldState (list (make-posn 300 (- 300 STEP))) "up"))
 (check-expect (clock-tick-handler (make-WorldState (list (make-posn 300 300)) "left")) (make-WorldState (list (make-posn (- 300 STEP) 300)) "left"))
+
+; List-of-posn posn -> List-of-posn
+(define (pop-push p lop)
+  (cons p (pop lop)))
+(check-expect (pop-push 6 (list 1 2 3 4 5)) (list 6 1 2 3 4))
+
+; List-of-posn posn -> List-of-posn
+(define (pop lop)
+  (cond
+    [(empty? (rest lop)) '()]
+    [else (cons (first lop) (pop (rest lop)))]))
+(check-expect (pop (list 1 2 3 4 5)) (list 1 2 3 4))
+
 
 ; WorldState String -> WorldState 
 ; for each keystroke, big-bang obtains the next state 
@@ -81,5 +94,5 @@
     [to-draw render]
     [on-key keystroke-handler]))
 
-; (main (make-WorldState (cons (make-posn 300 300) (cons (make-posn 285 300) '())) ""))
+; (main (make-WorldState (cons (make-posn 300 300) (cons (make-posn 285 300) (cons (make-posn 270 300 '()))) ""))
 ; (main (make-WorldState (cons (make-posn 500 300)'()) ""))
