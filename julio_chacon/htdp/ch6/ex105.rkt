bb79d2a43970e03b2638b9f46b5ef2993e2f76a6
Julio Chacon <jchacon@hyfnlocal.com>
2019-03-08 14:02:08 -0600

    ex105

---
+++ julio_chacon/htdp/ch6/ex105.rkt
@@ -0,0 +1,23 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex105) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
+; A Coordinate is one of: 
+; – a NegativeNumber 
+; interpretation on the y axis, distance from top
+; – a PositiveNumber 
+; interpretation on the x axis, distance from left
+; – a Posn
+; interpretation an ordinary Cartesian point
+
+(define-struct coordinate [p])
+
+
+(make-coordinate -10)
+(make-coordinate -5)
+
+(make-coordinate 10)
+(make-coordinate 5)
+
+
+(make-coordinate (make-posn 10 5))
+(make-coordinate (make-posn -10 -5))
